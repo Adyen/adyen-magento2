45074486c2afd753ea7ee7bcebe6c9c4732fa6e3
rikterbeek <rikterbeek@me.com>
2016-02-23 09:05:17 +0100

    #17 show the version based on setup_version

--- Block/Adminhtml/System/Config/Field/Version.php
+++ Block/Adminhtml/System/Config/Field/Version.php
@@ -28,10 +28,24 @@ namespace Adyen\Payment\Block\Adminhtml\System\Config\Field;
 
 class Version extends \Magento\Config\Block\System\Config\Form\Field
 {
+
+    /**
+     * @var \Magento\Framework\Module\ModuleListInterface
+     */
+    protected $_moduleList;
+
+    public function __construct(
+        \Magento\Framework\Module\ModuleListInterface $moduleList,
+        \Magento\Backend\Block\Template\Context $context,
+        array $data = []
+    )
+    {
+        parent::__construct($context, $data);
+        $this->_moduleList = $moduleList;
+    }
+
     protected function _getElementHtml(\Magento\Framework\Data\Form\Element\AbstractElement $element)
     {
-        // make this dynamic
-        //$configVer = $this->moduleList->getOne($moduleName)['setup_version'];
-        return (string) "0.1.0";
+        return (string) $this->_moduleList->getOne("Adyen_Payment")['setup_version'];
     }
 }
