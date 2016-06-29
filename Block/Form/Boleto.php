e262e563796edeca1d52b81ca4b6af5bd34f7841
Rik ter Beek <rikterbeek@me.com>
2016-06-29 11:28:48 +0200

    #59 add missing file

--- Block/Form/Boleto.php
+++ Block/Form/Boleto.php
@@ -30,5 +30,39 @@ class Boleto extends \Magento\Payment\Block\Form
      * @var string
      */
     protected $_template = 'Adyen_Payment::form/boleto.phtml';
+
+    /**
+     * @var \Adyen\Payment\Helper\Data
+     */
+    protected $_adyenHelper;
+
+    /**
+     * Boleto constructor.
+     *
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param \Adyen\Payment\Helper\Data $adyenHelper
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Adyen\Payment\Helper\Data $adyenHelper,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->_adyenHelper = $adyenHelper;
+    }
+
+    /**
+     * @return array
+     */
+    public function getBoletoTypes()
+    {
+        $boletoTypes = $this->_adyenHelper->getBoletoTypes();
+        $types = [];
+        foreach ($boletoTypes as $boletoType) {
+            $types[$boletoType['value']] = $boletoType['label'];
+        }
+        return $types;
+    }
     
 }
