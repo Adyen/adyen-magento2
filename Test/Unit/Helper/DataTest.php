d89b1b68449c547a7e48a14df4bf435a9abe5eb7
Jeroen <jeroen@reachdigital.nl>
2018-03-26 11:06:00 +0200

    Compatibility with PHPUnit 6

--- Test/Unit/Helper/DataTest.php
+++ Test/Unit/Helper/DataTest.php
@@ -23,10 +23,11 @@
 
 namespace Adyen\Payment\Tests\Helper;
 
-use Adyen\Payment\Helper\Data;
-
-class DataTest extends \PHPUnit_Framework_TestCase
+class DataTest extends \PHPUnit\Framework\TestCase
 {
+    /**
+     * @var \Adyen\Payment\Helper\Data
+     */
     private $dataHelper;
 
     private function getSimpleMock($originalClassName)
@@ -38,36 +39,35 @@ class DataTest extends \PHPUnit_Framework_TestCase
 
     public function setUp()
     {
-        $context = $this->getSimpleMock('\Magento\Framework\App\Helper\Context');
-        $encryptor = $this->getSimpleMock('\Magento\Framework\Encryption\EncryptorInterface');
-        $dataStorage = $this->getSimpleMock('\Magento\Framework\Config\DataInterface');
-        $country = $this->getSimpleMock('\Magento\Directory\Model\Config\Source\Country');
-        $moduleList = $this->getSimpleMock('\Magento\Framework\Module\ModuleListInterface');
-        $billingAgreementCollectionFactory = $this->getSimpleMock('\Adyen\Payment\Model\ResourceModel\Billing\Agreement\CollectionFactory');
-        $assetRepo = $this->getSimpleMock('\Magento\Framework\View\Asset\Repository');
-        $assetSource = $this->getSimpleMock('\Magento\Framework\View\Asset\Source');
-        $notificationFactory = $this->getSimpleMock('\Adyen\Payment\Model\ResourceModel\Notification\CollectionFactory');
+        $context = $this->getSimpleMock(\Magento\Framework\App\Helper\Context::class);
+        $encryptor = $this->getSimpleMock(\Magento\Framework\Encryption\EncryptorInterface::class);
+        $dataStorage = $this->getSimpleMock(\Magento\Framework\Config\DataInterface::class);
+        $country = $this->getSimpleMock(\Magento\Directory\Model\Config\Source\Country::class);
+        $moduleList = $this->getSimpleMock(\Magento\Framework\Module\ModuleListInterface::class);
+        $billingAgreementCollectionFactory = $this->getSimpleMock(\Adyen\Payment\Model\ResourceModel\Billing\Agreement\CollectionFactory::class);
+        $assetRepo = $this->getSimpleMock(\Magento\Framework\View\Asset\Repository::class);
+        $assetSource = $this->getSimpleMock(\Magento\Framework\View\Asset\Source::class);
+        $notificationFactory = $this->getSimpleMock(\Adyen\Payment\Model\ResourceModel\Notification\CollectionFactory::class);
 
-        $this->dataHelper = new Data($context, $encryptor, $dataStorage, $country, $moduleList,
+        $this->dataHelper = new \Adyen\Payment\Helper\Data($context, $encryptor, $dataStorage, $country, $moduleList,
             $billingAgreementCollectionFactory, $assetRepo, $assetSource, $notificationFactory);
     }
 
     public function testFormatAmount()
     {
-        $this->assertEquals("1234", $this->dataHelper->formatAmount("12.34", "EUR"));
-        $this->assertEquals("1200", $this->dataHelper->formatAmount("12.00", "USD"));
-        $this->assertEquals("12", $this->dataHelper->formatAmount("12.00", "JPY"));
+        $this->assertEquals('1234', $this->dataHelper->formatAmount('12.34', 'EUR'));
+        $this->assertEquals('1200', $this->dataHelper->formatAmount('12.00', 'USD'));
+        $this->assertEquals('12', $this->dataHelper->formatAmount('12.00', 'JPY'));
     }
     
     public function testisPaymentMethodOpenInvoiceMethod()
     {
-        $this->assertEquals(true, $this->dataHelper->isPaymentMethodOpenInvoiceMethod("klarna"));
-        $this->assertEquals(true, $this->dataHelper->isPaymentMethodOpenInvoiceMethod("klarna_account"));
-        $this->assertEquals(true, $this->dataHelper->isPaymentMethodOpenInvoiceMethod("afterpay"));
-        $this->assertEquals(true, $this->dataHelper->isPaymentMethodOpenInvoiceMethod("afterpay_default"));
-        $this->assertEquals(true, $this->dataHelper->isPaymentMethodOpenInvoiceMethod("ratepay"));
-        $this->assertEquals(false, $this->dataHelper->isPaymentMethodOpenInvoiceMethod("ideal"));
-        $this->assertEquals(true, $this->dataHelper->isPaymentMethodOpenInvoiceMethod("test_klarna"));
+        $this->assertEquals(true, $this->dataHelper->isPaymentMethodOpenInvoiceMethod('klarna'));
+        $this->assertEquals(true, $this->dataHelper->isPaymentMethodOpenInvoiceMethod('klarna_account'));
+        $this->assertEquals(true, $this->dataHelper->isPaymentMethodOpenInvoiceMethod('afterpay'));
+        $this->assertEquals(true, $this->dataHelper->isPaymentMethodOpenInvoiceMethod('afterpay_default'));
+        $this->assertEquals(true, $this->dataHelper->isPaymentMethodOpenInvoiceMethod('ratepay'));
+        $this->assertEquals(false, $this->dataHelper->isPaymentMethodOpenInvoiceMethod('ideal'));
+        $this->assertEquals(true, $this->dataHelper->isPaymentMethodOpenInvoiceMethod('test_klarna'));
     }
 }
-
