9eff8baa05384dd253453041669dbcd80c0dfb1e
Alessio Zampatti <gigendh@gmail.com>
2018-06-11 15:35:18 +0200

    PW-455: Support partial refunds for open invoice payment methods (#278)
    * PW-455: Support partial refunds for open invoice payment methods
    
    * PW-455: Use Plugin(Interceptor) instead of Event and Observer
    
    * PW-455: Removed observer reference
    
    * PW-455: Refactor
    
    * PW-455: Refactor
    
    * PW-455: Removed plugin and added check on invoice

---
+++ Model/ResourceModel/Invoice/Collection.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Payment Module
+ *
+ * Copyright (c) 2018 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+
+namespace Adyen\Payment\Model\ResourceModel\Invoice;
+
+class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
+{
+    /**
+     * Construct
+     */
+    public function _construct()
+    {
+        $this->_init('Adyen\Payment\Model\Invoice', 'Adyen\Payment\Model\ResourceModel\Invoice');
+    }
+
+}
