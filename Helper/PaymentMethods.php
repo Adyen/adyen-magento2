7da167ec143ebdee0a042f5e0c2f79f4a69bf990
attilak <attila.kiss@adyen.com>
2018-11-21 15:30:45 +0100

    Using paymentMethods instead of directory lookup and some optimizations

--- Helper/PaymentMethods.php
+++ Helper/PaymentMethods.php
@@ -33,68 +33,62 @@ class PaymentMethods extends AbstractHelper
     /**
      * @var \Magento\Quote\Api\CartRepositoryInterface
      */
-    protected $_quoteRepository;
-
-    /**
-     * @var \Magento\Quote\Model\QuoteIdMaskFactory
-     */
-    protected $_quoteIdMaskFactory;
+    protected $quoteRepository;
 
     /**
      * @var \Magento\Framework\App\Config\ScopeConfigInterface $config
      */
-    protected $_config;
+    protected $config;
 
     /**
      * @var \Adyen\Payment\Helper\Data
      */
-    protected $_adyenHelper;
+    protected $adyenHelper;
 
     /**
      * @var \Magento\Checkout\Model\Session
      */
-    protected $_session;
+    protected $session;
 
     /**
      * @var \Magento\Framework\Locale\ResolverInterface
      */
-    protected $_localeResolver;
+    protected $localeResolver;
 
     /**
      * @var \Adyen\Payment\Logger\AdyenLogger
      */
-    protected $_adyenLogger;
+    protected $adyenLogger;
 
     /**
      * @var Repository
      */
-    protected $_assetRepo;
+    protected $assetRepo;
 
     /**
      * @var RequestInterface
      */
-    protected $_request;
+    protected $request;
 
     /**
      * @var \Magento\Framework\View\Asset\Source
      */
-    protected $_assetSource;
+    protected $assetSource;
 
     /**
      * @var \Magento\Framework\View\DesignInterface
      */
-    protected $_design;
+    protected $design;
 
     /**
      * @var \Magento\Framework\View\Design\Theme\ThemeProviderInterface
      */
-    protected $_themeProvider;
+    protected $themeProvider;
 
     /**
      * PaymentMethods constructor.
      *
      * @param \Magento\Quote\Api\CartRepositoryInterface $quoteRepository
-     * @param \Magento\Quote\Model\QuoteIdMaskFactory $quoteIdMaskFactory
      * @param \Magento\Framework\App\Config\ScopeConfigInterface $config
      * @param Data $adyenHelper
      * @param \Magento\Checkout\Model\Session $session
@@ -108,7 +102,6 @@ class PaymentMethods extends AbstractHelper
      */
     public function __construct(
         \Magento\Quote\Api\CartRepositoryInterface $quoteRepository,
-        \Magento\Quote\Model\QuoteIdMaskFactory $quoteIdMaskFactory,
         \Magento\Framework\App\Config\ScopeConfigInterface $config,
         \Adyen\Payment\Helper\Data $adyenHelper,
         \Magento\Checkout\Model\Session $session,
@@ -120,18 +113,17 @@ class PaymentMethods extends AbstractHelper
         \Magento\Framework\View\DesignInterface $design,
         \Magento\Framework\View\Design\Theme\ThemeProviderInterface $themeProvider
     ) {
-        $this->_quoteRepository = $quoteRepository;
-        $this->_quoteIdMaskFactory = $quoteIdMaskFactory;
-        $this->_config = $config;
-        $this->_adyenHelper = $adyenHelper;
-        $this->_session = $session;
-        $this->_localeResolver = $localeResolver;
-        $this->_adyenLogger = $adyenLogger;
-        $this->_assetRepo = $assetRepo;
-        $this->_request = $request;
-        $this->_assetSource = $assetSource;
-        $this->_design = $design;
-        $this->_themeProvider = $themeProvider;
+        $this->quoteRepository = $quoteRepository;
+        $this->config = $config;
+        $this->adyenHelper = $adyenHelper;
+        $this->session = $session;
+		$this->localeResolver = $localeResolver;
+        $this->adyenLogger = $adyenLogger;
+        $this->assetRepo = $assetRepo;
+        $this->request = $request;
+        $this->assetSource = $assetSource;
+        $this->design = $design;
+        $this->themeProvider = $themeProvider;
     }
 
     /**
@@ -140,9 +132,10 @@ class PaymentMethods extends AbstractHelper
     public function getPaymentMethods($quoteId, $country = null)
     {
         // get quote from quoteId
-        $quote = $this->_quoteRepository->getActive($quoteId);
+        $quote = $this->quoteRepository->getActive($quoteId);
         $store = $quote->getStore();
-        $paymentMethods = $this->_addHppMethodsToConfig($store, $country);
+
+        $paymentMethods = $this->addHppMethodsToConfig($store, $country);
         return $paymentMethods;
     }
 
@@ -150,23 +143,23 @@ class PaymentMethods extends AbstractHelper
      * @param $store
      * @return array
      */
-    protected function _addHppMethodsToConfig($store, $country)
+    protected function addHppMethodsToConfig($store, $country)
     {
         $paymentMethods = [];
 
-        $ccEnabled = $this->_config->getValue(
+        $ccEnabled = $this->config->getValue(
             'payment/' . \Adyen\Payment\Model\Ui\AdyenCcConfigProvider::CODE . '/active',
             \Magento\Store\Model\ScopeInterface::SCOPE_STORES,
             $store->getCode()
         );
-        $ccTypes = array_keys($this->_adyenHelper->getCcTypesAltData());
-        $sepaEnabled = $this->_config->getValue(
+        $ccTypes = array_keys($this->adyenHelper->getCcTypesAltData());
+        $sepaEnabled = $this->config->getValue(
             'payment/' . \Adyen\Payment\Model\Ui\AdyenSepaConfigProvider::CODE . '/active',
             \Magento\Store\Model\ScopeInterface::SCOPE_STORES,
             $store->getCode()
         );
 
-        foreach ($this->_fetchHppMethods($store, $country) as $methodCode => $methodData) {
+        foreach ($this->fetchHppMethods($store, $country) as $methodCode => $methodData) {
             /*
              * skip payment methods if it is a creditcard that is enabled in adyen_cc
              * or if payment is sepadirectdebit and SEPA api is enabled
@@ -188,54 +181,54 @@ class PaymentMethods extends AbstractHelper
      * @param $country
      * @return array
      */
-    protected function _fetchHppMethods($store, $country)
+    protected function fetchHppMethods($store, $country)
     {
-        $skinCode = $this->_adyenHelper->getAdyenHppConfigData('skin_code');
-        $merchantAccount = $this->_adyenHelper->getAdyenAbstractConfigData('merchant_account');
+        $merchantAccount = $this->adyenHelper->getAdyenAbstractConfigData('merchant_account');
 
-        if (!$skinCode || !$merchantAccount) {
+        if (!$merchantAccount) {
             return [];
         }
 
         $adyFields = [
-            "paymentAmount" => (int)$this->_adyenHelper->formatAmount(
-                $this->_getCurrentPaymentAmount(),
-                $this->_getCurrentCurrencyCode($store)
-            ),
-            "currencyCode" => $this->_getCurrentCurrencyCode($store),
-            "merchantReference" => "Get Payment methods",
-            "skinCode" => $skinCode,
-            "merchantAccount" => $merchantAccount,
-            "sessionValidity" => date(
-                DATE_ATOM,
-                mktime(date("H") + 1, date("i"), date("s"), date("m"), date("j"), date("Y"))
-            ),
-            "countryCode" => $this->_getCurrentCountryCode($store, $country),
-            "shopperLocale" => $this->_getCurrentLocaleCode($store)
+			"merchantAccount" => $merchantAccount,
+			"countryCode" => $this->getCurrentCountryCode($store, $country),
+            "amount" => [
+				"currency" => $this->getCurrentCurrencyCode($store),
+            	"value" => (int)$this->adyenHelper->formatAmount(
+					$this->getCurrentPaymentAmount(),
+					$this->getCurrentCurrencyCode($store)
+				),
+			],
+            "shopperReference" => $this->getCurrentShopperReference(),
+			"shopperLocale" => $this->getCurrentLocaleCode($store)
         ];
 
-        $responseData = $this->_getDirectoryLookupResponse($adyFields, $store);
+        $responseData = $this->getPaymentMethodsResponse($adyFields, $store);
 
         $paymentMethods = [];
         if (isset($responseData['paymentMethods'])) {
             foreach ($responseData['paymentMethods'] as $paymentMethod) {
-                $paymentMethodCode = $paymentMethod['brandCode'];
-                $paymentMethod = $this->_fieldMapPaymentMethod($paymentMethod);
 
+            	if ($paymentMethod['type'] == "scheme") {
+            		continue;
+				}
+
+                $paymentMethodCode = $paymentMethod['type'];
+                $paymentMethod = $this->fieldMapPaymentMethod($paymentMethod);
 
                 // check if payment method is an openinvoice method
                 $paymentMethod['isPaymentMethodOpenInvoiceMethod'] =
-                    $this->_adyenHelper->isPaymentMethodOpenInvoiceMethod($paymentMethodCode);
+                    $this->adyenHelper->isPaymentMethodOpenInvoiceMethod($paymentMethodCode);
 
                 // add icon location in result
-                if ($this->_adyenHelper->showLogos()) {
+                if ($this->adyenHelper->showLogos()) {
                     // Fix for MAGETWO-70402 https://github.com/magento/magento2/pull/7686
                     // Explicitly setting theme
                     $themeCode = "Magento/blank";
 
-                    $themeId = $this->_design->getConfigurationDesignTheme(\Magento\Framework\App\Area::AREA_FRONTEND);
+                    $themeId = $this->design->getConfigurationDesignTheme(\Magento\Framework\App\Area::AREA_FRONTEND);
                     if (!empty($themeId)) {
-                        $theme = $this->_themeProvider->getThemeById($themeId);
+                        $theme = $this->themeProvider->getThemeById($themeId);
                         if ($theme && !empty($theme->getCode())) {
                             $themeCode = $theme->getCode();
                         }
@@ -244,14 +237,14 @@ class PaymentMethods extends AbstractHelper
                     $params = [];
                     $params = array_merge([
                         'area' => \Magento\Framework\App\Area::AREA_FRONTEND,
-                        '_secure' => $this->_request->isSecure(),
+                        '_secure' => $this->request->isSecure(),
                         'theme' => $themeCode
                     ], $params);
 
-                    $asset = $this->_assetRepo->createAsset('Adyen_Payment::images/logos/' .
+                    $asset = $this->assetRepo->createAsset('Adyen_Payment::images/logos/' .
                         $paymentMethodCode . '.png', $params);
 
-                    $placeholder = $this->_assetSource->findSource($asset);
+                    $placeholder = $this->assetSource->findSource($asset);
 
                     $icon = null;
                     if ($placeholder) {
@@ -274,9 +267,9 @@ class PaymentMethods extends AbstractHelper
     /**
      * @return bool|int
      */
-    protected function _getCurrentPaymentAmount()
+    protected function getCurrentPaymentAmount()
     {
-        if (($grandTotal = $this->_getQuote()->getGrandTotal()) > 0) {
+        if (($grandTotal = $this->getQuote()->getGrandTotal()) > 0) {
             return $grandTotal;
         }
         return 10;
@@ -287,19 +280,19 @@ class PaymentMethods extends AbstractHelper
      * @param $store
      * @return mixed
      */
-    protected function _getCurrentCurrencyCode($store)
+    protected function getCurrentCurrencyCode($store)
     {
-        return $this->_getQuote()->getQuoteCurrencyCode() ?: $store->getBaseCurrencyCode();
+        return $this->getQuote()->getQuoteCurrencyCode() ?: $store->getBaseCurrencyCode();
     }
 
     /**
      * @param $store
      * @return int|mixed|string
      */
-    protected function _getCurrentCountryCode($store, $country)
+    protected function getCurrentCountryCode($store, $country)
     {
         // if fixed countryCode is setup in config use this
-        $countryCode = $this->_adyenHelper->getAdyenHppConfigData('country_code', $store->getId());
+        $countryCode = $this->adyenHelper->getAdyenHppConfigData('country_code', $store->getId());
 
         if ($countryCode != "") {
             return $countryCode;
@@ -309,11 +302,11 @@ class PaymentMethods extends AbstractHelper
             return $country;
         }
 
-        if ($country = $this->_getQuote()->getBillingAddress()->getCountry()) {
+        if ($country = $this->getQuote()->getBillingAddress()->getCountry()) {
             return $country;
         }
 
-        $defaultCountry = $this->_config->getValue(
+        $defaultCountry = $this->config->getValue(
             \Magento\Tax\Model\Config::CONFIG_XML_PATH_DEFAULT_COUNTRY,
             \Magento\Store\Model\ScopeInterface::SCOPE_STORES,
             $store->getCode()
@@ -330,20 +323,20 @@ class PaymentMethods extends AbstractHelper
      * @param $store
      * @return mixed|string
      */
-    protected function _getCurrentLocaleCode($store)
+    protected function getCurrentLocaleCode($store)
     {
-        $localeCode = $this->_adyenHelper->getAdyenAbstractConfigData('shopper_locale', $store->getId());
+        $localeCode = $this->adyenHelper->getAdyenAbstractConfigData('shopper_locale', $store->getId());
         if ($localeCode != "") {
             return $localeCode;
         }
 
-        $locale = $this->_localeResolver->getLocale();
+        $locale = $this->localeResolver->getLocale();
         if ($locale) {
             return $locale;
         }
 
         // should have the value if not fall back to default
-        $localeCode = $this->_config->getValue(
+        $localeCode = $this->config->getValue(
             \Magento\Directory\Helper\Data::XML_PATH_DEFAULT_LOCALE,
             \Magento\Store\Model\ScopeInterface::SCOPE_STORES,
             $store->getCode()
@@ -355,7 +348,7 @@ class PaymentMethods extends AbstractHelper
     /**
      * @var array
      */
-    protected $_fieldMapPaymentMethod = [
+    protected $fieldMapPaymentMethod = [
         'name' => 'title'
     ];
 
@@ -363,9 +356,9 @@ class PaymentMethods extends AbstractHelper
      * @param $paymentMethod
      * @return mixed
      */
-    protected function _fieldMapPaymentMethod($paymentMethod)
+    protected function fieldMapPaymentMethod($paymentMethod)
     {
-        foreach ($this->_fieldMapPaymentMethod as $field => $newField) {
+        foreach ($this->fieldMapPaymentMethod as $field => $newField) {
             if (isset($paymentMethod[$field])) {
                 $paymentMethod[$newField] = $paymentMethod[$field];
                 unset($paymentMethod[$field]);
@@ -380,41 +373,20 @@ class PaymentMethods extends AbstractHelper
      * @return array
      * @throws \Adyen\AdyenException
      */
-    protected function _getDirectoryLookupResponse($requestParams, $store)
+    protected function getPaymentMethodsResponse($requestParams, $store)
     {
-        $cacheKey = $this->_getCacheKeyForRequest($requestParams, $store);
 
         // initialize the adyen client
-        $client = new \Adyen\Client();
-
-        if ($this->_adyenHelper->isDemoMode()) {
-            $client->setEnvironment(\Adyen\Environment::TEST);
-        } else {
-            $client->setEnvironment(\Adyen\Environment::LIVE);
-        }
-
-        // connect to magento log
-        $client->setLogger($this->_adyenLogger);
-
-        $hmacKey = $this->_adyenHelper->getHmac();
-
-        // create and add signature
-        try {
-            $requestParams["merchantSig"] = \Adyen\Util\Util::calculateSha256Signature($hmacKey, $requestParams);
-        } catch (\Adyen\AdyenException $e) {
-            $this->_adyenLogger->error($e->getMessage());
-            // return empty result
-            return [];
-        }
+        $client = $this->adyenHelper->initializeAdyenClient($this->getQuote()->getStoreId());
 
         // initialize service
-        $service = new \Adyen\Service\DirectoryLookup($client);
+        $service = $this->adyenHelper->createAdyenCheckoutService($client);
 
         try {
-            $responseData = $service->directoryLookup($requestParams);
+            $responseData = $service->paymentMethods($requestParams);
         } catch (\Adyen\AdyenException $e) {
-            $this->_adyenLogger->error(
-                "The Directory Lookup response is empty check your Adyen configuration in Magento."
+            $this->adyenLogger->error(
+                "The Payment methods response is empty check your Adyen configuration in Magento."
             );
             // return empty result
             return [];
@@ -424,40 +396,18 @@ class PaymentMethods extends AbstractHelper
     }
 
     /**
-     * @var array
-     */
-    protected $_cacheParams = [
-        'currencyCode',
-        'merchantReference',
-        'skinCode',
-        'merchantAccount',
-        'countryCode',
-        'shopperLocale',
-    ];
-
-    /**
-     * @param $requestParams
-     * @param $store
-     * @return string
-     */
-    protected function _getCacheKeyForRequest($requestParams, $store)
-    {
-        $cacheParams = [];
-        $cacheParams['store'] = $store->getId();
-        foreach ($this->_cacheParams as $paramKey) {
-            if (isset($requestParams[$paramKey])) {
-                $cacheParams[$paramKey] = $requestParams[$paramKey];
-            }
-        }
-
-        return md5(implode('|', $cacheParams));
-    }
-
-    /**
      * @return \Magento\Quote\Model\Quote
      */
-    protected function _getQuote()
+    protected function getQuote()
     {
-        return $this->_session->getQuote();
+        return $this->session->getQuote();
     }
+
+	/**
+	 * @return int
+	 */
+    protected function getCurrentShopperReference()
+	{
+		return $this->getQuote()->getCustomerId();
+	}
 }
