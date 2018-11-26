eb16c541678c45cf0a4aa4e3588c74d764793482
attilak <attila.kiss@adyen.com>
2018-11-26 15:38:15 +0100

    Handle response from Checkout API redirect

--- Helper/Data.php
+++ Helper/Data.php
@@ -1666,7 +1666,7 @@ class Data extends AbstractHelper
 
 	/**
 	 * @param $client
-	 * @return mixed
+	 * @return \Adyen\Service\Checkout
 	 */
 	public function createAdyenCheckoutService($client)
 	{
