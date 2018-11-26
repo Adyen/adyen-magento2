0cc444b5aa295cee34e2859a90fa29d9537f4ac9
attilak <attila.kiss@adyen.com>
2018-11-26 13:54:58 +0100

    - Exclude open invoice payments from the checkout flow and route them through hpp  - Code changes based on review

--- Gateway/Response/CheckoutPaymentsDetailsHandler.php
+++ Gateway/Response/CheckoutPaymentsDetailsHandler.php
@@ -76,5 +76,9 @@ class CheckoutPaymentsDetailsHandler implements HandlerInterface
         // do not close transaction so you can do a cancel() and void
         $payment->setIsTransactionClosed(false);
         $payment->setShouldCloseParentTransaction(false);
+
+		$payment->unsAdditionalInformation('paymentData');
+		$payment->unsAdditionalInformation('paRequest');
+		$payment->unsAdditionalInformation('md');
     }
 }
