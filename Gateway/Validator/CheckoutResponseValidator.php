06c5993deb8ed59b294e8771d3d2d674773a8bdc
Rik ter Beek <Rik.terBeek@adyen.com>
2019-02-21 10:49:01 +0800

    add support for banktransfer_iban

--- Gateway/Validator/CheckoutResponseValidator.php
+++ Gateway/Validator/CheckoutResponseValidator.php
@@ -27,44 +27,54 @@ use Magento\Payment\Gateway\Validator\AbstractValidator;
 
 class CheckoutResponseValidator extends AbstractValidator
 {
-	/**
-	 * @var \Adyen\Payment\Logger\AdyenLogger
-	 */
-	private $adyenLogger;
+    /**
+     * @var \Adyen\Payment\Logger\AdyenLogger
+     */
+    private $adyenLogger;
 
-	/**
-	 * GeneralResponseValidator constructor.
-	 *
-	 * @param \Magento\Payment\Gateway\Validator\ResultInterfaceFactory $resultFactory
-	 * @param \Adyen\Payment\Logger\AdyenLogger $adyenLogger
-	 */
-	public function __construct(
-		\Magento\Payment\Gateway\Validator\ResultInterfaceFactory $resultFactory,
-		\Adyen\Payment\Logger\AdyenLogger $adyenLogger
-	) {
-		$this->adyenLogger = $adyenLogger;
-		parent::__construct($resultFactory);
-	}
+    /**
+     * GeneralResponseValidator constructor.
+     *
+     * @param \Magento\Payment\Gateway\Validator\ResultInterfaceFactory $resultFactory
+     * @param \Adyen\Payment\Logger\AdyenLogger $adyenLogger
+     */
+    public function __construct(
+        \Magento\Payment\Gateway\Validator\ResultInterfaceFactory $resultFactory,
+        \Adyen\Payment\Logger\AdyenLogger $adyenLogger
+    ) {
+        $this->adyenLogger = $adyenLogger;
+        parent::__construct($resultFactory);
+    }
 
-	/**
-	 * @param array $validationSubject
-	 * @return \Magento\Payment\Gateway\Validator\ResultInterface
-	 */
-	public function validate(array $validationSubject)
-	{
-		$response = \Magento\Payment\Gateway\Helper\SubjectReader::readResponse($validationSubject);
-		$paymentDataObjectInterface = \Magento\Payment\Gateway\Helper\SubjectReader::readPayment($validationSubject);
-		$payment = $paymentDataObjectInterface->getPayment();
+    /**
+     * @param array $validationSubject
+     * @return \Magento\Payment\Gateway\Validator\ResultInterface
+     */
+    public function validate(array $validationSubject)
+    {
+        $response = \Magento\Payment\Gateway\Helper\SubjectReader::readResponse($validationSubject);
+        $paymentDataObjectInterface = \Magento\Payment\Gateway\Helper\SubjectReader::readPayment($validationSubject);
+        $payment = $paymentDataObjectInterface->getPayment();
 
-		$payment->setAdditionalInformation('3dActive', false);
-		$isValid = true;
-		$errorMessages = [];
+        $payment->setAdditionalInformation('3dActive', false);
+        $isValid = true;
+        $errorMessages = [];
 
         // validate result
         if (isset($response['resultCode'])) {
             switch ($response['resultCode']) {
                 case "Authorised":
                 case "Received":
+
+                    // For banktransfers store all bankTransfer details
+                    if (!empty($response['additionalData']['bankTransfer.owner'])) {
+                        foreach ($response['additionalData'] as $key => $value) {
+                            if (strpos($key, 'bankTransfer') === 0) {
+                                $payment->setAdditionalInformation($key, $value);
+                            }
+                        }
+                    }
+
                     $payment->setAdditionalInformation('pspReference', $response['pspReference']);
                     break;
                 case "PresentToShopper":
@@ -97,82 +107,82 @@ class CheckoutResponseValidator extends AbstractValidator
                     break;
                 case "RedirectShopper":
 
-					$redirectUrl = null;
-					$paymentData = null;
+                    $redirectUrl = null;
+                    $paymentData = null;
 
-					if (!empty($response['redirect']['url'])) {
-						$redirectUrl = $response['redirect']['url'];
-					}
+                    if (!empty($response['redirect']['url'])) {
+                        $redirectUrl = $response['redirect']['url'];
+                    }
 
-					if (!empty($response['redirect']['method'])) {
-						$redirectMethod = $response['redirect']['method'];
-					}
+                    if (!empty($response['redirect']['method'])) {
+                        $redirectMethod = $response['redirect']['method'];
+                    }
 
-					if (!empty($response['paymentData'])) {
-						$paymentData = $response['paymentData'];
-					}
+                    if (!empty($response['paymentData'])) {
+                        $paymentData = $response['paymentData'];
+                    }
 
-					// If the redirect data is there then the payment is a card payment with 3d secure
-					if (isset($response['redirect']['data']['PaReq']) && isset($response['redirect']['data']['MD'])) {
+                    // If the redirect data is there then the payment is a card payment with 3d secure
+                    if (isset($response['redirect']['data']['PaReq']) && isset($response['redirect']['data']['MD'])) {
 
-						$paReq = null;
-						$md = null;
+                        $paReq = null;
+                        $md = null;
 
-						$payment->setAdditionalInformation('3dActive', true);
+                        $payment->setAdditionalInformation('3dActive', true);
 
-						if (!empty($response['redirect']['data']['PaReq'])) {
-							$paReq = $response['redirect']['data']['PaReq'];
-						}
+                        if (!empty($response['redirect']['data']['PaReq'])) {
+                            $paReq = $response['redirect']['data']['PaReq'];
+                        }
 
-						if (!empty($response['redirect']['data']['MD'])) {
-							$md = $response['redirect']['data']['MD'];
-						}
+                        if (!empty($response['redirect']['data']['MD'])) {
+                            $md = $response['redirect']['data']['MD'];
+                        }
 
-						if ($paReq && $md && $redirectUrl && $paymentData && $redirectMethod) {
-							$payment->setAdditionalInformation('redirectUrl', $redirectUrl);
-							$payment->setAdditionalInformation('redirectMethod', $redirectMethod);
-							$payment->setAdditionalInformation('paRequest', $paReq);
-							$payment->setAdditionalInformation('md', $md);
-							$payment->setAdditionalInformation('paymentData', $paymentData);
-						} else {
-							$isValid = false;
-							$errorMsg = __('3D secure is not valid.');
-							$this->adyenLogger->error($errorMsg);;
-							$errorMessages[] = $errorMsg;
-						}
-					// otherwise it is an alternative payment method which only requires the redirect url to be present
-					} else {
-						// Flag to show we are in the checkoutAPM flow
-						$payment->setAdditionalInformation('checkoutAPM', true);
+                        if ($paReq && $md && $redirectUrl && $paymentData && $redirectMethod) {
+                            $payment->setAdditionalInformation('redirectUrl', $redirectUrl);
+                            $payment->setAdditionalInformation('redirectMethod', $redirectMethod);
+                            $payment->setAdditionalInformation('paRequest', $paReq);
+                            $payment->setAdditionalInformation('md', $md);
+                            $payment->setAdditionalInformation('paymentData', $paymentData);
+                        } else {
+                            $isValid = false;
+                            $errorMsg = __('3D secure is not valid.');
+                            $this->adyenLogger->error($errorMsg);;
+                            $errorMessages[] = $errorMsg;
+                        }
+                        // otherwise it is an alternative payment method which only requires the redirect url to be present
+                    } else {
+                        // Flag to show we are in the checkoutAPM flow
+                        $payment->setAdditionalInformation('checkoutAPM', true);
 
-						if ($redirectUrl && $paymentData && $redirectMethod) {
-							$payment->setAdditionalInformation('redirectUrl', $redirectUrl);
-							$payment->setAdditionalInformation('redirectMethod', $redirectMethod);
-							$payment->setAdditionalInformation('paymentData', $paymentData);
-						} else {
-							$isValid = false;
-							$errorMsg = __('Payment method is not valid.');
-							$this->adyenLogger->error($errorMsg);;
-							$errorMessages[] = $errorMsg;
-						}
-					}
+                        if ($redirectUrl && $paymentData && $redirectMethod) {
+                            $payment->setAdditionalInformation('redirectUrl', $redirectUrl);
+                            $payment->setAdditionalInformation('redirectMethod', $redirectMethod);
+                            $payment->setAdditionalInformation('paymentData', $paymentData);
+                        } else {
+                            $isValid = false;
+                            $errorMsg = __('Payment method is not valid.');
+                            $this->adyenLogger->error($errorMsg);;
+                            $errorMessages[] = $errorMsg;
+                        }
+                    }
 
-					break;
-				case "Refused":
-					$errorMsg = __('The payment is REFUSED.');
-					// this will result the specific error
-					throw new \Magento\Framework\Exception\LocalizedException(__($errorMsg));
-					break;
-				default:
-					$errorMsg = __('Error with payment method please select different payment method.');
-					throw new \Magento\Framework\Exception\LocalizedException(__($errorMsg));
-					break;
-			}
-		} else {
-			$errorMsg = __('Error with payment method please select different payment method.');
-			throw new \Magento\Framework\Exception\LocalizedException(__($errorMsg));
-		}
+                    break;
+                case "Refused":
+                    $errorMsg = __('The payment is REFUSED.');
+                    // this will result the specific error
+                    throw new \Magento\Framework\Exception\LocalizedException(__($errorMsg));
+                    break;
+                default:
+                    $errorMsg = __('Error with payment method please select different payment method.');
+                    throw new \Magento\Framework\Exception\LocalizedException(__($errorMsg));
+                    break;
+            }
+        } else {
+            $errorMsg = __('Error with payment method please select different payment method.');
+            throw new \Magento\Framework\Exception\LocalizedException(__($errorMsg));
+        }
 
-		return $this->createResult($isValid, $errorMessages);
-	}
+        return $this->createResult($isValid, $errorMessages);
+    }
 }
