8beab8e264d981528600c826f25a7bbd13793a26
marcoss <marcos.silvagarcia@adyen.com>
2019-08-14 16:25:57 +0200

    Expose last 4 digits of API keys
    To help debug issues, expose the last 4 digits of API keys.

---
+++ Test/Unit/Comment/ApiKeyEndingTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Adyen\Payment\Test\Comment;
+
+use Adyen\Payment\Model\Comment\ApiKeyEnding;
+use Magento\Framework\Encryption\Encryptor;
+
+class ApiKeyEndingTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @var ApiKeyEnding
+     */
+    private $apiKeyEndingComment;
+
+    public function setUp()
+    {
+        $encryptor = $this->createMock(Encryptor::class);
+        $map = [
+            ['4321', '1234'],
+            ['asdfasdfasdf', 'qwerqwerqwer']
+        ];
+        $encryptor->method('decrypt')
+            ->will($this->returnValueMap($map));
+
+        $this->apiKeyEndingComment = new ApiKeyEnding($encryptor);
+    }
+
+    public function testSomething()
+    {
+        $this->assertEquals('API key ending with: <strong>1234</strong>', $this->apiKeyEndingComment->getCommentText('4321'));
+        $this->assertEquals('API key ending with: <strong>qwer</strong>', $this->apiKeyEndingComment->getCommentText('asdfasdfasdf'));
+    }
+}
