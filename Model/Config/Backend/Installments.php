57abb30d80cf6cc9e07ea60dfea1410544000b1d
asrar7787 <asrar7787@gmail.com>
2019-09-06 12:38:47 +0100

    updated logic implementation

--- Model/Config/Backend/Installments.php
+++ Model/Config/Backend/Installments.php
@@ -112,11 +112,13 @@ class Installments extends \Magento\Framework\App\Config\Value
     protected function _afterLoad()
     {
         $value = $this->getValue();
-        if (!empty($value) && is_array($value)) {
-            $value = $this->serializer->unserialize($value);
-            $value = $this->encodeArrayFieldValue($value);
-            $this->setValue($value);
+        if (empty($value)) {
+            return $this;
         }
+
+        $value = $this->serializer->unserialize($value);
+        $value = $this->encodeArrayFieldValue($value);
+        $this->setValue($value);
         return $this;
     }
 
