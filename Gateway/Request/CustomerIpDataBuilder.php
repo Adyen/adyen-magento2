962a96782dec812d037c4e129e5ce00b5c2c9a1a
attilak <attila.kiss@adyen.com>
2019-10-16 16:21:17 +0200

    Order->getXForwardedFor() can return null, in this case fall back for Order->getRemoteIp()

--- Gateway/Request/CustomerIpDataBuilder.php
+++ Gateway/Request/CustomerIpDataBuilder.php
@@ -39,7 +39,8 @@ class CustomerIpDataBuilder implements BuilderInterface
      *
      * @param \Adyen\Payment\Helper\Requests $adyenRequestsHelper
      */
-    public function __construct(\Adyen\Payment\Helper\Requests $adyenRequestsHelper)
+    public function __construct(
+        \Adyen\Payment\Helper\Requests $adyenRequestsHelper)
     {
         $this->adyenRequestsHelper = $adyenRequestsHelper;
     }
@@ -52,7 +53,12 @@ class CustomerIpDataBuilder implements BuilderInterface
     {
         /** @var \Magento\Payment\Gateway\Data\PaymentDataObject $paymentDataObject */
         $paymentDataObject = \Magento\Payment\Gateway\Helper\SubjectReader::readPayment($buildSubject);
-        $shopperIp = $paymentDataObject->getPayment()->getOrder()->getXForwardedFor();
+        $order = $paymentDataObject->getPayment()->getOrder();
+        $shopperIp = $order->getXForwardedFor();
+
+        if (empty($shopperIp)) {
+            $shopperIp = $order->getRemoteIp();
+        }
 
         return $this->adyenRequestsHelper->buildCustomerIpData([], $shopperIp);
     }
