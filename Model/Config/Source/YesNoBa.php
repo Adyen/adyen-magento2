7b83041bee952d79344a8998155e6e910bc7a847
alessio <alessio.zampatti@adyen.com>
2019-11-14 15:45:11 +0100

    Extended YesNo with changed label for billing agreements

---
+++ Model/Config/Source/YesNoBa.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Payment Module
+ *
+ * Copyright (c) 2019 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+
+namespace Adyen\Payment\Model\Config\Source;
+
+class YesNoBa extends \Magento\Config\Model\Config\Source\YesNo
+{
+
+    /**
+     * Options getter
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        return [['value' => 1, 'label' => __('Yes - as billing agreement')], ['value' => 0, 'label' => __('No')]];
+    }
+
+}
