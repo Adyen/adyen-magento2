f7b1ab1e5a58f82d207b74d8c80d1e7ec14c75bd
Alessio Zampatti <alessio.zampatti@adyen.com>
2019-11-14 16:35:17 +0100

    PW-1688: UI Alternative Payment Methods => Local Payment Methods (#583)
    * PW-1688: UI Alternative Payment Methods => Local Payment Methods
    
    * Update Model/Config/Source/SplitPaymentRefundStrategy.php
    
    Co-Authored-By: Marcos Garcia <marcos.asgarcia@gmail.com>
    
    * fixed capital
    
    * Extended YesNo with changed label for billing agreements
    
    * remove unused file
    
    * fix typo

--- Model/Config/Source/YesNoBillingAgreements.php
+++ Model/Config/Source/YesNoBillingAgreements.php
@@ -13,39 +13,28 @@
  *                               #############
  *                               ############
  *
- * Adyen Payment module (https://www.adyen.com/)
+ * Adyen Payment Module
  *
- * Copyright (c) 2015 Adyen BV (https://www.adyen.com/)
- * See LICENSE.txt for license details.
+ * Copyright (c) 2019 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
  *
  * Author: Adyen <magento@adyen.com>
  */
 
 namespace Adyen\Payment\Model\Config\Source;
 
-class SplitPaymentRefundStrategy implements \Magento\Framework\Option\ArrayInterface
+class YesNoBillingAgreements extends \Magento\Config\Model\Config\Source\YesNo
 {
-    const REFUND_FIRST_PAYEMENT_FIRST = 1;
-    const REFUND_LAST_PAYEMENT_FIRST = 2;
-    const REFUND_ON_RATIO = 3;
 
     /**
+     * Options getter
+     *
      * @return array
      */
     public function toOptionArray()
     {
-        return $this->getSplitPaymentRefundStrategies();
+        return [['value' => 1, 'label' => __('Yes - as billing agreement')], ['value' => 0, 'label' => __('No')]];
     }
-    
-    /**
-     * @return array
-     */
-    public function getSplitPaymentRefundStrategies()
-    {
-        return [
-            self::REFUND_FIRST_PAYEMENT_FIRST => __('Refund from first payment first'),
-            self::REFUND_LAST_PAYEMENT_FIRST => __('Refund from last payment first'),
-            self::REFUND_ON_RATIO => __('Refund based on ratio')
-        ];
-    }
+
 }
