0bc6fdf8cd2d09ab8506be25da51c51408c9c4f9
acampos1916 <angel.campos@adyen.com>
2020-01-24 11:18:51 +0100

    [PW-1947]: Comment for clarity

--- Model/Cron.php
+++ Model/Cron.php
@@ -865,6 +865,7 @@ class Cron
 
         switch ($this->_eventCode) {
             case Notification::REFUND_FAILED:
+                //Trigger admin notice for REFUND_FAILED notifications
                 $this->_addRefundFailedNotice();
                 break;
             case Notification::REFUND:
