be98223ecfd52191b495b720527a7eece8e1e260
attilak <attila.kiss@adyen.com>
2020-02-05 15:10:38 +0100

    Add new API endpoint to retrieve order payment status

---
+++ Model/AdyenOrderPaymentStatus.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Payment Module
+ *
+ * Copyright (c) 2020 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+
+namespace Adyen\Payment\Model;
+
+class AdyenOrderPaymentStatus implements \Adyen\Payment\Api\AdyenOrderPaymentStatusInterface
+{
+    /**
+     * {@inheritDoc}
+     * @throws MagentoNoSuchEntityException
+     * @throws AdyenException
+     */
+    public function getOrderPaymentStatus($orderId)
+    {
+        return json_encode($orderId);
+    }
+}
