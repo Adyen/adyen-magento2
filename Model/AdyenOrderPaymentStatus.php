0ba75878bd5d80bc3ca5668d486923ea206bdcbe
attilak <attila.kiss@adyen.com>
2020-02-11 11:28:36 +0100

    Add AdyenOrderPaymentStatus class to retrieve order payment status
    Add getOrderPaymentStatus() to handle threeDS2 extra steps but nothing
    more
    Later this function can be used for more extensive PWA implementation

--- Model/AdyenOrderPaymentStatus.php
+++ Model/AdyenOrderPaymentStatus.php
@@ -24,8 +24,44 @@
 
 namespace Adyen\Payment\Model;
 
+use Magento\Framework\Exception\NoSuchEntityException;
+use Adyen\Payment\Model\Ui\AdyenCcConfigProvider;
+
 class AdyenOrderPaymentStatus implements \Adyen\Payment\Api\AdyenOrderPaymentStatusInterface
 {
+    /**
+     * @var \Magento\Sales\Api\OrderRepositoryInterface
+     */
+    protected $orderRepository;
+
+    /**
+     * @var \Adyen\Payment\Logger\AdyenLogger
+     */
+    protected $adyenLogger;
+
+    /**
+     * @var \Adyen\Payment\Helper\Data
+     */
+    protected $adyenHelper;
+
+    /**
+     * AdyenOrderPaymentStatus constructor.
+     *
+     * @param \Magento\Sales\Api\OrderRepositoryInterface $orderRepository
+     * @param \Adyen\Payment\Logger\AdyenLogger $adyenLogger
+     * @param \Adyen\Payment\Helper\Data $adyenHelper
+     */
+    public function __construct(
+        \Magento\Sales\Api\OrderRepositoryInterface $orderRepository,
+        \Adyen\Payment\Logger\AdyenLogger $adyenLogger,
+        \Adyen\Payment\Helper\Data $adyenHelper
+    )
+    {
+        $this->orderRepository = $orderRepository;
+        $this->adyenLogger = $adyenLogger;
+        $this->adyenHelper = $adyenHelper;
+    }
+
     /**
      * {@inheritDoc}
      * @throws MagentoNoSuchEntityException
@@ -33,6 +69,23 @@ class AdyenOrderPaymentStatus implements \Adyen\Payment\Api\AdyenOrderPaymentSta
      */
     public function getOrderPaymentStatus($orderId)
     {
-        return json_encode($orderId);
+        try {
+            $order = $this->orderRepository->get($orderId);
+            $payment = $order->getPayment();
+
+            if ($payment->getMethod() === AdyenCcConfigProvider::CODE) {
+                return $this->adyenHelper->buildThreeDS2ProcessResponseJson(
+                    $payment->getAdditionalInformation('threeDSType'),
+                    $payment->getAdditionalInformation('threeDS2Token')
+                );
+            } else {
+                return $result;
+            }
+        } catch (NoSuchEntityException $e) {
+            $this->adyenLogger->error("Exception: " . $e->getMessage());
+            throw new \Magento\Framework\Exception\LocalizedException(__('This order no longer exists.'));
+        }
+
+        return $result;
     }
 }
