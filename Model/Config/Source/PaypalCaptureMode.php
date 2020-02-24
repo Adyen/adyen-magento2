6a7a95d20415cb4c15f2f8c5f4aa7c184f1eecc4
acampos1916 <angel.campos@adyen.com>
2020-02-24 16:25:52 +0100

    [PW-1819]: New Paypal Capture Mode source model to replace Yes/No labels with Manual/Immediately

---
+++ Model/Config/Source/PaypalCaptureMode.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Payment module (https://www.adyen.com/)
+ *
+ * Copyright (c) 2020 Adyen BV (https://www.adyen.com/)
+ * See LICENSE.txt for license details.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+
+namespace Adyen\Payment\Model\Config\Source;
+
+class PaypalCaptureMode implements \Magento\Framework\Option\ArrayInterface
+{
+    const OPTIONS = [
+        [
+            'value' => 0,
+            'label' => 'Capture immediately'
+        ],
+        [
+            'value' => 1,
+            'label' => 'Capture manually'
+        ],
+    ];
+
+    /**
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        return self::OPTIONS;
+    }
+}
