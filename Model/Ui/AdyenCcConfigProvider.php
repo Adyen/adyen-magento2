3b4c8a83c0a4a3ad5c87b67b9bcd3fb00c4864d5
Ángel Campos <angel.campos@adyen.com>
2020-02-28 15:33:52 +0100

    Update Model/Ui/AdyenCcConfigProvider.php
    Co-Authored-By: cyattilakiss <42297201+cyattilakiss@users.noreply.github.com>

--- Model/Ui/AdyenCcConfigProvider.php
+++ Model/Ui/AdyenCcConfigProvider.php
@@ -182,15 +182,19 @@ class AdyenCcConfigProvider implements ConfigProviderInterface
         $types = [];
         $ccTypes = $this->_adyenHelper->getAdyenCcTypes();
         $enableAvailableTypes = $this->_adyenHelper->getAdyenCcConfigData('enablecctypes');
-        if ($enableAvailableTypes) {
-            $availableTypes = $this->_adyenHelper->getAdyenCcConfigData('cctypes');
-            if ($availableTypes) {
-                $availableTypes = explode(',', $availableTypes);
-                foreach (array_keys($ccTypes) as $code) {
-                    if (in_array($code, $availableTypes)) {
-                        $types[$code] = $ccTypes[$code]['name'];
-                    }
-                }
+        if (!$enableAvailableTypes) {
+            return $types;
+        }
+        
+        $availableTypes = $this->_adyenHelper->getAdyenCcConfigData('cctypes');
+        if (!$availableTypes) {
+            return $types;
+        }
+        
+        $availableTypes = explode(',', $availableTypes);
+        foreach (array_keys($ccTypes) as $code) {
+            if (in_array($code, $availableTypes)) {
+                $types[$code] = $ccTypes[$code]['name'];
             }
         }
 
@@ -298,4 +302,3 @@ class AdyenCcConfigProvider implements ConfigProviderInterface
         return $this->_request;
     }
 }
-
