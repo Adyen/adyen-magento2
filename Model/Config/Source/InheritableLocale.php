a99678165deb9149e655326451121399a3252755
marcoss <marcos.silvagarcia@adyen.com>
2020-03-20 17:10:28 +0100

    Change shopper locale config input type

---
+++ Model/Config/Source/InheritableLocale.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Adyen\Payment\Model\Config\Source;
+
+
+use Magento\Config\Model\Config\Source\Locale as MagentoLocale;
+
+class InheritableLocale extends MagentoLocale
+{
+    public function toOptionArray()
+    {
+        $locales = parent::toOptionArray();
+        array_unshift($locales, [
+            'value' => '',
+            'label' => 'Use system locale'
+        ]);
+        return $locales;
+    }
+}
