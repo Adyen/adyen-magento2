9d60080db31e16865af1dba9894175d9bf0a1b45
acampos1916 <angel.campos@adyen.com>
2020-04-17 15:17:07 +0200

    [PW-2264]: New data builder for LPMs to include storePaymentMethod flag based on configuration

---
+++ Gateway/Request/RecurringAlternativePaymentMethodsDataBuilder.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Payment module (https://www.adyen.com/)
+ *
+ * Copyright (c) 2015 Adyen BV (https://www.adyen.com/)
+ * See LICENSE.txt for license details.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+
+namespace Adyen\Payment\Gateway\Request;
+
+use Magento\Payment\Gateway\Request\BuilderInterface;
+
+class RecurringAlternativePaymentMethodsDataBuilder implements BuilderInterface
+{
+    /**
+     * @var \Magento\Framework\App\State
+     */
+    private $appState;
+
+    /**
+     * @var \Adyen\Payment\Helper\Requests
+     */
+    private $adyenRequestsHelper;
+
+    /**
+     * RecurringDataBuilder constructor.
+     *
+     * @param \Magento\Framework\Model\Context $context
+     * @param \Adyen\Payment\Helper\Requests $adyenRequestsHelper
+     */
+    public function __construct(
+        \Magento\Framework\Model\Context $context,
+        \Adyen\Payment\Helper\Requests $adyenRequestsHelper
+    ) {
+        $this->appState = $context->getAppState();
+        $this->adyenRequestsHelper = $adyenRequestsHelper;
+    }
+
+
+    /**
+     * @param array $buildSubject
+     * @return array
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function build(array $buildSubject)
+    {
+        /** @var \Magento\Payment\Gateway\Data\PaymentDataObject $paymentDataObject */
+        $paymentDataObject = \Magento\Payment\Gateway\Helper\SubjectReader::readPayment($buildSubject);
+        $payment = $paymentDataObject->getPayment();
+        $storeId = $payment->getOrder()->getStoreId();
+        $areaCode = $this->appState->getAreaCode();
+        $additionalInformation = $payment->getAdditionalInformation();
+        $request['body'] = $this->adyenRequestsHelper->buildRecurringAlternativePaymentMethodsData([], $areaCode,
+            $storeId, $additionalInformation);
+        return $request;
+    }
+}
