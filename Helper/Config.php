3e0b2d62d1a9b4f48edf5f512d76bf6127a079a7
Ángel Campos <angel.campos@adyen.com>
2020-04-20 11:40:10 +0200

    [PW-2199]: Add configuration to toggle order cancellation on notification processing (#692)
    * [PW-2199]: New can_cancel config value that avoids all order cancellations
    
    * [PW-2199]: Removed can_cancel check from files not related to notification processing
    
    * [PW-2199]: Renamed config value for clarity
    
    * [PW-2199]: Fixing adyenHelper object
    
    * [PW-2199]: Checking config value before changing order status to NEW
    
    * Newline at the end of Config.php
    
    * [PW-2199]: Removing dependency on AbstractHelper

---
+++ Helper/Config.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Payment module (https://www.adyen.com/)
+ *
+ * Copyright (c) 2020 Adyen BV (https://www.adyen.com/)
+ * See LICENSE.txt for license details.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+
+namespace Adyen\Payment\Helper;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
+class Config
+{
+
+    const XML_PAYMENT_PREFIX = "payment";
+    const XML_ADYEN_ABSTRACT_PREFIX = "adyen_abstract";
+    const XML_NOTIFICATIONS_CAN_CANCEL_FIELD = "notifications_can_cancel";
+
+    /**
+     * @var Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    protected $scopeConfig;
+
+    /**
+     * Config constructor.
+     * @param Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(
+        ScopeConfigInterface $scopeConfig
+    ) {
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * Retrieve flag for notifications_can_cancel
+     *
+     * @param int $storeId
+     * @return bool
+     */
+    public function getNotificationsCanCancel($storeId = null)
+    {
+        return (bool)$this->getConfigData(
+            self::XML_NOTIFICATIONS_CAN_CANCEL_FIELD,
+            self::XML_ADYEN_ABSTRACT_PREFIX,
+            $storeId,
+            true
+        );
+    }
+
+    /**
+     * Retrieve information from payment configuration
+     *
+     * @param string $field
+     * @param string $xmlPrefix
+     * @param int $storeId
+     * @param bool|false $flag
+     * @return bool|mixed
+     */
+    private function getConfigData($field, $xmlPrefix, $storeId, $flag = false)
+    {
+        $path = implode("/", [self::XML_PAYMENT_PREFIX, $xmlPrefix, $field]);
+
+        if (!$flag) {
+            return $this->scopeConfig->getValue($path, \Magento\Store\Model\ScopeInterface::SCOPE_STORE, $storeId);
+        } else {
+            return $this->scopeConfig->isSetFlag($path, \Magento\Store\Model\ScopeInterface::SCOPE_STORE, $storeId);
+        }
+    }
+}
