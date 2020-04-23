aef5b40a9411cf0a40f2b5f713b802f21c349b8b
attilak <attila.kiss@adyen.com>
2020-04-23 16:54:35 +0200

    Do not save payment methods in the adyen-method-list
    Remove adyen-method-list.js
    Remove callback parameter from retrieveAvailablePaymentMethods and
    remove handling the ajax request handling when done
    Move processThreeDS2 from threeds2.js and remove threeds2.js file
    Remove callback parameter from processThreeDS2 and
    remove handling the ajax request handling when done

--- view/frontend/web/js/model/adyen-payment-service.js
+++ view/frontend/web/js/model/adyen-payment-service.js
@@ -6,34 +6,19 @@ define(
     [
         'underscore',
         'Magento_Checkout/js/model/quote',
-        'Adyen_Payment/js/model/adyen-method-list',
         'Magento_Customer/js/model/customer',
         'Magento_Checkout/js/model/url-builder',
         'mage/storage',
         'Adyen_Payment/js/bundle',
     ],
-    function (_, quote, methodList, customer, urlBuilder, storage) {
+    function (_, quote, customer, urlBuilder, storage, adyenComponent) {
         'use strict';
         var checkoutComponent = {};
         return {
             /**
-             * Populate the list of payment methods
-             * @param {Array} methods
-             */
-            setPaymentMethods: function (methods) {
-                methodList(methods);
-            },
-            /**
-             * Get the list of available payment methods.
-             * @returns {Array}
-             */
-            getAvailablePaymentMethods: function () {
-                return methodList();
-            },
-            /**
              * Retrieve the list of available payment methods from the server
              */
-            retrieveAvailablePaymentMethods: function (callback) {
+            retrieveAvailablePaymentMethods: function () {
                 var self = this;
 
                 // retrieve payment methods
@@ -52,23 +37,30 @@ define(
                     shippingAddress: quote.shippingAddress()
                 };
 
-                storage.post(
-                    serviceUrl, JSON.stringify(payload)
-                ).done(
-                    function (response) {
-                        var jsonResponse = JSON.parse(response);
-                        console.log(jsonResponse);
-
-                        self.setPaymentMethods(jsonResponse);
-                        if (callback) {
-                            callback();
-                        }
-                    }
-                ).fail(
-                    function () {
-                        self.setPaymentMethods([]);
-                    }
+                return storage.post(
+                    serviceUrl,
+                    JSON.stringify(payload),
+                    true
                 )
+            },
+            /**
+             * The results that the 3DS2 components returns in the onComplete callback needs to be sent to the
+             * backend to the /adyen/threeDS2Process endpoint and based on the response render a new threeDS2
+             * component or place the order (validateThreeDS2OrPlaceOrder)
+             * @param response
+             */
+            processThreeDS2: function (data) {
+                var payload = {
+                    "payload": JSON.stringify(data)
+                };
+
+                var serviceUrl = urlBuilder.createUrl('/adyen/threeDS2Process', {});
+
+                return storage.post(
+                    serviceUrl,
+                    JSON.stringify(payload),
+                    true
+                );
             },
             getOrderPaymentStatus: function (orderId) {
                 var serviceUrl = urlBuilder.createUrl('/adyen/orders/:orderId/payment-status', {
