6d93a63914eb9172e93071ff678e76a525830dd7
Marcos Garcia <marcos.asgarcia@gmail.com>
2020-06-12 10:07:47 +0200

    Remove unnecessary conditional

--- Controller/Process/Json.php
+++ Controller/Process/Json.php
@@ -333,11 +333,9 @@ class Json extends \Magento\Framework\App\Action\Action
 
         // If notification is test check if fields are correct if not return error
         if ($this->_isTestNotification($response['pspReference'])) {
-            if (!$usernameIsValid || !$passwordIsValid) {
-                $this->_returnResult(
-                    'username (PHP_AUTH_USER) and\or password (PHP_AUTH_PW) are not the same as Magento settings'
-                );
-            }
+            $this->_returnResult(
+                'username (PHP_AUTH_USER) and\or password (PHP_AUTH_PW) are not the same as Magento settings'
+            );
         }
         return false;
     }
