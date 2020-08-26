87c72debde70fc0ebb74ec4f45490d3fffcd5870
Ángel Campos <angel.campos@adyen.com>
2020-08-26 12:38:02 +0200

    [PW-2913] Adding redirect data builder to CC authorize requests (#799)
    * [PW-2913] Adding redirect data builder to CC authorize requests
    
    * Adding termUrl to redirect form
    
    * Adding termUrl to redirect form
    
    * Update Gateway/Validator/CheckoutResponseValidator.php
    
    Co-authored-by: Attila Kiss <42297201+cyattilakiss@users.noreply.github.com>
    
    * [PW-2913 ] Adjusting Redirect controller for new 3DS1 flow
    
    * Removing redirect data builder from AdyenPaymentCcVaultAuthorizeRequest
    
    Co-authored-by: Attila Kiss <42297201+cyattilakiss@users.noreply.github.com>

---
+++ Gateway/Request/RedirectDataBuilder.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Payment module (https://www.adyen.com/)
+ *
+ * Copyright (c) 2020 Adyen BV (https://www.adyen.com/)
+ * See LICENSE.txt for license details.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+
+namespace Adyen\Payment\Gateway\Request;
+
+use Magento\Payment\Gateway\Request\BuilderInterface;
+
+class RedirectDataBuilder implements BuilderInterface
+{
+    /**
+     * @var \Magento\Framework\App\State
+     */
+    private $appState;
+
+    /**
+     * @var \Adyen\Payment\Helper\Requests
+     */
+    private $adyenRequestsHelper;
+
+    /**
+     * RedirectDataBuilder constructor.
+     *
+     * @param \Magento\Framework\Model\Context $context
+     * @param \Adyen\Payment\Helper\Requests $adyenRequestsHelper
+     */
+    public function __construct(
+        \Magento\Framework\Model\Context $context,
+        \Adyen\Payment\Helper\Requests $adyenRequestsHelper
+    ) {
+        $this->appState = $context->getAppState();
+        $this->adyenRequestsHelper = $adyenRequestsHelper;
+    }
+
+    /**
+     * @param array $buildSubject
+     * @return array
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function build(array $buildSubject)
+    {
+        $request['body'] = $this->adyenRequestsHelper->buildRedirectData([]);
+
+        return $request;
+    }
+}
