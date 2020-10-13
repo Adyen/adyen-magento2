04bc8e55f182d12e3246b13abf96fbcc2526aa04
acampos1916 <angel.campos@adyen.com>
2020-10-13 14:00:00 +0200

    [PW-2029] AdyenAmountCurrency model

---
+++ Model/AdyenAmountCurrency.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Payment module (https://www.adyen.com/)
+ *
+ * Copyright (c) 2020 Adyen BV (https://www.adyen.com/)
+ * See LICENSE.txt for license details.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+
+namespace Adyen\Payment\Model;
+
+class AdyenAmountCurrency
+{
+    protected $amount;
+
+    protected $currencyCode;
+
+    public function __construct($amount, $currencyCode)
+    {
+        $this->amount = $amount;
+        $this->currencyCode = $currencyCode;
+    }
+
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    public function getCurrencyCode()
+    {
+        return $this->currencyCode;
+    }
+}
