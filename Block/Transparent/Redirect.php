c4b4bfa4c31221be5d199938e854e482ddcbc4a9
alexandros <alexandros.moraitis@adyen.com>
2020-10-13 15:12:55 +0200

    Same site cookie changes

---
+++ Block/Transparent/Redirect.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Payment module (https://www.adyen.com/)
+ *
+ * Copyright (c) 2020 Adyen BV (https://www.adyen.com/)
+ * See LICENSE.txt for license details.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+
+namespace Adyen\Payment\Block\Transparent;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
+use Magento\Framework\Url;
+
+class Redirect extends Template
+{
+    /**
+     * Route path key to make redirect url.
+     */
+    const ROUTE_PATH = 'route_path';
+
+    /**
+     * @var UrlInterface
+     */
+    private $url;
+    /**
+     * @param Context $context
+     * @param UrlInterface $url
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        Url $url,
+        array $data = []
+    ) {
+        $this->url = $url;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * Returns url for redirect.
+     *
+     * @return string
+     * @since 100.3.5
+     */
+    public function getRedirectUrl()
+    {
+        return $this->url->getUrl($this->getData(self::ROUTE_PATH));
+    }
+
+    /**
+     * Returns params to be redirected.
+     *
+     * Encodes invalid UTF-8 values to UTF-8 to prevent character escape error.
+     * Some payment methods, send data in merchant defined language encoding
+     * which can be different from the system character encoding (UTF-8).
+     *
+     * @return array
+     * @since 100.3.5
+     */
+    public function getPostParams()
+    {
+        $params = [];
+        foreach ($this->_request->getPostValue() as $name => $value) {
+            if (!empty($value) && mb_detect_encoding($value, 'UTF-8', true) === false) {
+                $value = utf8_encode($value);
+            }
+            $params[$name] = $value;
+        }
+        return $params;
+    }
+
+
+}
---
+++ Controller/Transparent/Redirect.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Payment module (https://www.adyen.com/)
+ *
+ * Copyright (c) 2020 Adyen BV (https://www.adyen.com/)
+ * See LICENSE.txt for license details.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+
+namespace Adyen\Payment\Controller\Transparent;
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\HttpPostActionInterface;
+use Magento\Framework\App\CsrfAwareActionInterface;
+use Magento\Framework\App\Request\InvalidRequestException;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\View\Result\LayoutFactory;
+
+class Redirect extends Action implements CsrfAwareActionInterface, HttpPostActionInterface
+{
+    /**
+     * @var \Adyen\Payment\Logger\AdyenLogger
+     */
+    protected $_adyenLogger;
+
+    /**
+     * @var LayoutFactory
+     */
+    private $resultLayoutFactory;
+
+    /**
+     * Redirect constructor.
+     * @param \Adyen\Payment\Logger\AdyenLogger $_adyenLogger
+     * @param LayoutFactory $resultLayoutFactory
+     */
+    public function __construct(
+        \Adyen\Payment\Logger\AdyenLogger $_adyenLogger,
+        LayoutFactory $resultLayoutFactory)
+    {
+        $this->_adyenLogger = $_adyenLogger;
+        $this->resultLayoutFactory = $resultLayoutFactory;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function validateForCsrf(RequestInterface $request): ?bool
+    {
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function createCsrfValidationException(RequestInterface $request): ?InvalidRequestException
+    {
+        return null;
+    }
+
+    public function execute()
+    {
+        $gatewayResponse = (array)$this->getRequest()->getPostValue();
+        $this->_adyenLogger->addAdyenDebug(
+            ['Adyen 3DS1 redirect:' => $gatewayResponse]
+        );
+
+        $resultLayout = $this->resultLayoutFactory->create();
+        $resultLayout->addDefaultHandle();
+        $resultLayout->getLayout()->getUpdate()->load(['adyen_transparent_payment_redirect']);
+
+        return $resultLayout;
+    }
+}
