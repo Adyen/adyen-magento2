ee59cdc8878d5f3e6a4e80126c7df469c88d74ed
acampos1916 <angel.campos@adyen.com>
2020-12-28 13:21:55 +0100

    Adding charged currency hidden config field

---
+++ Model/Config/Source/ChargedCurrency.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Payment module (https://www.adyen.com/)
+ *
+ * Copyright (c) 2020 Adyen BV (https://www.adyen.com/)
+ * See LICENSE.txt for license details.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+
+namespace Adyen\Payment\Model\Config\Source;
+
+use Magento\Framework\Data\OptionSourceInterface;
+
+class ChargedCurrency implements OptionSourceInterface
+{
+    public function toOptionArray()
+    {
+        return array(
+            ['value' => 'display', 'label' => 'Display currency'],
+            ['value' => 'base', 'label' => 'Global/Website currency']
+        );
+    }
+}
