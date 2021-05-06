9de07df9abef83a4ff84468574ced438b7539971
alexandros <alexandros.moraitis@adyen.com>
2021-05-06 10:58:43 +0200

    Change the order of the checks

--- Helper/Requests.php
+++ Helper/Requests.php
@@ -202,17 +202,6 @@ class Requests extends AbstractHelper
                 $requestBilling["houseNumberOrName"] = $address["house_number"];
             }
 
-            if (!empty($billingAddress->getPostcode())) {
-                $requestBilling["postalCode"] = $billingAddress->getPostcode();
-                if ($billingAddress->getCountryId() == "BR") {
-                    $requestBilling["postalCode"] = preg_replace(
-                        '/[^\d]/',
-                        '',
-                        $billingAddress->getPostcode()
-                    );
-                }
-            }
-
             if (!empty($billingAddress->getCity())) {
                 $requestBilling["city"] = $billingAddress->getCity();
             }
@@ -223,6 +212,17 @@ class Requests extends AbstractHelper
 
             if (!empty($billingAddress->getCountryId())) {
                 $requestBilling["country"] = $billingAddress->getCountryId();
+            }
+
+            if (!empty($billingAddress->getPostcode())) {
+                $requestBilling["postalCode"] = $billingAddress->getPostcode();
+                if ($billingAddress->getCountryId() == "BR") {
+                    $requestBilling["postalCode"] = preg_replace(
+                        '/[^\d]/',
+                        '',
+                        $billingAddress->getPostcode()
+                    );
+                }
             }
 
             // If nothing is changed which means delivery address is not filled
@@ -259,17 +259,6 @@ class Requests extends AbstractHelper
                 $requestDelivery["houseNumberOrName"] = $address["house_number"];
             }
 
-            if (!empty($shippingAddress->getPostcode())) {
-                $requestDelivery["postalCode"] = $shippingAddress->getPostcode();
-                if ($billingAddress->getCountryId() == "BR") {
-                    $requestBilling["postalCode"] = preg_replace(
-                        '/[^\d]/',
-                        '',
-                        $billingAddress->getPostcode()
-                    );
-                }
-            }
-
             if (!empty($shippingAddress->getCity())) {
                 $requestDelivery["city"] = $shippingAddress->getCity();
             }
@@ -280,6 +269,17 @@ class Requests extends AbstractHelper
 
             if (!empty($shippingAddress->getCountryId())) {
                 $requestDelivery["country"] = $shippingAddress->getCountryId();
+            }
+
+            if (!empty($shippingAddress->getPostcode())) {
+                $requestDelivery["postalCode"] = $shippingAddress->getPostcode();
+                if ($shippingAddress->getCountryId() == "BR") {
+                    $requestDelivery["postalCode"] = preg_replace(
+                        '/[^\d]/',
+                        '',
+                        $shippingAddress->getPostcode()
+                    );
+                }
             }
 
             // If nothing is changed which means delivery address is not filled
