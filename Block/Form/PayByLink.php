72d54c10cf6e0b579e514f9c94825a06d2a4ad39
acampos1916 <angel.campos@adyen.com>
2021-06-30 10:56:02 +0200

    [PW-4159] Adjustments to PBL block class

--- Block/Form/PayByLink.php
+++ Block/Form/PayByLink.php
@@ -35,47 +35,49 @@ class PayByLink extends Form
 
     const MIN_EXPIRY_DAYS = 1;
     const MAX_EXPIRY_DAYS = 70;
-    const DAYS_TO_EXPIRE_CONFIG_PATH = "payment/adyen_pay_by_link/days_to_expire";
+    const DAYS_TO_EXPIRE_CONFIG_PATH = 'payment/adyen_pay_by_link/days_to_expire';
+    const DATE_FORMAT = 'd-m-Y';
 
     /**
      * @var string
      */
     protected $_template = 'Adyen_Payment::form/pay_by_link.phtml';
 
-    /**
-     * Get the minimum expiry date
-     * @throws NoSuchEntityException
-     * @throws \Exception
-     */
-    public function getDefaultExpiryDate(): int
+    public function getDefaultExpiryDate()
     {
-        $defaultExpiryDays = $this->_scopeConfig->getValue(
-            self::DAYS_TO_EXPIRE_CONFIG_PATH, ScopeInterface::SCOPE_STORE,
-            $this->_storeManager->getStore()->getId()
-        );
-        return $this->getNowPlusDays(2);
+        try {
+            $defaultExpiryDays = $this->_scopeConfig->getValue(
+                self::DAYS_TO_EXPIRE_CONFIG_PATH, ScopeInterface::SCOPE_STORE,
+                $this->_storeManager->getStore()->getId()
+            );
+        } catch (NoSuchEntityException $e) {
+            // There was a problem fetching the store, use the minimum expiry days as default
+            $defaultExpiryDays = self::MIN_EXPIRY_DAYS;
+        }
+        return $this->getNowPlusDays($defaultExpiryDays, false);
     }
 
-    /**
-     * @throws \Exception
-     */
     public function getMinExpiryDate(): int
     {
         return $this->getNowPlusDays(self::MIN_EXPIRY_DAYS);
     }
 
-    /**
-     * @throws \Exception
-     */
     public function getMaxExpiryDate(): int
     {
         return $this->getNowPlusDays(self::MAX_EXPIRY_DAYS);
     }
 
-    public function getNowPlusDays($days)
+    private function getNowPlusDays($days, $timestamp = true)
     {
         $date = new DateTime('now', new DateTimeZone('UTC'));
-        return $date->add(new DateInterval('P' . $days . 'D'))
-                ->getTimestamp() * 1000;
+        try {
+            $date->add(new DateInterval('P' . $days . 'D'));
+        } catch (\Exception $e) {
+            /*
+            Ignore exceptions and return original date, the validator will make sure that the selected
+            date is within the accepted range
+            */
+        }
+        return $timestamp ? $date->getTimestamp() * 1000 : $date->format(self::DATE_FORMAT);
     }
 }
