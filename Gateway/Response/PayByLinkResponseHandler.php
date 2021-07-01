3dbcecce70de2579727ce1b2c2e05f7ab1a7181e
acampos1916 <angel.campos@adyen.com>
2021-07-01 17:06:31 +0200

    [PW-4159] PBL response handler

---
+++ Gateway/Response/PayByLinkResponseHandler.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Payment module (https://www.adyen.com/)
+ *
+ * Copyright (c) 2021 Adyen NV (https://www.adyen.com/)
+ * See LICENSE.txt for license details.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+
+namespace Adyen\Payment\Gateway\Response;
+
+use Adyen\Payment\Helper\Data;
+use Magento\Payment\Gateway\Helper\SubjectReader;
+use Magento\Payment\Gateway\Response\HandlerInterface;
+
+class PayByLinkResponseHandler implements HandlerInterface
+{
+    /**
+     * @var Data
+     */
+    protected $adyenHelper;
+
+    public function __construct(
+        Data $adyenHelper
+    ) {
+        $this->adyenHelper = $adyenHelper;
+    }
+
+    /**
+     * This is being used for all checkout methods (adyen hpp payment method)
+     *
+     * @param array $handlingSubject
+     * @param array $response
+     */
+    public function handle(array $handlingSubject, array $response)
+    {
+        $paymentDataObject = SubjectReader::readPayment($handlingSubject);
+
+        $payment = $paymentDataObject->getPayment();
+
+        // set transaction not to processing by default wait for notification
+        $payment->setIsTransactionPending(true);
+
+        if (!empty($response['url'])) {
+            $payment->setAdditionalInformation('payByLink', $response['url']);
+        }
+
+        // do not close transaction so you can do a cancel() and void
+        $payment->setIsTransactionClosed(false);
+        $payment->setShouldCloseParentTransaction(false);
+    }
+}
