d54e8e06d44f7796a4da60711b489511403813c3
peterojo <peter.ojo@adyen.com>
2021-10-18 16:24:05 +0200

    [PW-5426] make donations request using php library

--- Gateway/Http/Client/TransactionDonate.php
+++ Gateway/Http/Client/TransactionDonate.php
@@ -24,18 +24,43 @@
 
 namespace Adyen\Payment\Gateway\Http\Client;
 
-
+use Adyen\AdyenException;
+use Adyen\Client;
+use Adyen\Payment\Helper\Data;
+use Adyen\Service\Checkout;
 use Magento\Payment\Gateway\Http\ClientInterface;
 use Magento\Payment\Gateway\Http\TransferInterface;
 
 class TransactionDonate implements ClientInterface
 {
+    /**
+     * @var Client
+     */
+    private $client;
+
+    /**
+     * @throws AdyenException
+     */
+    public function __construct(Data $adyenHelper)
+    {
+        $this->client = $adyenHelper->initializeAdyenClient();
+    }
 
     /**
      * @inheritDoc
+     * @throws AdyenException
      */
     public function placeRequest(TransferInterface $transferObject)
     {
-        // TODO: Implement placeRequest() method.
+        $request = $transferObject->getBody();
+        $service = new Checkout($this->client);
+
+        try {
+            $response = $service->donations($request);
+        } catch (AdyenException $e) {
+            $response['error'] = $e->getMessage();
+        }
+
+        return $response;
     }
 }
