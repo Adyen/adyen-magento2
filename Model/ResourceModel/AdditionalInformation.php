2ef7ebc5c2fbea8e047f89ff7e9527040772eab5
Titus Naber <titus.naber@adyen.com>
2021-12-01 13:07:42 +0100

    Added resource models file

---
+++ Model/ResourceModel/AdditionalInformation.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Payment module (https://www.adyen.com/)
+ *
+ * Copyright (c) 2021 Adyen BV (https://www.adyen.com/)
+ * See LICENSE.txt for license details.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+
+namespace Adyen\Payment\Model\ResourceModel;
+
+
+class AdditionalInformation extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
+{
+    /**
+     * Construct
+     */
+    public function _construct()
+    {
+        $this->_init('adyen_payment_additional_information', 'entity_id');
+    }
+
+}
