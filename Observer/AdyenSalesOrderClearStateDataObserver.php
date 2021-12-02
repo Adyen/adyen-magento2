635e39f7ff0eb088516e311e27a5649c2e48be75
acampos1916 <angel.campos@adyen.com>
2021-12-02 16:11:34 +0100

    [PW-5925] State data cleanup observer for quotes and sales orders

---
+++ Observer/AdyenSalesOrderClearStateDataObserver.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Payment Module
+ *
+ * Copyright (c) 2021 Adyen N.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+
+namespace Adyen\Payment\Observer;
+
+use Adyen\Payment\Logger\AdyenLogger;
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Quote\Api\CartRepositoryInterface;
+use Magento\Quote\Model\ResourceModel\Quote\Payment as QuotePaymentResourceModel;
+use Magento\Sales\Model\ResourceModel\Order\Payment as OrderPaymentResourceModel;
+
+class AdyenClearStateDataObserver implements ObserverInterface
+{
+
+    const ERROR_MSG = "State data was not cleaned-up: %s";
+    const STATE_DATA_KEY = "stateData";
+
+    /**
+     * @var CartRepositoryInterface
+     */
+    private $cartRepository;
+    /**
+     * @var QuotePaymentResourceModel
+     */
+    private $quotePaymentResourceModel;
+    /**
+     * @var OrderPaymentResourceModel
+     */
+    private $orderPaymentResourceModel;
+    /**
+     * @var AdyenLogger
+     */
+    private $adyenLogger;
+
+    public function __construct(
+        CartRepositoryInterface $cartRepository,
+        QuotePaymentResourceModel $quotePaymentResourceModel,
+        OrderPaymentResourceModel $orderPaymentResourceModel,
+        AdyenLogger $adyenLogger
+
+    ) {
+        $this->cartRepository = $cartRepository;
+        $this->quotePaymentResourceModel = $quotePaymentResourceModel;
+        $this->orderPaymentResourceModel = $orderPaymentResourceModel;
+        $this->adyenLogger = $adyenLogger;
+    }
+
+    public function execute(Observer $observer)
+    {
+        /** @var \Magento\Sales\Model\Order $order */
+        $order = $observer->getEvent()->getOrder();
+        $paymentMethod = $order->getPayment()->getMethod();
+        if (strpos($paymentMethod, 'adyen_') !== false) {
+
+            $quote = $this->cartRepository->get($order->getQuoteId());
+
+            try {
+                $quotePayment = $quote->getPayment();
+                $quotePayment->unsAdditionalInformation(self::STATE_DATA_KEY);
+                $this->quotePaymentResourceModel->save($quotePayment);
+            } catch (\Exception $exception) {
+                $this->adyenLogger->addError(__(self::ERROR_MSG, $exception->getMessage()));
+            }
+
+            try {
+                $orderPayment = $order->getPayment();
+                $orderPayment->unsAdditionalInformation(self::STATE_DATA_KEY);
+                $this->orderPaymentResourceModel->save($orderPayment);
+            } catch (\Exception $exception) {
+                $this->adyenLogger->addError(__(self::ERROR_MSG, $exception->getMessage()));
+            }
+        }
+    }
+}
