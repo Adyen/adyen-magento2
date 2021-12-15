93844a4cc7e03d8335703d2e58a8aa6180a0fe0b
Michael Paul <michael.paul@adyen.com>
2021-12-15 17:20:31 +0100

    [PW-6001] Use storePaymentMethodMode on Pay by Link

--- Helper/Requests.php
+++ Helper/Requests.php
@@ -356,9 +356,13 @@ class Requests extends AbstractHelper
         $enableOneclick = $this->adyenHelper->getAdyenAbstractConfigData('enable_oneclick', $storeId);
         $enableVault = $this->adyenHelper->isCreditCardVaultEnabled();
         $storedPaymentMethodsEnabled = $this->adyenHelper->getAdyenOneclickConfigData('active', $storeId);
-
         $stateData = $payment->getAdditionalInformation('stateData');
-        $request['storePaymentMethod'] = (bool)($stateData['storePaymentMethod'] ?? $storedPaymentMethodsEnabled);
+
+        if ('adyen_pay_by_link' === $payment->getMethod()) {
+            $request['storePaymentMethodMode'] = 'askForConsent';
+        } else {
+            $request['storePaymentMethod'] = (bool)($stateData['storePaymentMethod'] ?? $storedPaymentMethodsEnabled);
+        }
 
         //recurring
         if ($storedPaymentMethodsEnabled) {
