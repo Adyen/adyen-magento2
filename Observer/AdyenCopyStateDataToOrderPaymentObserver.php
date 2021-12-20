08e6c1e9539e95825d41d62db971bbecf4a6d8cb
acampos1916 <angel.campos@adyen.com>
2021-12-20 12:05:56 +0100

    New property for stateData in payment adapter

---
+++ Observer/AdyenCopyStateDataToOrderPaymentObserver.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Payment Module
+ *
+ * Copyright (c) 2021 Adyen N.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+
+namespace Adyen\Payment\Observer;
+
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Quote\Model\Quote;
+use Magento\Sales\Model\Order;
+
+class AdyenCopyStateDataToOrderPaymentObserver implements ObserverInterface
+{
+    public function execute(Observer $observer)
+    {
+        /** @var Order $order */
+        $order = $observer->getEvent()->getOrder();
+
+        /** @var Quote $quote */
+        $quote = $observer->getEvent()->getQuote();
+
+        $order->getPayment()->setAdditionalInformation(
+            'stateData',
+            $quote->getPayment()->getMethodInstance()->getStateData()
+        );
+
+    }
+}
