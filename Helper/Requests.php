5a2765c425b95a28314bb2de85be4b1a6ae801b7
Michael Paul <michael.paul@adyen.com>
2022-01-04 17:32:33 +0100

    [PW-6091] Do not send recurring parameters for PayPal

--- Helper/Requests.php
+++ Helper/Requests.php
@@ -24,6 +24,7 @@
 namespace Adyen\Payment\Helper;
 
 use Adyen\Payment\Model\Ui\AdyenPayByLinkConfigProvider;
+use Adyen\Payment\Observer\AdyenHppDataAssignObserver;
 use Adyen\Payment\Observer\AdyenOneclickDataAssignObserver;
 use Adyen\Util\Uuid;
 use Magento\Framework\App\Helper\AbstractHelper;
@@ -352,15 +353,18 @@ class Requests extends AbstractHelper
     }
 
     /**
-     * @param $request
-     * @param $areaCode
-     * @param $storeId
+     * @param int $storeId
      * @param $payment
+     * @return array
      */
-    public function buildRecurringData(int $storeId, $payment, $request = [])
+    public function buildRecurringData(int $storeId, $payment): array
     {
-        $enableOneclick = $this->adyenHelper->getAdyenAbstractConfigData('enable_oneclick', $storeId);
-        $enableVault = $this->adyenHelper->isCreditCardVaultEnabled();
+        $request = [];
+        // Recurring payments feature is not currently available for PayPal
+        if ($payment->getAdditionalInformation(AdyenHppDataAssignObserver::BRAND_CODE) === 'paypal') {
+            return $request;
+        }
+
         $storedPaymentMethodsEnabled = $this->adyenHelper->getAdyenOneclickConfigData('active', $storeId);
         // Initialize the request body with the current state data
         // Multishipping checkout uses the cc_number field for state data
@@ -375,14 +379,11 @@ class Requests extends AbstractHelper
 
         //recurring
         if ($storedPaymentMethodsEnabled) {
-            if ($enableVault) {
+            if ($this->adyenHelper->isCreditCardVaultEnabled()) {
                 $request['recurringProcessingModel'] = 'Subscription';
             } else {
-                if ($enableOneclick) {
-                    $request['recurringProcessingModel'] = 'CardOnFile';
-                } else {
-                    $request['recurringProcessingModel'] = 'Subscription';
-                }
+                $enableOneclick = $this->adyenHelper->getAdyenAbstractConfigData('enable_oneclick', $storeId);
+                $request['recurringProcessingModel'] = $enableOneclick ? 'CardOnFile' : 'Subscription';
             }
         }
 
