a025cb2e702d44f2425ecadeaeaab1e11bad7bda
cand <can.demiralp@adyen.com>
2022-03-23 17:02:14 +0100

    [PW-2568] - Removed unnecessary variable.

--- Ui/Component/Listing/Column/NotificationColumn.php
+++ Ui/Component/Listing/Column/NotificationColumn.php
@@ -108,7 +108,7 @@ class NotificationColumn extends \Magento\Ui\Component\Listing\Columns\Column
             if (!$item["done"] && $item["processing"]) {
                 $item["actions"] = sprintf(
                     '<a href="%s">%s</a>',
-                    $orderUrl = $this->backendHelper->getUrl("adyen/notifications/webhookReprocess", ["entity_id" => $item["entity_id"]]),
+                    $this->backendHelper->getUrl("adyen/notifications/webhookReprocess", ["entity_id" => $item["entity_id"]]),
                     "Reprocess Webhook"
                 );
             }
