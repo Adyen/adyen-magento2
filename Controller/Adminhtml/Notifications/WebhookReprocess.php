a5ddda114814e0fd6bd88beaf08b555018c46672
cand <can.demiralp@adyen.com>
2022-03-23 17:01:19 +0100

    [PW-2568] - Updated the year info on top of the files.

--- Controller/Adminhtml/Notifications/WebhookReprocess.php
+++ Controller/Adminhtml/Notifications/WebhookReprocess.php
@@ -15,7 +15,7 @@
  *
  * Adyen Payment module (https://www.adyen.com/)
  *
- * Copyright (c) 2020 Adyen BV (https://www.adyen.com/)
+ * Copyright (c) 2022 Adyen BV (https://www.adyen.com/)
  * See LICENSE.txt for license details.
  *
  * Author: Adyen <magento@adyen.com>
