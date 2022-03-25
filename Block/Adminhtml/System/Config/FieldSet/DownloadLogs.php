0fb55a65ea9379a102c1ffb6ab9c6ae527d1f6c4
cand <can.demiralp@adyen.com>
2022-03-25 15:25:39 +0100

    [PW-5620] - Download Logs button added under the Advanced Settings tab.

---
+++ Block/Adminhtml/System/Config/FieldSet/DownloadLogs.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Payment module (https://www.adyen.com/)
+ *
+ * Copyright (c) 2022 Adyen N.V. (https://www.adyen.com/)
+ * See LICENSE.txt for license details.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+
+namespace Adyen\Payment\Block\Adminhtml\System\Config\FieldSet;
+
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\View\Helper\SecureHtmlRenderer;
+
+class DownloadLogs extends \Magento\Config\Block\System\Config\Form\Fieldset
+{
+    /**
+     * @var \Magento\Config\Model\Config
+     */
+    protected $_backendConfig;
+
+    /**
+     * @var SecureHtmlRenderer
+     */
+    private $secureRenderer;
+
+    /**
+     * @param \Magento\Backend\Block\Context $context
+     * @param \Magento\Backend\Model\Auth\Session $authSession
+     * @param \Magento\Framework\View\Helper\Js $jsHelper
+     * @param \Magento\Config\Model\Config $backendConfig
+     * @param array $data
+     * @param SecureHtmlRenderer|null $secureRenderer
+     */
+    public function __construct(
+        \Magento\Backend\Block\Context $context,
+        \Magento\Backend\Model\Auth\Session $authSession,
+        \Magento\Framework\View\Helper\Js $jsHelper,
+        \Magento\Config\Model\Config $backendConfig,
+        array $data = [],
+        ?SecureHtmlRenderer $secureRenderer = null
+    )
+    {
+        $this->_backendConfig = $backendConfig;
+        $secureRenderer = $secureRenderer ?? ObjectManager::getInstance()->get(SecureHtmlRenderer::class);
+        parent::__construct($context, $authSession, $jsHelper, $data, $secureRenderer);
+        $this->secureRenderer = $secureRenderer;
+    }
+
+    /**
+     * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
+     * @return string
+     * @SuppressWarnings(PHPMD.NPathComplexity)
+     */
+    protected function _getHeaderTitleHtml($element)
+    {
+        $downloadUrl = "#";
+        $cssClasses = "adyen-logs-download-button";
+
+        $html = "
+            <div class='download-logs-container'>
+                <a href='$downloadUrl' class='$cssClasses'>Download Logs</a>
+            </div>
+        ";
+
+        return $html;
+    }
+
+    /**
+     *
+     * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
+     * @return string
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    protected function _getHeaderCommentHtml($element)
+    {
+        return '';
+    }
+
+    /**
+     * Get collapsed state on-load
+     *
+     * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
+     * @return false
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    protected function _isCollapseState($element)
+    {
+        return false;
+    }
+}
