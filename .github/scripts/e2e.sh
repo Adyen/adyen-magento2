ec3e22c18964685e5bb48b5e673a59340746976f
Michael Paul <michael@michaelpaul.com.br>
2022-03-30 16:06:17 +0000

    Add E2E step

---
+++ .github/scripts/e2e.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euo pipefail
+
+# Checkout E2E tests
+git clone https://github.com/Adyen/adyen-integration-tools-tests.git;
+cd adyen-integration-tools-tests;
+
+# Setup environment
+npm ci;
+npx playwright install --with-deps;
+
+# Run tests
+npx playwright test --project=chromium --config=./projects/magento/magento.config.cjs;
