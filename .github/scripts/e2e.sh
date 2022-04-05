ac268b8944e812e426615f6a51e4c809b39420c9
Michael Paul <michael.paul@adyen.com>
2022-04-05 13:45:18 +0000

    Add client key and tests branch

--- .github/scripts/e2e.sh
+++ .github/scripts/e2e.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 # Checkout E2E tests
 git clone https://github.com/Adyen/adyen-integration-tools-tests.git;
 cd adyen-integration-tools-tests;
+git checkout $INTEGRATION_TESTS_BRANCH;
 
 # Setup environment
 npm ci;
