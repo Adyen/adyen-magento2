965751cd0acbb75ceecdf08e3a233d1287baed67
Michael Paul <michael.paul@adyen.com>
2022-04-20 14:59:17 +0000

    Archive reports

--- .github/scripts/e2e.sh
+++ .github/scripts/e2e.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 # Checkout E2E tests
+cd /tmp;
 git clone https://github.com/Adyen/adyen-integration-tools-tests.git;
 cd adyen-integration-tools-tests;
 git checkout $INTEGRATION_TESTS_BRANCH;
