408c90c47f56eb107fed2944c62e46f1bedb6bea
jeant <jean.teuma@adyen.com>
2022-05-27 13:15:09 +0200

    PW-6737 - Also test the recurringProcessingModel

--- Test/Unit/Helper/RequestsTest.php
+++ Test/Unit/Helper/RequestsTest.php
@@ -8,49 +8,77 @@ use PHPUnit\Framework\TestCase;
 
 class RequestsTest extends TestCase
 {
+    /** @var Requests $sut */
     private $sut;
+
+    /** @var Payment $paymentMock */
     private $paymentMock;
 
-    public function testGuestNoStorePaymentMethod()
+    public function testBuildCardRecurringGuestNoStorePaymentMethod()
     {
-        $this->setMockObjects([]);
+        $this->setMockObjects([], false, '');
         $this->assertEmpty($this->sut->buildCardRecurringData(1, $this->paymentMock));
     }
 
-    public function testStorePaymentMethodFalse()
+    public function testBuildCardRecurringStorePaymentMethodFalse()
     {
-        $this->setMockObjects(['storePaymentMethod' => false]);
-        $storePaymentMethodValue = $this->sut->buildCardRecurringData(1, $this->paymentMock)['storePaymentMethod'];
+        $this->setMockObjects(['storePaymentMethod' => false], false, '');
+        $request = $this->sut->buildCardRecurringData(1, $this->paymentMock);
 
-        $this->assertFalse($storePaymentMethodValue);
+        $this->assertFalse($request['storePaymentMethod']);
     }
 
-    public function testStorePaymentMethodTrue()
+    public function testBuildCardRecurringStorePaymentMethodTrueVault()
     {
-        $this->setMockObjects(['storePaymentMethod' => true]);
-        $storePaymentMethodValue = $this->sut->buildCardRecurringData(1, $this->paymentMock)['storePaymentMethod'];
+        $this->setMockObjects(['storePaymentMethod' => true], true, '');
+        $request = $this->sut->buildCardRecurringData(1, $this->paymentMock);
 
-        $this->assertTrue($storePaymentMethodValue);
+        $this->assertTrue($request['storePaymentMethod']);
+        $this->assertEquals(Recurring::SUBSCRIPTION, $request['recurringProcessingModel']);
     }
 
-    private function setMockObjects(array $stateDataArray): void
+    public function testBuildCardRecurringStorePaymentMethodTrueAdyenCardOnFile()
+    {
+        $this->setMockObjects(['storePaymentMethod' => true], false, Recurring::CARD_ON_FILE);
+        $request = $this->sut->buildCardRecurringData(1, $this->paymentMock);
+
+        $this->assertTrue($request['storePaymentMethod']);
+        $this->assertEquals(Recurring::CARD_ON_FILE, $request['recurringProcessingModel']);
+    }
+
+    public function testBuildCardRecurringStorePaymentMethodTrueAdyenSubscription()
+    {
+        $this->setMockObjects(['storePaymentMethod' => true], false, Recurring::SUBSCRIPTION);
+        $request = $this->sut->buildCardRecurringData(1, $this->paymentMock);
+
+        $this->assertTrue($request['storePaymentMethod']);
+        $this->assertEquals(Recurring::SUBSCRIPTION, $request['recurringProcessingModel']);
+    }
+
+    private function setMockObjects(array $stateDataArray, bool $vaultEnabled, string $adyenTokenType): void
     {
         // Model\Order\Payment\Interceptor
         $stateDataMock = $this->createMock(StateData::class);
         $stateDataMock->method('getStateData')->willReturn($stateDataArray);
 
+        $vaultHelperMock = $this->createMock(Vault::class);
+        $vaultHelperMock->method('isCardVaultEnabled')->willReturn($vaultEnabled);
+
+        $configHelperMock = $this->createMock(Config::class);
+        $configHelperMock->method('getCardRecurringType')->willReturn($adyenTokenType);
+
+        $this->sut = new Requests(
+            $this->createMock(Data::class),
+            $configHelperMock,
+            $this->createMock(Address::class),
+            $stateDataMock,
+            $this->createMock(PaymentMethods::class),
+            $vaultHelperMock
+        );
+
         $orderMock = $this->createMock(Order::class);
         $orderMock->method('getQuoteId')->willReturn(1);
         $this->paymentMock = $this->createMock(Payment::class);
         $this->paymentMock->method('getOrder')->willReturn($orderMock);
-
-        $this->sut = new Requests(
-            $this->createMock(Data::class),
-            $this->createMock(Config::class),
-            $this->createMock(Address::class),
-            $stateDataMock,
-            $this->createMock(PaymentMethods::class),
-            $this->createMock(Vault::class)
-        );
     }
 }
