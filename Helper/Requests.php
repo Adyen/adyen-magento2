0a5dd15a26ece24dfa6c4eeb635a2295052535a5
jeant <jean.teuma@adyen.com>
2022-05-27 10:47:50 +0200

    PW-6737 - Update token creation to stop creating tokens for guest users

--- Helper/Requests.php
+++ Helper/Requests.php
@@ -380,16 +380,23 @@ class Requests extends AbstractHelper
     public function buildCardRecurringData(int $storeId, $payment)
     {
         $request = [];
+        $storePaymentMethod = false;
 
-        $storedPaymentMethodsEnabled = $this->adyenHelper->getAdyenOneclickConfigData('active', $storeId);
         // Initialize the request body with the current state data
         // Multishipping checkout uses the cc_number field for state data
         $stateData = $this->stateData->getStateData($payment->getOrder()->getQuoteId()) ?:
             (json_decode($payment->getCcNumber(), true) ?: []);
 
-        $request['storePaymentMethod'] = (bool)($stateData['storePaymentMethod'] ?? $storedPaymentMethodsEnabled);
+        // If PayByLink
+        // Else, if option to store token exists, get the value from the checkbox
+        if ($payment->getMethod() === AdyenPayByLinkConfigProvider::CODE) {
+            $request['storePaymentMethodMode'] = 'askForConsent';
+        } elseif (array_key_exists('storePaymentMethod', $stateData)) {
+            $storePaymentMethod = (bool)($stateData['storePaymentMethod']);
+            $request['storePaymentMethod'] = $storePaymentMethod;
+        }
 
-        if ($storedPaymentMethodsEnabled) {
+        if ($storePaymentMethod) {
             if ($this->vaultHelper->isCardVaultEnabled()) {
                 $request['recurringProcessingModel'] = 'Subscription';
             } else {
