210840e489442d91d2c1eff04aa4fe01a2c7f95b
RokPopov <rok.popovledinski@adyen.com>
2022-06-16 17:47:17 +0200

    add new model and xml file for custom cache type

---
+++ Model/Cache/Type/AdyenCache.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ *
+ * Adyen Payment Module
+ *
+ * Copyright (c) 2021 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+// phpcs:disable Generic.CodeAnalysis.UselessOverridingMethod.Found
+
+namespace Adyen\Payment\Model\Cache\Type;
+
+use Magento\Framework\App\Cache\Type\FrontendPool;
+use Magento\Framework\Cache\Frontend\Decorator\TagScope;
+
+class AdyenCache extends TagScope
+{
+    /**
+     * Cache type code unique among all cache types
+     */
+    const TYPE_IDENTIFIER = 'adyen';
+
+    /**
+     * The tag name that limits the cache cleaning scope within a particular tag
+     */
+    const CACHE_TAG = 'adyen-webhook';
+
+    /**
+     * Adyen cache constructor.
+     *:q
+     * @param FrontendPool
+     */
+    public $cacheFrontendPool;
+
+    public function __construct(
+        FrontendPool $cacheFrontendPool
+    ) {
+        parent::__construct(
+            $cacheFrontendPool->get(self::TYPE_IDENTIFIER),
+            self::CACHE_TAG
+        );
+    }
+}
