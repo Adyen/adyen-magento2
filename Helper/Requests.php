2056f8a410672112142a9efd9163bd3415269ea6
Michal Zwolenik <devetus@gmail.com>
2022-06-23 10:55:58 +0100

    Remove shopper reference private property

--- Helper/Requests.php
+++ Helper/Requests.php
@@ -51,8 +51,6 @@ class Requests extends AbstractHelper
      */
     private $vaultHelper;
 
-    private $shopperReference;
-
     /**
      * Requests constructor.
      *
@@ -443,13 +441,13 @@ class Requests extends AbstractHelper
     public function getShopperReference($customerId, $orderIncrementId): string
     {
         if ($customerId) {
-            $this->shopperReference = str_pad($customerId, 3, '0', STR_PAD_LEFT);
+            $shopperReference = str_pad($customerId, 3, '0', STR_PAD_LEFT);
         } else {
             $uuid = Uuid::generateV4();
             $guestCustomerId = $orderIncrementId . $uuid;
-            $this->shopperReference = $guestCustomerId;
+            $shopperReference = $guestCustomerId;
         }
 
-        return $this->shopperReference;
+        return $shopperReference;
     }
 }
