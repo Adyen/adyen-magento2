60e4e93d083b1c8789cbb60002241975a38839a3
jeant <jean.teuma@adyen.com>
2022-06-30 15:07:20 +0200

    PW-6790 - Remove RecurringPmVaultDataBuilder and use the normal vault data builder instead

--- Gateway/Request/RecurringVaultDataBuilder.php
+++ Gateway/Request/RecurringVaultDataBuilder.php
@@ -11,6 +11,7 @@
 
 namespace Adyen\Payment\Gateway\Request;
 
+use Adyen\Payment\Helper\Vault;
 use Magento\Payment\Gateway\Data\PaymentDataObject;
 use Magento\Payment\Gateway\Helper\SubjectReader;
 use Magento\Payment\Gateway\Request\BuilderInterface;
@@ -29,11 +30,15 @@ class RecurringVaultDataBuilder implements BuilderInterface
         $payment = $paymentDataObject->getPayment();
         $extensionAttributes = $payment->getExtensionAttributes();
         $paymentToken = $extensionAttributes->getVaultPaymentToken();
+        $details = json_decode($paymentToken->getTokenDetails() ?: '{}', true);
 
         // Add paymentMethod object in array, since currently checkout component is not loaded for vault payment methods
-        $requestBody['paymentMethod'] = [
-            'storedPaymentMethodId' => $paymentToken->getGatewayToken()
-        ];
+        $requestBody['paymentMethod'] = ['storedPaymentMethodId' => $paymentToken->getGatewayToken()];
+
+        // For now this will only be used by tokens created trough adyen_hpp payment methods
+        if (array_key_exists(Vault::TOKEN_TYPE, $details)) {
+            $requestBody['recurringProcessingModel'] = $details[Vault::TOKEN_TYPE];
+        }
 
         $request['body'] = $requestBody;
 
