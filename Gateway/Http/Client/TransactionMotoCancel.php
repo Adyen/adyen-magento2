0123b97165585b0179a79f8a21e924c9a135716a
Can Demiralp <ecandemiralp@gmail.com>
2022-07-19 16:13:27 +0200

    [PW-6706] - Add capture, cancel, refund and void commands for MOTO

---
+++ Gateway/Http/Client/TransactionMotoCancel.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ *
+ * Adyen Payment module (https://www.adyen.com/)
+ *
+ * Copyright (c) 2022 Adyen N.V. (https://www.adyen.com/)
+ * See LICENSE.txt for license details.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+
+namespace Adyen\Payment\Gateway\Http\Client;
+
+use Adyen\Service\Modification;
+use Magento\Payment\Gateway\Http\ClientInterface;
+
+/**
+ * Class TransactionSale
+ */
+class TransactionMotoCancel implements ClientInterface
+{
+    /**
+     * @var \Adyen\Payment\Helper\Data
+     */
+    private $adyenHelper;
+
+    /**
+     * PaymentRequest constructor.
+     * @param \Adyen\Payment\Helper\Data $adyenHelper
+     */
+    public function __construct(
+        \Adyen\Payment\Helper\Data $adyenHelper
+    ) {
+        $this->adyenHelper = $adyenHelper;
+    }
+
+    /**
+     * @param \Magento\Payment\Gateway\Http\TransferInterface $transferObject
+     * @return null
+     */
+    public function placeRequest(\Magento\Payment\Gateway\Http\TransferInterface $transferObject)
+    {
+        $request = $transferObject->getBody();
+
+        $client = $this->adyenHelper->initializeAdyenMotoClient($request['merchantAccount']);
+        $service = new Modification($client);
+
+        try {
+            $response = $service->cancel($request);
+        } catch (\Adyen\AdyenException $e) {
+            $response['error'] = $e->getMessage();
+        }
+        return $response;
+    }
+}
