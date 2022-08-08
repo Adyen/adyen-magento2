97e8b8da2367c61ef25e2a2ea8350a242649765e
RokPopov <rok.popovledinski@adyen.com>
2022-08-08 10:56:54 +0200

    change the way the popup modal x button is targeted

--- view/frontend/web/js/view/payment/method-renderer/adyen-cc-method.js
+++ view/frontend/web/js/view/payment/method-renderer/adyen-cc-method.js
@@ -309,10 +309,10 @@ define(
                 }
             },
             handleOnAdditionalDetails: function(result) {
+                $('.action-close').hide();
 
                 var self = this;
 
-                $(this.modalLabel + ' .action-close').hide();
                 var request = result.data;
                 request.orderId = self.orderId;
 
