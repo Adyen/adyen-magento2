258b7977ce9310acc5fa0ddfeecfa198d56ed732
RokPopov <rok.popovledinski@adyen.com>
2022-08-12 11:27:15 +0200

    update text

--- view/frontend/web/js/view/checkout/validator-mixin.js
+++ view/frontend/web/js/view/checkout/validator-mixin.js
@@ -18,7 +18,7 @@ define(['jquery'], function($) {
                 // The format is 000.000.000-00 (CPF) and 00.000.000/0000-00 (CNPJ)
                 return /(^\d{3}\.\d{3}\.\d{3}\-\d{2}$)|(^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$)/.test(value);
             },
-            $.mage.__('Please enter a valid brazilian social security number (Ex: 123.456.789-10).')
+            $.mage.__('Please enter a valid brazilian social security number (Ex: 123.456.789-10 or 12.345.678/1234-56).')
         )
 
         return target;
