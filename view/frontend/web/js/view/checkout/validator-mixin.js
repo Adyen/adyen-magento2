9f84ca1a693337ad770b59351a4eaec2088376f4
RokPopov <rok.popovledinski@adyen.com>
2022-08-12 11:19:46 +0200

    add CNPJ as accepted format for Boleto

--- view/frontend/web/js/view/checkout/validator-mixin.js
+++ view/frontend/web/js/view/checkout/validator-mixin.js
@@ -15,8 +15,8 @@ define(['jquery'], function($) {
         $.validator.addMethod(
             "validate-ssn-br",
             function(value, element) {
-                // The format is 000.000.000-00
-                return /^([-\.\s]?(\d{3})){3}[-\.\s]?(\d{2})$/.test(value);
+                // The format is 000.000.000-00 (CPF) and 00.000.000/0000-00 (CNPJ)
+                return /(^\d{3}\.\d{3}\.\d{3}\-\d{2}$)|(^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$)/.test(value);
             },
             $.mage.__('Please enter a valid brazilian social security number (Ex: 123.456.789-10).')
         )
