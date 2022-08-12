d18910f5ceea0d164217aab6997155c333bfc304
Rok Popov Ledinski <rok.popovledinski@adyen.com>
2022-08-12 13:49:43 +0200

    split regex for readability
    Co-authored-by: Michael Paul <michael.paul@adyen.com>

--- view/frontend/web/js/view/checkout/validator-mixin.js
+++ view/frontend/web/js/view/checkout/validator-mixin.js
@@ -15,8 +15,11 @@ define(['jquery'], function($) {
         $.validator.addMethod(
             "validate-ssn-br",
             function(value, element) {
-                // The format is 000.000.000-00 (CPF) and 00.000.000/0000-00 (CNPJ)
-                return /(^\d{3}\.\d{3}\.\d{3}\-\d{2}$)|(^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$)/.test(value);
+                // The CPF format is: 000.000.000-00
+                const cpf = /^\d{3}\.\d{3}\.\d{3}\-\d{2}$/.test(value);
+                // The CNPJ format is: 00.000.000/0000-00
+                const cnpj = /^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$/.test(value);
+                return cpf || cnpj;
             },
             $.mage.__('Please enter a valid brazilian social security number (Ex: 123.456.789-10 or 12.345.678/1234-56).')
         )
