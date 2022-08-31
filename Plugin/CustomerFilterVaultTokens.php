2c899e9291cf27b61b5b9e4bbe99bb7788246b76
RokPopov <rok.popovledinski@adyen.com>
2022-08-31 16:06:23 +0200

    add interceptor that returns filtered customer session tokens

---
+++ Plugin/CustomerFilterVaultTokens.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ *
+ * Adyen Payment module (https://www.adyen.com/)
+ *
+ * Copyright (c) 2022 Adyen NV (https://www.adyen.com/)
+ * See LICENSE.txt for license details.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+
+namespace Adyen\Payment\Plugin;
+
+use Magento\Vault\Model\CustomerTokenManagement;
+use Adyen\Payment\Helper\Recurring;
+
+class CustomerFilterVaultTokens
+{
+    /**
+     * Returns filtered list of payment tokens for current customer session
+     * @param CustomerTokenManagement $customerTokenManagement
+     * @param array $customerSessionTokens
+     * @return CustomerTokenManagement[]
+     */
+    public function afterGetCustomerSessionTokens(CustomerTokenManagement $customerTokenManagement, array $customerSessionTokens)
+    {
+        foreach($customerSessionTokens as $key => $token) {
+            if (strpos($token->getPaymentMethodCode(), 'adyen_') === 0) {
+                $tokenDetails = json_decode($token->getTokenDetails());
+                if ($tokenDetails->tokenType === Recurring::UNSCHEDULED_CARD_ON_FILE || $tokenDetails->tokenType === Recurring::SUBSCRIPTION) {
+                    // remove the element from the array
+                    unset($customerSessionTokens[$key]);
+                }
+            }
+        }
+
+        return $customerSessionTokens;
+    }
+}
