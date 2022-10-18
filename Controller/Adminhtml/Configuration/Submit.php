d36169c9e07b56240b868bfa6bedd40c62479b3d
alexandros <alexandros.moraitis@adyen.com>
2022-10-18 20:24:28 +0200

    Add the generic template

---
+++ Controller/Adminhtml/Configuration/Submit.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Adyen\Payment\Controller\Adminhtml\Configuration;
+
+
+use Magento\Backend\App\Action;
+use Magento\Framework\App\ResponseInterface;
+
+class Submit extends Action
+{
+
+    public function execute()
+    {
+        // TODO: Implement execute() method.
+        //this will do the transport builder
+    }
+}
