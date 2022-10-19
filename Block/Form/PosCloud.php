829936ae3a5901299aea6d7162e957fd28f0bc2e
Can Demiralp <ecandemiralp@gmail.com>
2022-10-19 17:03:48 +0200

    [PW-7265] - Implement installments and funding source selection for backend POS payments (#1768)
    * [PW-7265] - Implement installments and funding source selection
    
    * [PW-7265] - Use Config helper for POS configurations
    
    * [PW-7265] - Code formatting
    
    * [PW-7265] - Method arguments type definition

--- Block/Form/PosCloud.php
+++ Block/Form/PosCloud.php
@@ -12,7 +12,12 @@
 
 namespace Adyen\Payment\Block\Form;
 
+use Adyen\Payment\Helper\Config;
 use Adyen\Payment\Helper\ConnectedTerminals;
+use Adyen\Payment\Helper\Data;
+use Adyen\Payment\Helper\PointOfSale;
+use Magento\Backend\Model\Session\Quote;
+use Magento\Framework\Serialize\SerializerInterface;
 use Magento\Framework\View\Element\Template;
 use Magento\Payment\Block\Form;
 
@@ -29,17 +34,57 @@ class PosCloud extends Form
     protected $connectedTerminalsHelper;
 
     /**
+     * @var Data
+     */
+    protected $adyenHelper;
+
+    /**
+     * @var Config
+     */
+    protected $configHelper;
+
+    /**
+     * @var SerializerInterface
+     */
+    protected $serializer;
+
+    /**
+     * @var Quote
+     */
+    protected $backendSession;
+
+    /**
+     * @var PointOfSale
+     */
+    protected $posHelper;
+
+    /**
      * @param Template\Context $context
      * @param ConnectedTerminals $connectedTerminalsHelper
+     * @param SerializerInterface $serializer
+     * @param Data $adyenHelper
+     * @param Quote $backendSession
+     * @param PointOfSale $posHelper
+     * @param Config $configHelper
      * @param array $data
      */
     public function __construct(
         Template\Context $context,
         ConnectedTerminals $connectedTerminalsHelper,
+        SerializerInterface $serializer,
+        Data $adyenHelper,
+        Quote $backendSession,
+        PointOfSale $posHelper,
+        Config $configHelper,
         array $data = []
     ) {
         parent::__construct($context, $data);
         $this->connectedTerminalsHelper = $connectedTerminalsHelper;
+        $this->serializer = $serializer;
+        $this->adyenHelper = $adyenHelper;
+        $this->backendSession = $backendSession;
+        $this->posHelper = $posHelper;
+        $this->configHelper = $configHelper;
     }
 
     /**
@@ -57,5 +102,59 @@ class PosCloud extends Form
         }
 
         return [];
+    }
+
+    public function getFormattedInstallments(): array
+    {
+        $serialisedInstallments = $this->configHelper->getAdyenPosCloudConfigData('installments');
+        $formattedInstallments = [];
+
+        if (isset($serialisedInstallments)) {
+            $installments = $this->serializer->unserialize($serialisedInstallments);
+
+            $amount = $this->backendSession->getQuote()->getGrandTotal();
+            $currencyCode = $this->backendSession->getQuote()->getCurrency()->getQuoteCurrencyCode();
+            $precision = $this->adyenHelper->decimalNumbers($currencyCode);
+
+            $formattedInstallments = $this->posHelper->getFormattedInstallments(
+                $installments,
+                $amount,
+                $currencyCode,
+                $precision
+            );
+        }
+
+        return $formattedInstallments;
+    }
+
+    public function hasInstallment()
+    {
+        return $this->configHelper->getAdyenPosCloudConfigData('enable_installments');
+    }
+
+    public function hasFundingSource(): bool
+    {
+        if ($this->backendSession->getQuote()->getBillingAddress() === null) {
+            return false;
+        }
+
+        $countryId = $this->backendSession->getQuote()->getBillingAddress()->getCountryId();
+        $currencyCode = $this->backendSession->getQuote()->getQuoteCurrencyCode();
+
+        $allowedCurrenciesByCountry = [
+            'BR' => 'BRL',
+            'MX' => 'MXN'
+        ];
+
+        return isset($allowedCurrenciesByCountry[$countryId]) &&
+            $currencyCode === $allowedCurrenciesByCountry[$countryId];
+    }
+
+    public function getFundingSourceOptions(): array
+    {
+        return [
+            'credit' => 'Credit Card',
+            'debit' => 'Debit Card'
+        ];
     }
 }
