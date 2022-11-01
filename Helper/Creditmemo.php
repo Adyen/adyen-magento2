82be0271f7e3f5d13f806d4f723ef96fd81c15b0
RokPopov <rok.popovledinski@adyen.com>
2022-11-01 15:43:20 +0100

    remove code smell

--- Helper/Creditmemo.php
+++ Helper/Creditmemo.php
@@ -127,7 +127,8 @@ class Creditmemo extends AbstractHelper
      * @throws AlreadyExistsException
      */
     public function linkAndUpdateAdyenCreditmemos(
-        Payment $adyenOrderPayment, MagentoCreditmemoModel $magentoCreditmemo
+        Payment $adyenOrderPayment,
+        MagentoCreditmemoModel $magentoCreditmemo
     ): void {
         $adyenCreditmemoLoader = $this->adyenCreditmemoFactory->create();
 
