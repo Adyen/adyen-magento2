acc923f4a8b475ee6cbe48d73596b0a052bc2659
jeant <jean.teuma@adyen.com>
2022-11-09 16:55:40 +0100

    PW-7410 - Make config provider generic

---
+++ Model/Ui/AdyenPaymentMethodConfigProvider.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ *
+ * Adyen Payment module (https://www.adyen.com/)
+ *
+ * Copyright (c) 2022 Adyen BV (https://www.adyen.com/)
+ * See LICENSE.txt for license details.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+
+namespace Adyen\Payment\Model\Ui;
+
+use Magento\Checkout\Model\ConfigProviderInterface;
+use Magento\Framework\App\RequestInterface;
+
+class AdyenPaymentMethodConfigProvider implements ConfigProviderInterface
+{
+    const CODE_IDEAL = 'adyen_ideal';
+
+    /** @var RequestInterface  */
+    protected $request;
+
+    public function __construct(
+        RequestInterface $request
+    ) {
+        $this->request = $request;
+    }
+
+    public function getConfig(): array
+    {
+        return [];
+    }
+
+    protected function getRequest(): RequestInterface
+    {
+        return $this->request;
+    }
+}
