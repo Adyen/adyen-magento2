3cf4f182ba164715bb0d5fa138f11f87f2016428
alexandros <alexandros.moraitis@adyen.com>
2022-11-23 15:07:12 +0100

    Add buttons for the forms

--- Block/Adminhtml/Support/ConfigurationSettingsForm.php
+++ Block/Adminhtml/Support/ConfigurationSettingsForm.php
@@ -2,8 +2,123 @@
 
 namespace Adyen\Payment\Block\Adminhtml\Support;
 
-class ConfigurationSettingsForm extends \Magento\Backend\Block\Page
+use Adyen\Payment\Block\Adminhtml\Support\Edit\Tab\ConfigurationSettings;
+
+class ConfigurationSettingsForm extends \Magento\Backend\Block\Widget\Form\Generic
 {
+    const HEADLESS_YES = 1;
+    const HEADLESS_NO = 0;
+    /**
+     * Prepare form before rendering HTML
+     *
+     * @return $this
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    protected function _prepareForm()
+    {
+        $form = $this->_formFactory->create([
+            'data' => [
+                'id' => 'configurationsettings_form',
+                'action' => $this->getData('action'),
+                'method' => 'post'
+            ]
+        ]);
+
+        $fieldset = $form->addFieldset('base_fieldset', ['legend' => __('Configuration settings')]);
+        $this->_addElementTypes($fieldset);
+        $fieldset->addField(
+            'pspReference',
+            'text',
+            [
+                'name' => 'pspReferenceConfigurationSettings',
+                'label' => __('PSP Reference'),
+                'title' => __('PSP Reference'),
+                'class' => '',
+                'required' => true
+            ]
+        );
+        $fieldset->addField(
+            'merchantReference',
+            'text',
+            [
+                'name' => 'merchantReference',
+                'label' => __('Merchant Reference'),
+                'title' => __('Merchant Reference'),
+                'class' => '',
+                'required' => false,
+            ]
+        );
+        $fieldset->addField(
+            'headless',
+            'radios',
+            [
+                'name' => 'headless',
+                'label' => __('Are you using headless integration?'),
+                'title' => __('Are you using headless integration?'),
+                'class' => '',
+                'required' => false,
+                'values' => [
+                    ['value' => self::HEADLESS_YES, 'label' => __('Yes')],
+                    ['value' => self::HEADLESS_NO, 'label' => __('No')]
+                ]
+            ]
+        );
+        $fieldset->addField(
+            'terminalId',
+            'text',
+            [
+                'name' => 'terminalId',
+                'label' => __('Terminal ID number'),
+                'title' => __('Terminal ID number'),
+                'class' => '',
+                'required' => false,
+            ]
+        );
+        $fieldset->addField(
+            'logs',
+            'file',
+            [
+                'name' => 'logs',
+                'label' => __('Attach Logs'),
+                'title' => __('Attach Logs'),
+                'class' => '',
+                'required' => false,
+            ]
+        );
+        $fieldset->addField(
+            'orderHistoryComments',
+            'textarea',
+            [
+                'name' => 'orderHistoryComments',
+                'label' => __('Order History Comments'),
+                'title' => __('Order History Comments'),
+                'class' => '',
+                'required' => false,
+            ]
+        );
+        $fieldset->addField(
+            'upload_button',
+            'button',
+            [
+                'name' => 'submit',
+
+                'title' => __('click'),
+                'class' => 'primary',
+                'data_attribute' => '',
+                'value' => 'Submit',
+                'data_attribute' => [
+                    'mage-init' => ['button' => ['event' => 'send', 'target' => '#support_form']],
+                ]
+            ]
+        );
+
+
+        $this->setForm($form);
+        return parent::_prepareForm();
+    }
+
+
     public function getSupportTopics(): array
     {
         return [
--- Block/Adminhtml/Support/OrderProcessingForm.php
+++ Block/Adminhtml/Support/OrderProcessingForm.php
@@ -2,7 +2,126 @@
 
 namespace Adyen\Payment\Block\Adminhtml\Support;
 
-class OrderProcessingForm extends \Magento\Backend\Block\Page
+class OrderProcessingForm extends \Magento\Backend\Block\Widget\Form\Generic
 {
+    const HEADLESS_YES = 1;
+    const HEADLESS_NO = 0;
 
+    /**
+     * Prepare form before rendering HTML
+     *
+     * @return $this
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    protected function _prepareForm()
+    {
+        $form = $this->_formFactory->create([
+            'data' => [
+                'id' => 'support_form',
+                'action' => $this->getData('action'),
+                'method' => 'post',
+            ]
+        ]);
+        $fieldset = $form->addFieldset('base_fieldset', ['legend' => __('Order processing')]);
+        $this->_addElementTypes($fieldset);
+        $fieldset->addField(
+            'pspReference',
+            'text',
+            [
+                'name' => 'pspReference',
+                'label' => __('PSP Reference'),
+                'title' => __('PSP Reference'),
+                'class' => '',
+                'required' => true
+            ]
+        );
+        $fieldset->addField(
+            'merchantReference',
+            'text',
+            [
+                'name' => 'merchantReference',
+                'label' => __('Merchant Reference'),
+                'title' => __('Merchant Reference'),
+                'class' => '',
+                'required' => false,
+            ]
+        );
+        $fieldset->addField(
+            'headless',
+            'radios',
+            [
+                'name' => 'headless',
+                'label' => __('Are you using headless integration?'),
+                'title' => __('Are you using headless integration?'),
+                'class' => '',
+                'required' => false,
+                'values' => [
+                    ['value' => self::HEADLESS_YES, 'label' => __('Yes')],
+                    ['value' => self::HEADLESS_NO, 'label' => __('No')]
+                ]
+            ]
+        );
+        $fieldset->addField(
+            'paymentMethod',
+            'text',
+            [
+                'name' => 'paymentMethod',
+                'label' => __('What payment method is causing the problem?'),
+                'title' => __('What payment method is causing the problem?'),
+                'class' => '',
+                'required' => false,
+            ]
+        );
+        $fieldset->addField(
+            'terminalId',
+            'text',
+            [
+                'name' => 'terminalId',
+                'label' => __('Terminal ID number'),
+                'title' => __('Terminal ID number'),
+                'class' => '',
+                'required' => false,
+            ]
+        );
+        $fieldset->addField(
+            'logs',
+            'file',
+            [
+                'name' => 'logs',
+                'label' => __('Attach Logs'),
+                'title' => __('Attach Logs'),
+                'class' => '',
+                'required' => false,
+            ]
+        );
+        $fieldset->addField(
+            'orderHistoryComments',
+            'textarea',
+            [
+                'name' => 'orderHistoryComments',
+                'label' => __('Order history comments'),
+                'title' => __('Order history comments'),
+                'class' => '',
+                'required' => false,
+            ]
+        );
+
+        $fieldset->addField(
+            'upload_button',
+            'button',
+            [
+                'name' => 'submit',
+
+                'title' => __('click'),
+                'class' => 'upload_button',
+                'data_attribute' => '',
+                'value' => 'Submit'
+//                'onclick' => 'varienImport.validateImport();'
+            ]
+        );
+
+        $this->setForm($form);
+        return parent::_prepareForm();
+    }
 }
