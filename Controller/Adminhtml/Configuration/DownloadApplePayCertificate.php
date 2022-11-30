d0f4a11e9a29a3426a46a18106aeaf912073fb80
Raoul Ritter <raoulflorisritter@gmail.com>
2022-11-30 15:41:37 +0100

    FIX SOnar cloud Informaiton

--- Controller/Adminhtml/Configuration/DownloadApplePayCertificate.php
+++ Controller/Adminhtml/Configuration/DownloadApplePayCertificate.php
@@ -27,6 +27,12 @@ use Exception;
 
 class DownloadApplePayCertificate extends Action
 {
+    const READ_LENGTH = 2048;
+    const MAX_FILES = 10;
+    const MAX_SIZE = 1000000;
+    const MAX_RATIO = 5;
+    const FILE_NAME = 'apple-developer-merchantid-domain-association';
+
     /**
      * @var DirectoryList
      */
@@ -55,12 +61,13 @@ class DownloadApplePayCertificate extends Action
      * @param AdyenLogger $adyenLogger
      */
     public function __construct(
-        Context $context,
+        Context       $context,
         DirectoryList $directoryList,
-        Config $configHelper,
-        File $fileIo,
-        AdyenLogger $adyenLogger
-    ) {
+        Config        $configHelper,
+        File          $fileIo,
+        AdyenLogger   $adyenLogger
+    )
+    {
         parent::__construct($context);
         $this->directoryList = $directoryList;
         $this->configHelper = $configHelper;
@@ -80,10 +87,9 @@ class DownloadApplePayCertificate extends Action
 
         $pubPath = $this->directoryList->getPath('pub');
         $directoryName = '.well-known';
-        $filename = 'apple-developer-merchantid-domain-association';
 
         $wellknownPath = $pubPath . '/' . $directoryName;
-        $applepayPath = $wellknownPath . '/' . $filename;
+        $applepayPath = $wellknownPath . '/' . self::FILE_NAME;
 
         $applepayUrl = $this->configHelper->getApplePayUrlPath();
 
@@ -91,7 +97,7 @@ class DownloadApplePayCertificate extends Action
             if ($this->fileIo->checkAndCreateFolder($wellknownPath, 0700)) {
                 $this->downloadAndUnzip($applepayUrl, $wellknownPath);
             } else {
-                $this->fileIo->chmod($wellknownPath, 0700);
+                $this->fileIo->chmod($wellknownPath, 0770);
                 if (!$this->fileIo->fileExists($applepayPath)) {
                     $this->downloadAndUnzip($applepayUrl, $wellknownPath);
                 }
@@ -113,12 +119,60 @@ class DownloadApplePayCertificate extends Action
      */
     private function downloadAndUnzip(string $applepayUrl, string $applepayPath)
     {
-        $tmpPath = tempnam(sys_get_temp_dir(), 'apple-developer-merchantid-domain-association');
+        $tmpPath = tempnam(sys_get_temp_dir(), self::FILE_NAME);
         file_put_contents($tmpPath, file_get_contents($applepayUrl));
 
         $zip = new \ZipArchive;
-        $zip->open($tmpPath);
-        $zip->extractTo($applepayPath);
-        $zip->close();
+        $fileCount = 0;
+        $totalSize = 0;
+
+        if ($zip->open($tmpPath) === true) {
+            for ($i = 0; $i < $zip->numFiles; $i++) {
+                $filename = $zip->getNameIndex($i);
+                if(self::FILE_NAME !== $filename){
+                    continue;
+                }
+                $stats = $zip->statIndex($i);
+
+                // Prevent ZipSlip path traversal (S6096)
+                if (strpos($filename, '../') !== false || substr($filename, 0, 1) === '/') {
+                    throw new Exception('');
+                }
+
+                if (substr($filename, -1) !== '/') {
+                    $fileCount++;
+                    if ($fileCount > 10) {
+                        // Reached max. number of files
+                        throw new Exception('The zip file you are trying to expand has morefile than it should for this function');
+                    }
+
+                    $applepayCerticateFilestream = $zip->getStream($filename); // Compliant
+                    $currentSize = 0;
+                    while (!feof($applepayCerticateFilestream)) {
+                        $currentSize += self::READ_LENGTH;
+                        $totalSize += self::READ_LENGTH;
+
+                        if ($totalSize > self::MAX_SIZE) {
+                            // Reached max. size
+                            throw new Exception('The zip file you are trying to expand is much larger htan expected');
+                        }
+
+                        // Additional protection: check compression ratio
+                        if ($stats['comp_size'] > 0) {
+                            $ratio = $currentSize / $stats['comp_size'];
+                            if ($ratio > self::MAX_RATIO) {
+                                // Reached max. compression ratio
+                                throw new Exception('Maximum compression ratio reached. Something is might be wrong with your zip file.');
+                            }
+                        }
+                        file_put_contents($applepayPath .'/' . $filename, fread($applepayCerticateFilestream, $totalSize), FILE_APPEND);
+
+                    }
+
+                    fclose($applepayCerticateFilestream);
+                }
+            }
+            $zip->close();
+        }
     }
 }
