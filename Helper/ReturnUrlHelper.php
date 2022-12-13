3f5d97dc84f3b3326216d17731200d3f03e3c504
Can Demiralp <ecandemiralp@gmail.com>
2022-12-13 05:59:02 -0300

    [PW-6497] - Rename Json and Result controllers (#1865)
    * [PW-6497] - Rename the controllers and update helpers
    
    * [PW-6497] - Change redirect to return

--- Helper/ReturnUrlHelper.php
+++ Helper/ReturnUrlHelper.php
@@ -57,9 +57,9 @@ class ReturnUrlHelper
         }
         
         if ('adminhtml' === $this->state->getAreaCode()) {
-            return rtrim($this->backendHelper->getHomePageUrl(), '/') . '/adyen/process/result';
+            return rtrim($this->backendHelper->getHomePageUrl(), '/') . '/adyen/return';
         }
         
-        return rtrim($this->url->getBaseUrl(), '/') . '/adyen/process/result';
+        return rtrim($this->url->getBaseUrl(), '/') . '/adyen/return';
     }
 }
