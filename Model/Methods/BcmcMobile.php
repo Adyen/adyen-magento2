769634bf3a0a1b7d4b45b21508090208ab7d2695
Can Demiralp <ecandemiralp@gmail.com>
2022-12-30 15:25:30 +0100

    [PW-7523] Add PayConiq as an individual payment method

---
+++ Model/Methods/BcmcMobile.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ *
+ * Adyen Payment Module
+ *
+ * Copyright (c) 2022 Adyen N.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+namespace Adyen\Payment\Model\Methods;
+
+use Adyen\Payment\Helper\PaymentMethods\PaymentMethodInterface;
+use Adyen\Payment\Model\AdyenPaymentMethod;
+
+class BcmcMobile extends AdyenPaymentMethod implements PaymentMethodInterface
+{
+    const CODE = 'adyen_bcmc_mobile';
+    const TX_VARIANT = 'bcmc_mobile';
+    const NAME = 'PayConiq';
+
+    public function supportsRecurring(): bool
+    {
+        return true;
+    }
+
+    public function supportsManualCapture(): bool
+    {
+        return true;
+    }
+
+    public function supportsAutoCapture(): bool
+    {
+        return true;
+    }
+
+    public function supportsCardOnFile(): bool
+    {
+        return false;
+    }
+
+    public function supportsSubscription(): bool
+    {
+        return true;
+    }
+
+    public function supportsUnscheduledCardOnFile(): bool
+    {
+        return true;
+    }
+}
