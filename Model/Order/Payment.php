fac40b4e17b58ec9a833fd2b6b8056f5b680009f
jeant <jean.teuma@adyen.com>
2023-01-03 13:40:39 +0100

    PW-7235 - Type OrderPaymentInterface and PaymentResponseInterface and their implementations

--- Model/Order/Payment.php
+++ Model/Order/Payment.php
@@ -3,7 +3,7 @@
  *
  * Adyen Payment module (https://www.adyen.com/)
  *
- * Copyright (c) 2015 Adyen BV (https://www.adyen.com/)
+ * Copyright (c) 2023 Adyen BV (https://www.adyen.com/)
  * See LICENSE.txt for license details.
  *
  * Author: Adyen <magento@adyen.com>
@@ -13,23 +13,20 @@
 namespace Adyen\Payment\Model\Order;
 
 use Adyen\Payment\Api\Data\OrderPaymentInterface;
+use DateTime;
+use Magento\Framework\Data\Collection\AbstractDb;
+use Magento\Framework\Model\AbstractModel;
+use Magento\Framework\Model\Context;
+use Magento\Framework\Model\ResourceModel\AbstractResource;
+use Magento\Framework\Registry;
 
-class Payment extends \Magento\Framework\Model\AbstractModel implements OrderPaymentInterface
+class Payment extends AbstractModel implements OrderPaymentInterface
 {
-    /**
-     * Notification constructor.
-     *
-     * @param \Magento\Framework\Model\Context $context
-     * @param \Magento\Framework\Registry $registry
-     * @param \Magento\Framework\Model\ResourceModel\AbstractResource|null $resource
-     * @param \Magento\Framework\Data\Collection\AbstractDb|null $resourceCollection
-     * @param array $data
-     */
     public function __construct(
-        \Magento\Framework\Model\Context $context,
-        \Magento\Framework\Registry $registry,
-        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
-        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
+        Context $context,
+        Registry $registry,
+        AbstractResource $resource = null,
+        AbstractDb $resourceCollection = null,
         array $data = []
     ) {
         parent::__construct($context, $registry, $resource, $resourceCollection, $data);
@@ -45,172 +42,102 @@ class Payment extends \Magento\Framework\Model\AbstractModel implements OrderPay
         $this->_init(\Adyen\Payment\Model\ResourceModel\Order\Payment::class);
     }
 
-    /**
-     * @return mixed
-     */
-    public function getPspreference()
+    public function getPspreference(): string
     {
         return $this->getData(self::PSPREFRENCE);
     }
 
-    /**
-     * @param string $pspreference
-     * @return $this
-     */
-    public function setPspreference($pspreference)
+    public function setPspreference(string $pspReference): OrderPaymentInterface
     {
-        return $this->setData(self::PSPREFRENCE, $pspreference);
+        return $this->setData(self::PSPREFRENCE, $pspReference);
     }
 
-    /**
-     * @return mixed
-     */
-    public function getMerchantReference()
+    public function getMerchantReference(): string
     {
         return $this->getData(self::MERCHANT_REFERENCE);
     }
 
-    /**
-     * @param string $merchantReference
-     * @return $this
-     */
-    public function setMerchantReference($merchantReference)
+    public function setMerchantReference(string $merchantReference): OrderPaymentInterface
     {
         return $this->setData(self::MERCHANT_REFERENCE, $merchantReference);
     }
 
-    /**
-     * @return mixed
-     */
-    public function getPaymentId()
+    public function getPaymentId(): int
     {
         return $this->getData(self::PAYMENT_ID);
     }
 
-    /**
-     * @param string $paymentId
-     * @return $this
-     */
-    public function setPaymentId($paymentId)
+    public function setPaymentId(int $paymentId): OrderPaymentInterface
     {
         return $this->setData(self::PAYMENT_ID, $paymentId);
     }
 
-    /**
-     * @return string|null
-     */
-    public function getPaymentMethod()
+    public function getPaymentMethod(): string
     {
         return $this->getData(self::PAYMENT_METHOD);
     }
 
-    /**
-     * @param string $paymentMethod
-     * @return $this
-     */
-    public function setPaymentMethod($paymentMethod)
+    public function setPaymentMethod(string $paymentMethod): OrderPaymentInterface
     {
         return $this->setData(self::PAYMENT_METHOD, $paymentMethod);
     }
 
-    /**
-     * @return mixed
-     */
-    public function getAmount()
+    public function getAmount(): int
     {
         return $this->getData(self::AMOUNT);
     }
 
-    /**
-     * @param string $amount
-     * @return $this
-     */
-    public function setAmount($amount)
+    public function setAmount(int $amount): OrderPaymentInterface
     {
         return $this->setData(self::AMOUNT, $amount);
     }
 
-    /**
-     * @return mixed
-     */
-    public function getTotalRefunded()
+    public function getTotalRefunded(): int
     {
         return $this->getData(self::TOTAL_REFUNDED);
     }
 
-    /**
-     * @param string $totalRefunded
-     * @return $this
-     */
-    public function setTotalRefunded($totalRefunded)
+    public function setTotalRefunded(int $totalRefunded): OrderPaymentInterface
     {
         return $this->setData(self::TOTAL_REFUNDED, $totalRefunded);
     }
 
-    /**
-     * @return mixed
-     */
-    public function getCreatedAt()
+    public function getCreatedAt(): DateTime
     {
         return $this->getData(self::CREATED_AT);
     }
 
-    /**
-     * @param string $createdAt
-     * @return $this
-     */
-    public function setCreatedAt($createdAt)
+    public function setCreatedAt(DateTime $createdAt): OrderPaymentInterface
     {
         return $this->setData(self::CREATED_AT, $createdAt);
     }
 
-    /**
-     * @return mixed
-     */
-    public function getUpdatedAt()
+    public function getUpdatedAt(): DateTime
     {
         return $this->getData(self::UPDATED_AT);
     }
 
-    /**
-     * @param string $updatedAt
-     * @return $this
-     */
-    public function setUpdatedAt($updatedAt)
+    public function setUpdatedAt(DateTime $updatedAt): OrderPaymentInterface
     {
         return $this->setData(self::UPDATED_AT, $updatedAt);
     }
 
-    /**
-     * @param $captureStatus
-     * @return Payment
-     */
-    public function setCaptureStatus($captureStatus)
+    public function setCaptureStatus(string $captureStatus): OrderPaymentInterface
     {
         return $this->setData(self::CAPTURE_STATUS, $captureStatus);
     }
 
-    /**
-     * @return array|mixed|null
-     */
-    public function getCaptureStatus()
+    public function getCaptureStatus(): string
     {
         return $this->getData(self::CAPTURE_STATUS);
     }
 
-    /**
-     * @return int
-     */
-    public function getTotalCaptured()
+    public function getTotalCaptured(): ?int
     {
         return $this->getData(self::TOTAL_CAPTURED);
     }
 
-    /**
-     * @param $totalCaptured
-     * @return Payment
-     */
-    public function setTotalCaptured($totalCaptured)
+    public function setTotalCaptured(int $totalCaptured): OrderPaymentInterface
     {
         return $this->setData(self::TOTAL_CAPTURED, $totalCaptured);
     }
