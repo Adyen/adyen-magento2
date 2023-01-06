bf41e12931d14f1578b222a28a47894746e32332
Jeantwan Teuma <Morerice@users.noreply.github.com>
2023-01-06 09:23:39 +0100

    [PW-7235] - Add type declarations to interfaces in `Api/Data` and their implementations  (#1899)
    * PW-7235 - Type AddressAdapterInterface and InvoiceInterface and their implementations
    
    * PW-7235 - Type CreditMemoInterface and NotificationInterface and their implementations
    
    * PW-7235 - Add Serializer to webhook class using xml and fix small issues
    
    * PW-7235 - Type OrderPaymentInterface and PaymentResponseInterface and their implementations
    
    * PW-7235 - Type StateDataInterface and its implementations
    
    * PW-7235 - Fix acquirerReference issue and remove 'Magento constraints' comment
    
    * PW-7235 - Fix test by returning a payment method in the mocked notification

--- Api/Data/CreditmemoInterface.php
+++ Api/Data/CreditmemoInterface.php
@@ -12,6 +12,8 @@
 
 namespace Adyen\Payment\Api\Data;
 
+use DateTime;
+
 interface CreditmemoInterface
 {
     /**
@@ -29,111 +31,45 @@ interface CreditmemoInterface
     const UPDATED_AT = 'updated_at';
 
     /**
-     * Gets the ID for the creditmemo.
-     *
      * @return int|null Entity ID.
      */
-    public function getEntityId(): ?int;
+    public function getEntityId();
 
     /**
-     * Sets entity ID.
-     *
      * @param int $entityId
      * @return $this
      */
-    public function setEntityId($entityId): CreditmemoInterface;
+    public function setEntityId($entityId);
 
-    /**
-     * Gets the Pspreference for the creditmemo(capture)
-     *
-     * @return string|null Pspreference.
-     */
     public function getPspreference(): ?string;
 
-    /**
-     * Sets Pspreference.
-     *
-     * @param string $pspreference
-     * @return $this
-     */
-    public function setPspreference(string $pspreference): CreditmemoInterface;
+    public function setPspreference(string $pspReference): CreditmemoInterface;
 
-    /**
-     * @return string
-     */
-    public function getOriginalReference(): string;
+    public function getOriginalReference(): ?string;
 
-    /**
-     * @param  string $originalReference
-     * @return $this
-     */
     public function setOriginalReference(string $originalReference): CreditmemoInterface;
 
-    /**
-     * Gets the CreditmemoID for the creditmemo.
-     *
-     * @return int|null Creditmemo ID.
-     */
     public function getCreditmemoId(): ?int;
 
-    /**
-     * @param int $creditMemoId
-     * @return $this
-     */
-    public function setCreditmemoId(int $creditmemoId): CreditmemoInterface;
+    public function setCreditmemoId(int $creditMemoId): CreditmemoInterface;
 
-    /**
-     * @return int|null
-     */
     public function getAmount(): ?int;
 
-    /**
-     * @param int $amount
-     * @return $this
-     */
     public function setAmount(int $amount): CreditmemoInterface;
 
-    /**
-     * @return int|null
-     */
     public function getAdyenPaymentOrderId(): ?int;
 
-    /**
-     * @param int $id
-     * @return $this
-     */
     public function setAdyenPaymentOrderId(int $id): CreditmemoInterface;
 
-    /**
-     * @return string|null
-     */
     public function getStatus(): ?string;
 
-    /**
-     * @param string $status
-     * @return $this
-     */
     public function setStatus(string $status): CreditmemoInterface;
 
-    /**
-     * @return \DateTime
-     */
-    public function getCreatedAt(): \DateTime;
+    public function getCreatedAt(): ?DateTime;
 
-    /**
-     * @param \DateTime $createdAt
-     * @return $this
-     */
-    public function setCreatedAt(\DateTime $createdAt): CreditmemoInterface;
+    public function setCreatedAt(DateTime $createdAt): CreditmemoInterface;
 
-    /**
-     * @return \DateTime
-     */
-    public function getUpdatedAt(): \DateTime;
+    public function getUpdatedAt(): ?DateTime;
 
-    /**
-     * @param \DateTime $updatedAt
-     * @return $this
-     */
-    public function setUpdatedAt(\DateTime $updatedAt): CreditmemoInterface;
+    public function setUpdatedAt(DateTime $updatedAt): CreditmemoInterface;
 }
