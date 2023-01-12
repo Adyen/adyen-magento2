9a54897cccb7cacf72ef46500626ab5c728331b5
RokPopov <>
2023-01-12 09:44:05 +0100

    add test for markCaseAsPendingReview() when review is required

--- Test/Unit/Helper/CaseManagementTest.php
+++ Test/Unit/Helper/CaseManagementTest.php
@@ -14,6 +14,7 @@ namespace Adyen\Payment\Test\Unit\Helper;
 use Adyen\Payment\Helper\CaseManagement;
 use Adyen\Payment\Helper\Config;
 use Adyen\Payment\Logger\AdyenLogger;
+use Adyen\Payment\Model\ResourceModel\Order\Payment;
 use Adyen\Payment\Test\Unit\AbstractAdyenTestCase;
 use Magento\Framework\App\Helper\Context;
 use Magento\Sales\Model\Order;
@@ -21,7 +22,7 @@ use PHPUnit\Framework\TestCase;
 
 class CaseManagementTest extends AbstractAdyenTestCase
 {
-    public function testRequiresManualReviewTrue(): bool
+    public function testRequiresManualReviewTrue(): void
     {
         $additionalData = [CaseManagement::FRAUD_MANUAL_REVIEW => 'true'];
         $caseManagementHelper = $this->createCaseManagementHelper();
@@ -29,7 +30,7 @@ class CaseManagementTest extends AbstractAdyenTestCase
         $this->assertTrue($caseManagementHelper->requiresManualReview($additionalData));
     }
 
-    public function testRequiresManualReviewNoFraudKey(): bool
+    public function testRequiresManualReviewNoFraudKey(): void
     {
         $additionalData = ['test' => 'myPatience'];
         $caseManagementHelper = $this->createCaseManagementHelper();
@@ -37,7 +38,7 @@ class CaseManagementTest extends AbstractAdyenTestCase
         $this->assertFalse($caseManagementHelper->requiresManualReview($additionalData));
     }
 
-    public function testRequiresManualReviewUnexpectedValue(): bool
+    public function testRequiresManualReviewUnexpectedValue(): void
     {
         $additionalData = [CaseManagement::FRAUD_MANUAL_REVIEW => '1'];
         $caseManagementHelper = $this->createCaseManagementHelper();
@@ -45,10 +46,48 @@ class CaseManagementTest extends AbstractAdyenTestCase
         $this->assertFalse($caseManagementHelper->requiresManualReview($additionalData));
     }
 
-    public function testMarkCaseAsPendingReview(): Order
+    public function testMarkCaseAsPendingReviewWithReviewRequiredStatus(): void
     {
+        $pspReference = '87161728741214';
+        $reviewRequiredStatus = 'fraud_manual_review_status';
+        $expectedManualReviewComment = 'Manual review required for order w/pspReference: 87161728741214. Please check the Adyen platform.';
+        $order = $this->createMock(Order::class);
+        $payment = $this->createGeneratedMock(Payment::class, ['getData']);
+        $configHelper = $this->createMock(Config::class);
+        $logger = $this->createMock(AdyenLogger::class);
+        $storeId = 1;
 
+        $configHelper->method('getFraudStatus')
+            ->with(Config::XML_STATUS_FRAUD_MANUAL_REVIEW, $storeId)
+            ->willReturn($reviewRequiredStatus);
+
+        $order->method('getState')
+            ->willReturn(Order::STATE_NEW);
+        $order->method('getPayment')->willReturn($payment);
+        $payment->method('getData')->willReturn('entity_id');
+
+        $order->expects($this->once())
+            ->method('addStatusHistoryComment')
+            ->with($expectedManualReviewComment, $reviewRequiredStatus);
+        $order->expects($this->once())
+            ->method('getStoreId')
+            ->willReturn($storeId);
+        $logger->expects($this->once())
+            ->method('addAdyenNotification')
+            ->with(
+                $this->stringContains('Order'),
+                $this->logicalAnd(
+                    $this->arrayHasKey('pspReference'),
+                    $this->arrayHasKey('merchantReference')
+                )
+            );
+
+        $caseManagementHelper = $this->createCaseManagementHelper($logger, $configHelper);
+        $returnOrder = $caseManagementHelper->markCaseAsPendingReview($order, $pspReference);
+        $this->assertSame($order, $returnOrder);
     }
+
+
 
     /**
      * @param $adyenLoggerMock
