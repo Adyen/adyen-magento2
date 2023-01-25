972ae6d1a01012d30e77b108525e2d55456b3f7f
Jeantwan Teuma <Morerice@users.noreply.github.com>
2023-01-25 11:23:56 +0100

    [PW-7778] - Implement GooglePay as an individual payment method (#1929)
    * PW-7778 - Add extra configuration to Paypal config
    
    * PW-7778 - Add files related to googlepay
    
    * PW-7778 - Fix onClick function

---
+++ Model/Methods/GooglePay.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ *
+ * Adyen Payment Module
+ *
+ * Copyright (c) 2023 Adyen N.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+namespace Adyen\Payment\Model\Methods;
+
+use Adyen\Payment\Helper\PaymentMethods\PaymentMethodInterface;
+use Adyen\Payment\Model\AdyenPaymentMethod;
+
+class GooglePay extends AdyenPaymentMethod implements PaymentMethodInterface
+{
+    const CODE = 'adyen_googlepay';
+    const TX_VARIANT = 'googlepay';
+    const NAME = 'Google Pay';
+
+    public function supportsRecurring(): bool
+    {
+        return true;
+    }
+
+    public function supportsManualCapture(): bool
+    {
+        return true;
+    }
+
+    public function supportsAutoCapture(): bool
+    {
+        return true;
+    }
+
+    public function supportsCardOnFile(): bool
+    {
+        return true;
+    }
+
+    public function supportsSubscription(): bool
+    {
+        return true;
+    }
+
+    public function supportsUnscheduledCardOnFile(): bool
+    {
+        return true;
+    }
+}
