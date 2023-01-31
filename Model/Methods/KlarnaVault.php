a946a5fb19d36ef458236df214c1dbd0b716a4bc
jeant <jean.teuma@adyen.com>
2023-01-31 15:00:21 +0100

    PW-7975 - Modify files to process payment using Klarna token

--- Model/Methods/KlarnaVault.php
+++ Model/Methods/KlarnaVault.php
@@ -7,4 +7,13 @@ use Magento\Vault\Model\Method\Vault;
 class KlarnaVault extends Vault
 {
     const CODE = 'adyen_klarna_vault';
+
+    /**
+     * @inheritdoc
+     * @since 100.1.0
+     */
+    public function isInitializeNeeded()
+    {
+        return false;
+    }
 }
