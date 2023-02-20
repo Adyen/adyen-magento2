797e870956cfbc44097f2928ee16112102fee693
jeant <jean.teuma@adyen.com>
2023-02-20 14:07:12 +0100

    PW-8009 - Be able to display the checkout component using hard-coded values

--- view/frontend/web/js/view/payment/method-renderer/vault.js
+++ view/frontend/web/js/view/payment/method-renderer/vault.js
@@ -11,16 +11,69 @@
 /*global define*/
 define([
     'jquery',
-    'Magento_Vault/js/view/payment/method-renderer/vault'
+    'ko',
+    'Magento_Vault/js/view/payment/method-renderer/vault',
+    'Adyen_Payment/js/model/adyen-payment-service',
+    'Adyen_Payment/js/model/adyen-configuration',
+    'Adyen_Payment/js/model/adyen-checkout'
 ], function (
     $,
-    VaultComponent
+    ko,
+    VaultComponent,
+    adyenPaymentService,
+    adyenConfiguration,
+    adyenCheckout
 ) {
     'use strict';
 
     return VaultComponent.extend({
         defaults: {
-            template: 'Adyen_Payment/payment/card-vault-form.html'
+            template: 'Adyen_Payment/payment/card-vault-form.html',
+            checkoutComponentBuilt: false
+        },
+
+        /**
+         * @returns {exports.initObservable}
+         */
+        initObservable: function () {
+            this._super()
+                .observe([
+                    'checkoutComponentBuilt'
+                ]);
+
+            return this;
+        },
+
+        initialize: function () {
+            let self = this;
+            this._super();
+            let paymentMethodsObserver = adyenPaymentService.getPaymentMethods();
+            paymentMethodsObserver.subscribe(
+                function (paymentMethodsResponse) {
+                    self.loadCheckoutComponent(paymentMethodsResponse)
+                });
+            self.loadCheckoutComponent(paymentMethodsObserver());
+
+            return this;
+        },
+
+        loadCheckoutComponent: async function(paymentMethodsResponse) {
+            this.checkoutComponent = await adyenCheckout.buildCheckoutComponent(
+                paymentMethodsResponse,
+                this.handleOnAdditionalDetails.bind(this)
+            );
+
+            if (this.checkoutComponent) {
+                this.checkoutComponentBuilt(true)
+            }
+        },
+
+        handleOnAdditionalDetails: function (result) {
+            var self = this;
+            var request = result.data;
+            request.orderId = self.orderId;
+            debugger;
+            console.log('Hello in handleOnAdditionalDetails');
         },
 
         /**
@@ -58,6 +111,37 @@ define([
          */
         getIcons: function (type) {
             return this.details.icon;
+        },
+        getClientKey: function () {
+            return adyenConfiguration.getClientKey();
+        },
+        renderCardVaultToken: function () {
+            let self = this;
+            debugger;
+            console.log('Rendering card vault token')
+            if (!this.getClientKey()) {
+                return false
+            }
+
+            let componentConfig = {
+                hideCVC: false,
+                brand: 'visa',
+                storedPaymentMethodId: '8416565937545766',
+                expiryMonth: '03',
+                expiryYear: '30',
+                holderName: 'First tester',
+                //onChange: this.handleOnChange.bind(this)
+            }
+
+            self.component = adyenCheckout.mountPaymentMethodComponent(
+                this.checkoutComponent,
+                'card',
+                componentConfig,
+                '#cvcContainer-card_vault_1'
+            )
+            this.component = self.component
+
+            return true
         }
     });
 });
