a68002b333c52f9059ab8e0fe20ffa0146f40838
skeutmeier <73367887+skeutmeier@users.noreply.github.com>
2023-02-27 15:41:03 +0100

    Update Helper/Data.php
    Co-authored-by: Peter Ojo <peterojo@users.noreply.github.com>

--- Helper/Data.php
+++ Helper/Data.php
@@ -372,7 +372,10 @@ class Data extends AbstractHelper
      */
     public function formatAmount($amount, $currency)
     {
-        if ($amount == null) {$amount = 0};	// PHP 8 does not accept first param to be NULL
+        if ($amount === null) {
+            // PHP 8 does not accept first param to be NULL
+            $amount = 0;
+        }
         return (int)number_format($amount, $this->decimalNumbers($currency), '', '');
     }
 
