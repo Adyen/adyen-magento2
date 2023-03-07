cfd688a0af20643eedd4b4817ea6283093cc7847
jeant <jean.teuma@adyen.com>
2023-03-07 15:53:18 +0100

    PW-8231 - Remove chainCalls

--- Observer/AdyenPosCloudDataAssignObserver.php
+++ Observer/AdyenPosCloudDataAssignObserver.php
@@ -3,7 +3,7 @@
  *
  * Adyen Payment module (https://www.adyen.com/)
  *
- * Copyright (c) 2019 Adyen BV (https://www.adyen.com/)
+ * Copyright (c) 2023 Adyen BV (https://www.adyen.com/)
  * See LICENSE.txt for license details.
  *
  * Author: Adyen <magento@adyen.com>
@@ -19,16 +19,11 @@ class AdyenPosCloudDataAssignObserver extends AbstractDataAssignObserver
 {
     const TERMINAL_ID = 'terminal_id';
     const NUMBER_OF_INSTALLMENTS = 'number_of_installments';
-    const CHAIN_CALLS = 'chain_calls';
     const FUNDING_SOURCE = 'funding_source';
 
-    /**
-     * @var array
-     */
-    protected $additionalInformationList = [
+    protected array $additionalInformationList = [
         self::TERMINAL_ID,
         self::NUMBER_OF_INSTALLMENTS,
-        self::CHAIN_CALLS,
         self::FUNDING_SOURCE
     ];
 
