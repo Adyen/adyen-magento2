0559045650c74de1b255551abc0fd643fc7921e2
Can Demiralp <ecandemiralp@gmail.com>
2023-03-10 11:19:04 +0100

    [PW-8242] Mount action component on card vault

--- Gateway/Request/RecurringVaultDataBuilder.php
+++ Gateway/Request/RecurringVaultDataBuilder.php
@@ -11,6 +11,7 @@
 
 namespace Adyen\Payment\Gateway\Request;
 
+use Adyen\Payment\Helper\StateData;
 use Adyen\Payment\Helper\Vault;
 use Magento\Payment\Gateway\Data\PaymentDataObject;
 use Magento\Payment\Gateway\Helper\SubjectReader;
@@ -18,6 +19,16 @@ use Magento\Payment\Gateway\Request\BuilderInterface;
 
 class RecurringVaultDataBuilder implements BuilderInterface
 {
+    /**
+     * @var StateData
+     */
+    private $stateData;
+
+    public function __construct(StateData $stateData)
+    {
+        $this->stateData = $stateData;
+    }
+
     /**
      * @param array $buildSubject
      * @return array
@@ -28,12 +39,13 @@ class RecurringVaultDataBuilder implements BuilderInterface
         /** @var PaymentDataObject $paymentDataObject */
         $paymentDataObject = SubjectReader::readPayment($buildSubject);
         $payment = $paymentDataObject->getPayment();
+        $order = $paymentDataObject->getOrder();
         $extensionAttributes = $payment->getExtensionAttributes();
         $paymentToken = $extensionAttributes->getVaultPaymentToken();
         $details = json_decode($paymentToken->getTokenDetails() ?: '{}', true);
 
-        // Add paymentMethod object in array, since currently checkout component is not loaded for vault payment methods
-        $requestBody['paymentMethod'] = ['storedPaymentMethodId' => $paymentToken->getGatewayToken()];
+        // Initialize the request body with the current state data
+        $requestBody = $this->stateData->getStateData($order->getQuoteId());
 
         // For now this will only be used by tokens created trough adyen_hpp payment methods
         if (array_key_exists(Vault::TOKEN_TYPE, $details)) {
--- view/frontend/web/js/view/payment/method-renderer/vault.js
+++ view/frontend/web/js/view/payment/method-renderer/vault.js
@@ -11,50 +11,242 @@
 /*global define*/
 define([
     'jquery',
-    'Magento_Vault/js/view/payment/method-renderer/vault'
-], function ($, VaultComponent) {
+    'ko',
+    'uiLayout',
+    'mage/url',
+    'Magento_Checkout/js/action/place-order',
+    'Magento_Checkout/js/model/full-screen-loader',
+    'Magento_Ui/js/model/messages',
+    'Magento_Vault/js/view/payment/method-renderer/vault',
+    'Adyen_Payment/js/model/adyen-payment-service',
+    'Adyen_Payment/js/model/adyen-configuration',
+    'Adyen_Payment/js/model/adyen-checkout',
+    'Adyen_Payment/js/model/adyen-payment-modal'
+], function (
+    $,
+    ko,
+    layout,
+    url,
+    placeOrderAction,
+    fullScreenLoader,
+    Messages,
+    VaultComponent,
+    adyenPaymentService,
+    adyenConfiguration,
+    adyenCheckout,
+    adyenPaymentModal
+) {
     'use strict';
+
+    let isValidObserver = ko.observable(false);
+    let validTokens = {};
 
     return VaultComponent.extend({
         defaults: {
-            template: 'Magento_Vault/payment/form'
+            template: 'Adyen_Payment/payment/card-vault-form.html',
+            checkoutComponentBuilt: false,
+            modalLabel: 'card_action_modal'
         },
 
-        /**
-         * Get last 4 digits of card
-         * @returns {String}
-         */
+        initObservable: function () {
+            this._super()
+                .observe([
+                    'checkoutComponentBuilt'
+                ]);
+
+            return this;
+        },
+
+        initialize: function () {
+            let self = this;
+            this._super();
+            let paymentMethodsObserver = adyenPaymentService.getPaymentMethods();
+            paymentMethodsObserver.subscribe(
+                function (paymentMethodsResponse) {
+                    self.loadCheckoutComponent(paymentMethodsResponse)
+                });
+            self.loadCheckoutComponent(paymentMethodsObserver());
+
+            return this;
+        },
+
+        loadCheckoutComponent: async function(paymentMethodsResponse) {
+            this.checkoutComponent = await adyenCheckout.buildCheckoutComponent(
+                paymentMethodsResponse,
+                this.handleOnAdditionalDetails.bind(this)
+            );
+
+            if (this.checkoutComponent) {
+                this.checkoutComponentBuilt(true)
+            }
+        },
+
+        handleOnAdditionalDetails: function (result) {
+            var self = this;
+            var request = result.data;
+            request.orderId = self.orderId;
+            console.log('Hello in handleOnAdditionalDetails');
+        },
+
+        renderCardVaultToken: function () {
+            let self = this;
+            if (!this.getClientKey()) {
+                return false
+            }
+
+            let componentConfig = {
+                hideCVC: false,
+                brand: this.getCardType(),
+                storedPaymentMethodId: this.getGatewayToken(),
+                expiryMonth: this.getExpirationMonth(),
+                expiryYear: this.getExpirationYear(),
+                //holderName: 'First tester',
+                onChange: this.handleOnChange.bind(this)
+            }
+
+            self.component = adyenCheckout.mountPaymentMethodComponent(
+                this.checkoutComponent,
+                'card',
+                componentConfig,
+                '#cvcContainer-' + this.getId()
+            )
+            this.component = self.component
+
+            return true
+        },
+
+        handleOnChange: function (state, component) {
+            validTokens[this.getId()] = !!state.isValid;
+            isValidObserver(validTokens)
+        },
+
+        isButtonActive: function () {
+            return this.isActive() && this.isPlaceOrderActionAllowed() && isValidObserver()[this.getId()];
+        },
+
+        placeOrder: function (data, event) {
+            let self = this;
+
+            if (event) {
+                event.preventDefault();
+            }
+
+            if (this.validate() && this.isPlaceOrderActionAllowed() === true) {
+                fullScreenLoader.startLoader();
+                this.isPlaceOrderActionAllowed(false);
+                this.getPlaceOrderDeferredObject().done(
+                    function (orderId) {
+                        adyenPaymentService.getOrderPaymentStatus(orderId).done(function (responseJSON) {
+                            self.handleAdyenResult(responseJSON, orderId);
+                        });
+                    },
+                ).fail(
+                    function () {
+                        fullScreenLoader.stopLoader();
+                        self.isPlaceOrderActionAllowed(true);
+                    }
+                );
+
+                return true;
+            }
+
+            return false;
+        },
+
+        getData: function () {
+            const self = this;
+            let stateData = self.component.data;
+            stateData = JSON.stringify(stateData);
+            window.sessionStorage.setItem('adyen.stateData', stateData);
+            return {
+                method: this.code,
+                additional_data: {
+                    stateData: stateData,
+                    public_hash: this.publicHash
+                },
+            };
+        },
+
+        handleAdyenResult: function (responseJSON, orderId) {
+            let self = this;
+            const response = JSON.parse(responseJSON);
+
+            if (!!response.isFinal) {
+                // Status is final redirect to the success page
+                window.location.replace(url.build(this.successPage));
+            } else {
+                self.handleAction(response.action, orderId);
+            }
+        },
+
+        handleAction: function (action, orderId) {
+            let self = this;
+            let popupModal;
+
+            if (action.type === 'threeDS2' || action.type === 'await') {
+                this.modalLabel = 'card_action_modal'
+                popupModal = self.showModal();
+            }
+            try {
+                // Determine threeDS2 modal size, based on screen width
+                const threeDSConfiguration = {
+                    challengeWindowSize: screen.width < 460 ? '01' : '02'
+                }
+
+                this.checkoutComponent.createFromAction(action, threeDSConfiguration).mount(
+                    '#' + this.modalLabel + '_content'
+                );
+            } catch (e) {
+                console.log(e);
+                self.closeModal(popupModal);
+            }
+        },
+
+        showModal: function() {
+            let actionModal = adyenPaymentModal.showModal(adyenPaymentService, fullScreenLoader, this.messageContainer, this.orderId, this.modalLabel, this.isPlaceOrderActionAllowed)
+            $("." + this.modalLabel + " .action-close").hide();
+
+            return actionModal;
+        },
+
         getMaskedCard: function () {
             return this.details.maskedCC;
         },
 
-        /**
-         * Get expiration date
-         * @returns {String}
-         */
         getExpirationDate: function () {
             return this.details.expirationDate;
         },
 
-        /**
-         * Get card type
-         * @returns {String}
-         */
+        getExpirationMonth: function () {
+            return this.getExpirationDate().split('/')[0].padStart(2, '0');
+        },
+
+        getExpirationYear: function () {
+            return this.getExpirationDate().split('/')[1]
+        },
+
         getCardType: function () {
             return this.details.type;
         },
-        /**
-         * @returns {String}
-         */
+
         getToken: function () {
             return this.publicHash;
         },
-        /**
-         * @param {String} type
-         * @returns {Boolean}
-         */
+
+        getGatewayToken: function () {
+            return this.gatewayToken;
+        },
+
         getIcons: function (type) {
             return this.details.icon;
+        },
+
+        getClientKey: function () {
+            return adyenConfiguration.getClientKey();
+        },
+
+        getPlaceOrderDeferredObject: function () {
+            return $.when(placeOrderAction(this.getData()));
         }
     });
 });
