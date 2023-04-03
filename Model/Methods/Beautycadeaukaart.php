ecd333ee26f8e2b7380d57e6ae12038c0e583158
Can Demiralp <ecandemiralp@gmail.com>
2023-04-03 14:54:13 +0200

    [PW-8248] Auto generate beautycadeaukaart payment method

---
+++ Model/Methods/Beautycadeaukaart.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Adyen Payment Module
+ *
+ * Copyright (c) 2023 Adyen N.V  .
+ */
+
+namespace Adyen\Payment\Model\Methods;
+
+use Adyen\Payment\Model\AdyenPaymentMethod;
+use Adyen\Payment\Model\Method\PaymentMethodInterface;
+
+class Beautycadeaukaart extends AdyenPaymentMethod implements PaymentMethodInterface
+{
+	public const CODE = 'adyen_beautycadeaukaart';
+	public const TX_VARIANT = 'beautycadeaukaart';
+	public const NAME = 'Beauty Giftcard';
+
+	public function supportsRecurring(): bool
+	{
+		return false;
+	}
+
+
+	public function supportsManualCapture(): bool
+	{
+		return false;
+	}
+
+
+	public function supportsAutoCapture(): bool
+	{
+		return false;
+	}
+
+
+	public function supportsCardOnFile(): bool
+	{
+		return false;
+	}
+
+
+	public function supportsSubscription(): bool
+	{
+		return false;
+	}
+
+
+	public function supportsUnscheduledCardOnFile(): bool
+	{
+		return false;
+	}
+
+
+	public function isWallet(): bool
+	{
+		return false;
+	}
+}
