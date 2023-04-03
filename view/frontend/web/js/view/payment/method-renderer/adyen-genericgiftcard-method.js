21a327a938feeba7f8c9dd707122d1733560e3ff
Can Demiralp <ecandemiralp@gmail.com>
2023-04-03 14:49:39 +0200

    [PW-8248] Auto generate genericgiftcard payment method

---
+++ view/frontend/web/js/view/payment/method-renderer/adyen-genericgiftcard-method.js
@@ -0,0 +1,28 @@
+/**
+ *
+ * Adyen Payment module (https://www.adyen.com/)
+ *
+ * Copyright (c) 2023 Adyen N.V. (https://www.adyen.com/)
+ * See LICENSE.txt for license details.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+define(
+    [
+        'Magento_Checkout/js/model/quote',
+        'Adyen_Payment/js/view/payment/method-renderer/adyen-pm-method',
+    ],
+    function(
+        quote,
+        adyenPaymentMethod,
+    ) {
+        return adyenPaymentMethod.extend({
+            initialize: function () {
+                this._super();
+            },
+            buildComponentConfiguration: function (paymentMethod, paymentMethodsExtraInfo, result) {
+                // TODO:: This function should be implemented.
+            }
+        })
+    }
+);
