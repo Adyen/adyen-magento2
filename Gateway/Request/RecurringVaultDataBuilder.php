10bc6cbc12975c05f7b55099bf199b1512a4d4d9
RokPopov <rok.popovledinski@adyen.com>
2023-06-29 12:51:17 +0200

    change the class from which we obtain the quote entity_id

--- Gateway/Request/RecurringVaultDataBuilder.php
+++ Gateway/Request/RecurringVaultDataBuilder.php
@@ -15,6 +15,7 @@ use Adyen\Payment\Helper\StateData;
 use Adyen\Payment\Helper\Vault;
 use Adyen\Payment\Model\Ui\AdyenCcConfigProvider;
 use Adyen\Payment\Model\Ui\AdyenOneclickConfigProvider;
+use Magento\Quote\Model\ResourceModel\Quote\CollectionFactory;
 use Magento\Payment\Gateway\Data\PaymentDataObject;
 use Magento\Payment\Gateway\Helper\SubjectReader;
 use Magento\Payment\Gateway\Request\BuilderInterface;
@@ -26,9 +27,18 @@ class RecurringVaultDataBuilder implements BuilderInterface
      */
     private $stateData;
 
-    public function __construct(StateData $stateData)
+    /**
+     * @var CollectionFactory
+     */
+    private $quoteCollectionFactory;
+
+    public function __construct(
+        StateData $stateData,
+        CollectionFactory $quoteCollectionFactory
+    )
     {
         $this->stateData = $stateData;
+        $this->quoteCollectionFactory = $quoteCollectionFactory;
     }
 
     /**
@@ -42,13 +52,15 @@ class RecurringVaultDataBuilder implements BuilderInterface
         $paymentDataObject = SubjectReader::readPayment($buildSubject);
         $payment = $paymentDataObject->getPayment();
         $paymentMethod = $payment->getMethodInstance();
-        $order = $paymentDataObject->getOrder();
         $extensionAttributes = $payment->getExtensionAttributes();
         $paymentToken = $extensionAttributes->getVaultPaymentToken();
         $details = json_decode($paymentToken->getTokenDetails() ?: '{}', true);
+        $quoteCollection = $this->quoteCollectionFactory->create();
+        $quoteCollection->setOrder('entity_id', 'desc');
+        $quote = $quoteCollection->getFirstItem();
 
         // Initialize the request body with the current state data
-        $requestBody = $this->stateData->getStateData($order->getQuoteId());
+        $requestBody = $this->stateData->getStateData($quote->getId());
 
         // For now this will only be used by tokens created trough adyen_hpp payment methods
         if (array_key_exists(Vault::TOKEN_TYPE, $details)) {
