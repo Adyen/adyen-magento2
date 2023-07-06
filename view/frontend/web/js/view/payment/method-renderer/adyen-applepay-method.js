4d99c3d792944c62e60f0b504f5162ceeb479c67
Can Demiralp <ecandemiralp@gmail.com>
2023-07-06 12:11:36 +0200

    [PW-8437] Implement Apple Pay payment method

---
+++ view/frontend/web/js/view/payment/method-renderer/adyen-applepay-method.js
@@ -0,0 +1,42 @@
+/**
+ *
+ * Adyen Payment module (https://www.adyen.com/)
+ *
+ * Copyright (c) 2023 Adyen N.V. (https://www.adyen.com/)
+ * See LICENSE.txt for license details.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+define(
+    [
+        'Magento_Checkout/js/model/quote',
+        'Adyen_Payment/js/view/payment/method-renderer/adyen-pm-method',
+    ],
+    function(
+        quote,
+        adyenPaymentMethodComponent,
+    ) {
+        return adyenPaymentMethodComponent.extend({
+            initialize: function () {
+                this._super();
+
+                this.adyenPaymentMethod(this.checkBrowserCompatibility());
+            },
+            buildComponentConfiguration: function (paymentMethod, paymentMethodsExtraInfo, result) {
+                let baseComponentConfiguration = this._super();
+                let applePayConfiguration = Object.assign(baseComponentConfiguration,
+                    {
+                        showPayButton: true,
+                        totalPriceLabel: baseComponentConfiguration.configuration.merchantName
+                    }
+                );
+
+                return applePayConfiguration;
+            },
+            checkBrowserCompatibility: function () {
+                // Disables Apple Pay for non-Safari browsers
+                return /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+            }
+        })
+    }
+);
