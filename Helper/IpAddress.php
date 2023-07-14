db7197a8972c7adead9373617b71250ed11fdf6b
khushboos <khushboo.singhvi@adyen.com>
2023-07-14 15:49:10 +0200

    Removing debug code

--- Helper/IpAddress.php
+++ Helper/IpAddress.php
@@ -82,7 +82,6 @@ class IpAddress
      */
     public function isIpAddressValid($ipAddresses): bool
     {
-        return true;
         $isNotificationsIpCheckEnabled = $this->configHelper->getNotificationsIpCheck();
 
         if (!$isNotificationsIpCheckEnabled) {
