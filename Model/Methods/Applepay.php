c7b0d8d363c44e859da408bfebec461a817b6cb8
Can Demiralp <ecandemiralp@gmail.com>
2023-07-20 12:24:59 +0200

    [PW-8511] Include ApplePay fix

--- Model/Methods/Applepay.php
+++ Model/Methods/Applepay.php
@@ -8,14 +8,38 @@
 
 namespace Adyen\Payment\Model\Methods;
 
+use Adyen\Payment\Block\Form\Hpp;
+use Adyen\Payment\Block\Info\Hpp as HppInfo;
 use Adyen\Payment\Model\AdyenPaymentMethod;
+use Adyen\Payment\Model\Api\PaymentRequest;
 use Adyen\Payment\Model\Method\PaymentMethodInterface;
+use Magento\Framework\Event\ManagerInterface;
+use Magento\Payment\Gateway\Command\CommandPoolInterface;
+use Magento\Payment\Gateway\Config\ValueHandlerPoolInterface;
+use Magento\Payment\Gateway\Data\PaymentDataObjectFactory;
+use Magento\Payment\Gateway\Validator\ValidatorPoolInterface;
 
 class Applepay extends AdyenPaymentMethod implements PaymentMethodInterface
 {
 	public const CODE = 'adyen_applepay';
 	public const TX_VARIANT = 'applepay';
 	public const NAME = 'Apple Pay';
+
+    public function __construct(
+        PaymentRequest $paymentRequest,
+        ManagerInterface $eventManager,
+        ValueHandlerPoolInterface $valueHandlerPool,
+        PaymentDataObjectFactory $paymentDataObjectFactory,
+        CommandPoolInterface $commandPool = null,
+        ValidatorPoolInterface $validatorPool = null
+    ) {
+        $code = self::CODE;
+        $formBlockType = Hpp::class;
+        $infoBlockType = HppInfo::class;
+
+        parent::__construct($paymentRequest, $eventManager, $valueHandlerPool,
+            $paymentDataObjectFactory, $code, $formBlockType, $infoBlockType, $commandPool, $validatorPool);
+    }
 
 	public function supportsRecurring(): bool
 	{
