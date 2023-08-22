8563546ebaf4e8edec3f8892618166f0ef71b841
RokPopov <rok.popovledinski@adyen.com>
2023-08-22 15:37:49 +0200

    setup custom log formatter

--- Logger/Handler/AdyenBase.php
+++ Logger/Handler/AdyenBase.php
@@ -12,9 +12,34 @@
 namespace Adyen\Payment\Logger\Handler;
 
 use Magento\Framework\Logger\Handler\Base;
+use Monolog\Formatter\LineFormatter;
+use Magento\Framework\Filesystem\DriverInterface;
 
 class AdyenBase extends Base
 {
+    /**
+     * @var string|null
+     */
+    private $logFormat;
+
+    /**
+     * AdyenBase constructor.
+     *
+     * @param DriverInterface $filesystem
+     * @param string|null $filePath
+     * @param string|null $fileName
+     * @param string|null $logFormat
+     */
+    public function __construct(DriverInterface $filesystem, ?string $filePath = null, ?string $fileName = null, ?string $logFormat = null)
+    {
+        parent::__construct($filesystem, $filePath, $fileName);
+
+        $this->logFormat = $logFormat;
+
+        if ($this->logFormat) {
+            $this->setFormatter(new LineFormatter($this->logFormat));
+        }
+    }
     /**
      * overwrite core it needs to be the exact level otherwise use different handler
      *
