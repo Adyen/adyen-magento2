f5b8d800bdcc36509b157c0588ca00850316b982
RokPopov <rok.popovledinski@adyen.com>
2023-08-28 15:54:34 +0200

    abstract buildComponentConfiguration in helper and use it for each payment method

---
+++ view/frontend/web/js/helper/configHelper.js
@@ -0,0 +1,158 @@
+/**
+ *
+ * Adyen Payment module (https://www.adyen.com/)
+ *
+ * Copyright (c) 2023 Adyen NV (https://www.adyen.com/)
+ * See LICENSE.txt for license details.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+define([
+    'jquery',
+    'Adyen_Payment/js/model/adyen-configuration',
+    'Magento_Checkout/js/model/quote'
+], function ($, adyenConfiguration, quote) {
+    'use strict';
+
+    return {
+        /**
+         * @param address
+         * @returns {{country: (string|*), firstName: (string|*), lastName: (string|*), city: (*|string), street: *, postalCode: (*|string), houseNumber: string, telephone: (string|*)}}
+         */
+        getFormattedAddress: function (address) {
+                let city = '';
+                let country = '';
+                let postalCode = '';
+                let street = '';
+                let houseNumber = '';
+
+                city = address.city;
+                country = address.countryId;
+                postalCode = address.postcode;
+
+                street = address.street.slice(0);
+
+                // address contains line items as an array, otherwise if string just pass along as is
+                if (Array.isArray(street)) {
+                    // getHouseNumberStreetLine > 0 implies the street line that is used to gather house number
+                    if (adyenConfiguration.getHouseNumberStreetLine() > 0) {
+                        // removes the street line from array that is used to contain house number
+                        houseNumber = street.splice(adyenConfiguration.getHouseNumberStreetLine() - 1, 1);
+                    } else { // getHouseNumberStreetLine = 0 uses the last street line as house number
+                        // in case there is more than 1 street lines in use, removes last street line from array that should be used to contain house number
+                        if (street.length > 1) {
+                            houseNumber = street.pop();
+                        }
+                    }
+
+                    // Concat street lines except house number
+                    street = street.join(' ');
+                }
+
+                var firstName = address.firstname;
+                var lastName = address.lastname;
+                var telephone = address.telephone;
+
+                return {
+                    city: city,
+                    country: country,
+                    postalCode: postalCode,
+                    street: street,
+                    houseNumber: houseNumber,
+                    firstName: firstName,
+                    lastName: lastName,
+                    telephone: telephone
+                };
+        },
+        getAdyenGender: function (gender) {
+            if (gender === 1) {
+                return 'MALE';
+            } else if (gender === 2) {
+                return 'FEMALE';
+            }
+            return 'UNKNOWN';
+        },
+        buildComponentConfiguration: function(paymentMethod, paymentMethodsExtraInfo) {
+            var self = this;
+            debugger;
+            var email = '';
+            var shopperGender = '';
+            var shopperDateOfBirth = '';
+
+            if (!!customerData.email) {
+                email = customerData.email;
+            } else if (!!quote.guestEmail) {
+                email = quote.guestEmail;
+            }
+
+            shopperGender = customerData.gender;
+            shopperDateOfBirth = customerData.dob;
+
+            var formattedShippingAddress = {};
+            var formattedBillingAddress = {};
+
+            if (!!quote.shippingAddress()) {
+                formattedShippingAddress = this.getFormattedAddress(quote.shippingAddress());
+            }
+
+            if (!!quote.billingAddress()) {
+                formattedBillingAddress = this.getFormattedAddress(quote.billingAddress());
+            }
+
+            /*Use the storedPaymentMethod object and the custom onChange function as the configuration object together*/
+            var configuration = Object.assign(paymentMethod,
+                {
+                    showPayButton: false,
+                    countryCode: formattedShippingAddress.country ? formattedShippingAddress.country : formattedBillingAddress.country, // Use shipping address details as default and fall back to billing address if missing
+                    hasHolderName: adyenConfiguration.getHasHolderName(),
+                    holderNameRequired: adyenConfiguration.getHasHolderName() &&
+                        adyenConfiguration.getHolderNameRequired(),
+                    data: {
+                        personalDetails: {
+                            firstName: formattedBillingAddress.firstName,
+                            lastName: formattedBillingAddress.lastName,
+                            telephoneNumber: formattedBillingAddress.telephone,
+                            shopperEmail: email,
+                            gender: this.getAdyenGender(shopperGender),
+                            dateOfBirth: shopperDateOfBirth,
+                        },
+                        billingAddress: {
+                            city: formattedBillingAddress.city,
+                            country: formattedBillingAddress.country,
+                            houseNumberOrName: formattedBillingAddress.houseNumber,
+                            postalCode: formattedBillingAddress.postalCode,
+                            street: formattedBillingAddress.street,
+                        },
+                    },
+                    onChange: function(state) {
+                        $('#stateData').val(state.isValid ? JSON.stringify(state.data) : '');
+                    },
+                    onClick: function(resolve, reject) {
+                        if (self.validate()) {
+                            resolve();
+                        } else {
+                            reject();
+                        }
+                    },
+                });
+
+            if (formattedShippingAddress) {
+                configuration.data.shippingAddress = {
+                    city: formattedShippingAddress.city,
+                    country: formattedShippingAddress.country,
+                    houseNumberOrName: formattedShippingAddress.houseNumber,
+                    postalCode: formattedShippingAddress.postalCode,
+                    street: formattedShippingAddress.street
+                };
+            }
+
+            // Use extra configuration from the paymentMethodsExtraInfo object if available
+            if (paymentMethod.type in paymentMethodsExtraInfo && 'configuration' in paymentMethodsExtraInfo[paymentMethod.type]) {
+                configuration = Object.assign(configuration, paymentMethodsExtraInfo[paymentMethod.type].configuration);
+            }
+
+            return configuration;
+        }
+    };
+});
+
