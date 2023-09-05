fe84b1def8fcb726740c905945660040cec77fe4
raoulritter <59527829+raoulritter@users.noreply.github.com>
2023-09-05 09:13:22 +0200

    [ECP-8523] - Enable ApplePay recurring to use Token

--- Model/Ui/AdyenGenericConfigProvider.php
+++ Model/Ui/AdyenGenericConfigProvider.php
@@ -41,6 +41,7 @@ class AdyenGenericConfigProvider implements ConfigProviderInterface
     const ADYEN_KLARNA_VAULT_CODE = 'adyen_klarna_vault';
     const ADYEN_PAYPAL_VAULT_CODE = 'adyen_paypal_vault';
     const ADYEN_GOOGLEPAY_VAULT_CODE = 'adyen_googlepay_vault';
+    const ADYEN_APPLEPAY_VAULT_CODE = 'adyen_applepay_vault';
 
     // Separate payment method tx_variants
     const IDEAL_TX_VARIANT = 'ideal';
