dac66baade8e7d7c93e7fa69df2d96cd6e0778e9
Can Demiralp <ecandemiralp@gmail.com>
2023-09-11 16:15:34 +0200

    [ECP-8578] Implement payment methods (#2203)
    * [ECP-8578] Implement Affirm
    
    * [ECP-8578] Implement Afterpay Touch
    
    * [ECP-8578] Fix logo image size
    
    * [ECP-8578] Implement Alipay
    
    * [ECP-8578] Implement AlipayHK
    
    * [ECP-8578] Implement Alma
    
    * [ECP-8578] Implement Atome
    
    * [ECP-8578] Implement Afterpay Default
    
    * [ECP-8578] Implement Bank Transfer in Belgium
    
    * [ECP-8578] Implement Bank Transfer in Switzerland
    
    * [ECP-8578] Implement Bank Transfer in Germany
    
    * [ECP-8578] Implement Bank Transfer in Great Britain
    
    * [ECP-8578] Implement International Bank Transfer (IBAN)
    
    * [ECP-8578] Implement Bank Transfer in the Netherlands
    
    * [ECP-8578] Implement BLIK
    
    * [ECP-8578] Implement Clearpay
    
    * [ECP-8578] Implement Direct Debit Great Britain
    
    * [ECP-8578] Implement Sofort
    
    * [ECP-8578] Implement Online Banking Finland
    
    * [ECP-8578] Implement eContext PayEasy ATM
    
    * [ECP-8578] Implement eContext PayEasy Online Banking
    
    * [ECP-8578] Implement eContext 7Eleven
    
    * [ECP-8578] Implement eContext Convenience Stores
    
    * [ECP-8578] Implement Entercash
    
    * [ECP-8578] Implement EPS
    
    * [ECP-8578] Implement Facilypay 4x
    
    * [ECP-8578] Implement Facilypay 6x
    
    * [ECP-8578] Implement Facilypay 10x
    
    * [ECP-8578] Implement Facilypay 12x
    
    * [ECP-8578] Implement giropay
    
    * [ECP-8578] Implement GrabPay MY
    
    * [ECP-8578] Implement GrabPay PH
    
    * [ECP-8578] Implement GrabPay SG
    
    * [ECP-8578] Implement KakaoPay
    
    * [ECP-8578] Implement Klarna Pay over Time
    
    * [ECP-8578] Implement Klarna Pay Now
    
    * [ECP-8578] Implement MB WAY
    
    * [ECP-8578] Implement 7Eleven via MOLPay
    
    * [ECP-8578] Implement Malaysian eBanking via Fpx MOLPay
    
    * [ECP-8578] Implement Thailand eBanking via MOLPay
    
    * [ECP-8578] Implement Online banking Czech Republic
    
    * [ECP-8578] Implement Online banking Poland
    
    * [ECP-8578] Implement Online banking Slovakia
    
    * [ECP-8578] Implement PayBright
    
    * [ECP-8578] Implement Open banking
    
    * [ECP-8578] Implement Pix
    
    * [ECP-8578] Implement Ratepay
    
    * [ECP-8578] Implement Ratepay Direct Debit
    
    * [ECP-8578] Implement Swish
    
    * [ECP-8578] Implement Trustly
    
    * [ECP-8578] Implement TWINT
    
    * [ECP-8578] Implement Walley
    
    * [ECP-8578] Implement WeChat Pay Web
    
    * [ECP-8578] Implement WeChat Pay QR
    
    * [ECP-8578] Implement Zip

---
+++ view/frontend/web/js/view/payment/method-renderer/multishipping/adyen-ratepay-method.js
@@ -0,0 +1,38 @@
+/**
+ *
+ * Adyen Payment module (https://www.adyen.com/)
+ *
+ * Copyright (c) 2023 Adyen N.V. (https://www.adyen.com/)
+ * See LICENSE.txt for license details.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+define(
+    [
+        'Adyen_Payment/js/view/payment/method-renderer/multishipping/adyen-pm-method'
+    ],
+    function(
+        adyenPaymentMethod
+    ) {
+        return adyenPaymentMethod.extend({
+            buildComponentConfiguration: function (paymentMethod, paymentMethodsExtraInfo) {
+                var ratePayId = window.checkoutConfig.payment.adyenHpp.ratePayId;
+                var dfValueRatePay = self.getRatePayDeviceIdentToken();
+
+                window.di = {
+                    t: dfValueRatePay.replace(':', ''),
+                    v: ratePayId,
+                    l: 'Checkout',
+                };
+
+                // Load Ratepay script
+                var ratepayScriptTag = document.createElement('script');
+                ratepayScriptTag.src = '//d.ratepay.com/' + ratePayId + '/di.js';
+                ratepayScriptTag.type = 'text/javascript';
+                document.body.appendChild(ratepayScriptTag);
+
+                return baseComponentConfiguration = this._super();
+            }
+        })
+    }
+);
