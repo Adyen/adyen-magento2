60da519b82a8999012f2ddcc577d5ae21883d877
Can Demiralp <ecandemiralp@gmail.com>
2023-09-20 11:18:57 +0200

    [ECP-8083] Remove terminal api initiate endpoint

--- Gateway/Request/PosCloudBuilder.php
+++ Gateway/Request/PosCloudBuilder.php
@@ -104,7 +104,15 @@ class PosCloudBuilder implements BuilderInterface
                 ]
         ];
 
-        if ($fundingSource === PaymentMethods::FUNDING_SOURCE_CREDIT) {
+        if ($fundingSource === PaymentMethods::FUNDING_SOURCE_DEBIT) {
+            $request['SaleToPOIRequest']['PaymentRequest']['PaymentTransaction']['TransactionConditions'] = [
+                "DebitPreferredFlag" => true
+            ];
+
+            $request['SaleToPOIRequest']['PaymentData'] = [
+                'PaymentType' => $transactionType,
+            ];
+        } else {
             if (isset($numberOfInstallments)) {
                 $request['SaleToPOIRequest']['PaymentRequest']['PaymentData'] = [
                     "PaymentType" => "Instalment",
@@ -124,14 +132,6 @@ class PosCloudBuilder implements BuilderInterface
 
             $request['SaleToPOIRequest']['PaymentRequest']['PaymentTransaction']['TransactionConditions'] = [
                 "DebitPreferredFlag" => false
-            ];
-        } elseif ($fundingSource === PaymentMethods::FUNDING_SOURCE_DEBIT) {
-            $request['SaleToPOIRequest']['PaymentRequest']['PaymentTransaction']['TransactionConditions'] = [
-                "DebitPreferredFlag" => true
-            ];
-
-            $request['SaleToPOIRequest']['PaymentData'] = [
-                'PaymentType' => $transactionType,
             ];
         }
 
