bba1eea25840d29d902a3dfb40008129780100da
Khushboo <khushboo.singhvi@adyen.com>
2023-10-02 14:57:29 +0200

    Update adyen-pm-method.js

--- view/frontend/web/js/view/payment/method-renderer/adyen-pm-method.js
+++ view/frontend/web/js/view/payment/method-renderer/adyen-pm-method.js
@@ -81,7 +81,6 @@ define(
             createCheckoutComponent: async function(paymentMethodsResponse) {
                 // Set to null by default and modify depending on the paymentMethods response
                 this.adyenPaymentMethod(null);
-                console.log("In debug mode");
                 if (this.checkBrowserCompatibility() && !!paymentMethodsResponse.paymentMethodsResponse) {
                     this.checkoutComponent = await adyenCheckout.buildCheckoutComponent(
                         paymentMethodsResponse,
