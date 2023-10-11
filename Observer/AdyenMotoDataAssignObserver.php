aee291da05b89b70702cf5d8fee64a258b2e351f
Khushboo <khushboo.singhvi@adyen.com>
2023-10-11 12:49:16 +0200

    [ECP-8591] Anomaly in Payment Method Transition: Persistence of cc_type Value Despite Payment Method Change (#2266)
    * [ECP-8591] - Anomaly in Payment Method Transition: Persistence of cc_type Value Despite Payment Method Change
    
    * [ECP-8591] - Anomaly in Payment Method Transition: Persistence of cc_type Value Despite Payment Method Change

--- Observer/AdyenMotoDataAssignObserver.php
+++ Observer/AdyenMotoDataAssignObserver.php
@@ -85,6 +85,9 @@ class AdyenMotoDataAssignObserver extends AbstractDataAssignObserver
         $data = $this->readDataArgument($observer);
         $paymentInfo = $this->readPaymentModelArgument($observer);
 
+        // Remove cc_type information from the previous payment
+        $paymentInfo->unsAdditionalInformation('cc_type');
+
         // Get additional data array
         $additionalData = $data->getData(PaymentInterface::KEY_ADDITIONAL_DATA);
         if (!is_array($additionalData)) {
