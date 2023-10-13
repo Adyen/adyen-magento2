fc814a43bed5a9cad9b0b9301be829535efd3d06
hossam <hossam.fares@adyen.com>
2023-10-13 15:51:44 +0200

    [ECP-8434] Remove-min-max-order-config-paths-from-the-database

--- Setup/Patch/Data/PreAuthorizedSettingsMigration.php
+++ Setup/Patch/Data/PreAuthorizedSettingsMigration.php
@@ -11,11 +11,40 @@ declare(strict_types=1);
 
 namespace Adyen\Payment\Setup\Patch\Data;
 
+use Magento\Framework\App\Config\ReinitableConfigInterface;
+use Magento\Framework\App\Config\Storage\WriterInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
-use Magento\Sales\Model\Order;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchVersionInterface;
 
-class PreAuthorizedSettingsMigration extends AbstractConfigurationSettingsMigration
+class PreAuthorizedSettingsMigration implements DataPatchInterface, PatchVersionInterface
 {
+    private ModuleDataSetupInterface $moduleDataSetup;
+    private WriterInterface $configWriter;
+    private ReinitableConfigInterface $reinitableConfig;
+
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        WriterInterface $configWriter,
+        ReinitableConfigInterface $reinitableConfig
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->configWriter = $configWriter;
+        $this->reinitableConfig = $reinitableConfig;
+    }
+
+    /**
+     * Do Upgrade
+     *
+     * @return void
+     */
+    public function apply()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        $this->updateSchemaVersion($this->moduleDataSetup);
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
     /**
      * Ensure that new path does not exist before updating
      *
@@ -24,9 +53,63 @@ class PreAuthorizedSettingsMigration extends AbstractConfigurationSettingsMigrat
     public function updateSchemaVersion(ModuleDataSetupInterface $setup)
     {
         $path = 'payment/adyen_abstract/payment_pre_authorized';
-        $existValue = Order::STATE_PENDING_PAYMENT;
-        $newValue = Order::STATE_PROCESSING;
-        $this->updateConfigValue($setup, $path, $existValue, $newValue);
+
+        $config = $this->findConfig($setup, $path, \Magento\Sales\Model\Order::STATE_PENDING_PAYMENT);
+        if (isset($config)) {
+            $this->configWriter->save(
+                $path,
+                \Magento\Sales\Model\Order::STATE_PROCESSING,
+                $config['scope'],
+                $config['scope_id']
+            );
+        }
+        // re-initialize otherwise it will cause errors
+        $this->reinitableConfig->reinit();
+    }
+
+    /**
+     * Return the config based on the passed path and value. If value is null, return the first item in array
+     *
+     * @param ModuleDataSetupInterface $setup
+     * @param string $path
+     * @param string|null $value
+     * @return array|null
+     */
+    private function findConfig(ModuleDataSetupInterface $setup, string $path, ?string $value): ?array
+    {
+        $config = null;
+        $configDataTable = $setup->getTable('core_config_data');
+        $connection = $setup->getConnection();
+        $select = $connection->select()->from($configDataTable)->where('path = ?', $path);
+        $matchingConfigs = $connection->fetchAll($select);
+
+        if (!empty($matchingConfigs) && is_null($value)) {
+            $config = reset($matchingConfigs);
+        } else {
+            foreach ($matchingConfigs as $matchingConfig) {
+                if ($matchingConfig['value'] === $value) {
+                    $config = $matchingConfig;
+                }
+            }
+        }
+
+        return $config;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getDependencies()
+    {
+        return [];
     }
 
     public static function getVersion()
