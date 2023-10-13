51f501dc379bccf1d7e76149364095d4da9d1634
Rok Popov Ledinski <rok.popov.ledinski90@gmail.com>
2023-10-13 15:41:45 -0400

    Update Setup/Patch/Data/VaultMigration.php

--- Setup/Patch/Data/VaultMigration.php
+++ Setup/Patch/Data/VaultMigration.php
@@ -2,7 +2,7 @@
 /**
  * Adyen Payment module (https://www.adyen.com/)
  *
- * Copyright (c) 2023 Adyen BV (https://www.adyen.com/)
+ * Copyright (c) 2023 Adyen N.V. (https://www.adyen.com/)
  * See LICENSE.txt for license details.
  *
  * Author: Adyen <magento@adyen.com>
