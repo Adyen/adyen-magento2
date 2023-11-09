9942d57f8b215f28a3d50895f4d974078719ac92
khushboos <khushboo.singhvi@adyen.com>
2023-11-09 13:28:23 +0100

    [ECP-8685] - Create instruction to be compliant with upcoming Visa requirement - V8

---
+++ Setup/Patch/Data/CreditCardsBecomeCards.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Adyen\Payment\Setup\Patch\Data;
+
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchVersionInterface;
+
+class CreditCardsBecomeCards implements DataPatchInterface, PatchVersionInterface
+{
+    private ModuleDataSetupInterface $moduleDataSetup;
+
+    public function __construct(
+        ModuleDataSetupInterface  $moduleDataSetup,
+    )
+    {
+        $this->moduleDataSetup = $moduleDataSetup;
+    }
+
+    /**
+     * Do Upgrade
+     *
+     * @return void
+     */
+    public function apply()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        $this->updateCreditCardToCards($this->moduleDataSetup);
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    /**
+     * Update the 'Credit Card' path to 'Cards'
+     *
+     * @param ModuleDataSetupInterface $setup
+     */
+    public function updateCreditCardToCards(ModuleDataSetupInterface $setup)
+    {
+        $connection = $setup->getConnection();
+        $configTable = $setup->getTable('core_config_data');
+
+        $oldValue = 'Credit Card';
+        $newValue = 'Cards';
+
+        $connection->update(
+            $configTable,
+            ['value' => $newValue],
+            ['value = ?' => $oldValue]
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function getVersion()
+    {
+        return '8.22.5';
+    }
+}
