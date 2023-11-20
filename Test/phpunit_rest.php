0636b896526168743368381d6e76b0a745446b48
Can Demiralp <ecandemiralp@gmail.com>
2023-11-20 16:20:16 +0100

    [ECP-8677] Use https for base url

--- Test/phpunit_rest.php
+++ Test/phpunit_rest.php
@@ -2,7 +2,7 @@
 
 ini_set('date.timezone', 'Europe/Amsterdam');
 
-define('TESTS_BASE_URL', 'http://' . getenv('MAGENTO_HOST'));
+define('TESTS_BASE_URL', 'https://' . getenv('MAGENTO_HOST'));
 define('TESTS_WEBSERVICE_USER', 'admin');
 define('TESTS_WEBSERVICE_APIKEY', '123123q');
 define('TESTS_MAGENTO_INSTALLATION', 'disabled');
