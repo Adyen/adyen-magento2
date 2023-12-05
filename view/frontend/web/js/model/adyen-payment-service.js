a543dc7a72141da2aaa54678de93e6ae3b1f2dd1
Marwa BRAHAM <114406666+Marwaa92@users.noreply.github.com>
2023-12-05 10:41:01 +0100

    Do not launch customer/section/load after adyen payment retrieve action

--- view/frontend/web/js/model/adyen-payment-service.js
+++ view/frontend/web/js/model/adyen-payment-service.js
@@ -52,7 +52,8 @@ define(
 
                 return storage.post(
                     serviceUrl,
-                    JSON.stringify(payload)
+                    JSON.stringify(payload),
+                    false
                 );
             },
 
