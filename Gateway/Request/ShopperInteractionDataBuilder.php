e781817e3a8ae971d78b10533ae3e1acd097424b
Can Demiralp <ecandemiralp@gmail.com>
2024-01-10 13:53:00 +0100

    [ECP-8893] Allow usage of storedPaymentMethodId in the request

--- Gateway/Request/ShopperInteractionDataBuilder.php
+++ Gateway/Request/ShopperInteractionDataBuilder.php
@@ -11,6 +11,7 @@
 
 namespace Adyen\Payment\Gateway\Request;
 
+use Adyen\Payment\Helper\StateData;
 use Adyen\Payment\Model\Ui\Adminhtml\AdyenMotoConfigProvider;
 use Adyen\Payment\Model\Ui\AdyenPayByLinkConfigProvider;
 use Magento\Framework\App\State;
@@ -26,10 +27,14 @@ class ShopperInteractionDataBuilder implements BuilderInterface
     const SHOPPER_INTERACTION_ECOMMERCE = 'Ecommerce';
 
     private State $appState;
+    private StateData $stateData;
 
-    public function __construct(Context $context)
-    {
+    public function __construct(
+        Context $context,
+        StateData $stateData
+    ) {
         $this->appState = $context->getAppState();
+        $this->stateData = $stateData;
     }
 
     /**
@@ -42,6 +47,7 @@ class ShopperInteractionDataBuilder implements BuilderInterface
         /** @var \Magento\Payment\Gateway\Data\PaymentDataObject $paymentDataObject */
         $paymentDataObject = SubjectReader::readPayment($buildSubject);
         $payment = $paymentDataObject->getPayment();
+        $order = $payment->getOrder();
         $paymentMethod = $payment->getMethodInstance()->getCode();
 
         if ($paymentMethod == AdyenPayByLinkConfigProvider::CODE) {
@@ -52,11 +58,15 @@ class ShopperInteractionDataBuilder implements BuilderInterface
         // Ecommerce is the default shopperInteraction
         $shopperInteraction = self::SHOPPER_INTERACTION_ECOMMERCE;
 
+        // Check if it's a tokenised payment or not.
+        $stateData = $this->stateData->getStateData($order->getQuoteId());
+        $storedPaymentMethodId = $this->stateData->getStoredPaymentMethodIdFromStateData($stateData);
+
         if ($paymentMethod == AdyenMotoConfigProvider::CODE &&
             $this->appState->getAreaCode() == \Magento\Framework\App\Area::AREA_ADMINHTML) {
             // Backend CC orders are MOTO
             $shopperInteraction = self::SHOPPER_INTERACTION_MOTO;
-        } elseif (str_contains($paymentMethod, '_vault')) {
+        } elseif (str_contains($paymentMethod, '_vault') || isset($storedPaymentMethodId)) {
             // Vault is ContAuth
             $shopperInteraction = self::SHOPPER_INTERACTION_CONTAUTH;
         }
