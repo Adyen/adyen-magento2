ac9e30480a3dd644ff3ae2d440c0ad2c2ee44391
Cenk Kucukiravul <cenkiravul35@gmail.com>
2024-01-25 14:25:04 +0100

    Update e2e bash script to have multi purpose

--- .github/scripts/e2e.sh
+++ .github/scripts/e2e.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
+
+# Base configuration and installation
 set -euo pipefail
+cd /tmp
+git clone https://github.com/Adyen/adyen-integration-tools-tests.git
+cd adyen-integration-tools-tests
+git checkout $INTEGRATION_TESTS_BRANCH
+rm -rf package-lock.json
+npm i
 
-# Checkout E2E tests
-cd /tmp;
-git clone https://github.com/Adyen/adyen-integration-tools-tests.git;
-cd adyen-integration-tools-tests;
-git checkout $INTEGRATION_TESTS_BRANCH;
+option="$1"
 
-# Setup environment
-rm -rf package-lock.json;
-npm i;
-
-# Run tests
-npm run test:ci:magento
+# Run the desired group of tests
+case $option in
+    "standard")
+        echo "Running Standard Set of E2E Tests."
+        npm run test:ci:magento
+        ;;
+    "express-checkout")
+        echo "Running Express Checkout E2E Tests."
+        npm run test:ci:magento:express-checkout
+        ;;
+    "all")
+        echo "Running All Magento E2E Tests"
+        npm run test:ci:magento:all
+        ;;
+esac
