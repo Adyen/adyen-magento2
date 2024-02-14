2f865a4a4732245fef9d0774bee7ce8d5dc00375
Bram <bgerritsen@emico.nl>
2024-02-14 12:19:55 +0100

    fix: change from recurring schema to recurring data

--- Console/Command/WebhookProcessorCommand.php
+++ Console/Command/WebhookProcessorCommand.php
@@ -2,7 +2,7 @@
 
 namespace Adyen\Payment\Console\Command;
 
-use Adyen\Payment\Cron\WebhookProcessorFactory;
+use Adyen\Payment\Cron\WebhookProcessor;
 use Exception;
 use Magento\Framework\Console\Cli;
 use Symfony\Component\Console\Command\Command;
@@ -12,13 +12,13 @@ use Symfony\Component\Console\Output\OutputInterface;
 class WebhookProcessorCommand extends Command
 {
     /**
-     * @var WebhookProcessorFactory
+     * @var WebhookProcessor
      */
-    private $webhookProcessorFactory;
+    private $webhookProcessor;
 
-    public function __construct(WebhookProcessorFactory $webhookProcessorFactory)
+    public function __construct(WebhookProcessor $webhookProcessor)
     {
-        $this->webhookProcessorFactory = $webhookProcessorFactory;
+        $this->webhookProcessor = $webhookProcessor;
         parent::__construct();
     }
 
@@ -35,7 +35,7 @@ class WebhookProcessorCommand extends Command
     {
         $output->writeln('Starting webhook processor.');
         try {
-            $this->webhookProcessorFactory->create()->execute();
+            $this->webhookProcessor->execute();
         } catch (Exception $e) {
             return Cli::RETURN_FAILURE;
         }
