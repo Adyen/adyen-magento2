0130fabe387e25271d4639e838d093221327bdc4
raoulritter <59527829+raoulritter@users.noreply.github.com>
2024-03-13 12:39:22 +0100

    Add AbstractDisputeWebhookHandlerTest.php

--- Test/Unit/Helper/Webhook/AbstractDisputeWebhookHandlerTest.php
+++ Test/Unit/Helper/Webhook/AbstractDisputeWebhookHandlerTest.php
@@ -15,6 +15,7 @@ use Adyen\Payment\Helper\Webhook\ChargebackReversedWebhookHandler;
 use Adyen\Payment\Logger\AdyenLogger;
 use Adyen\Payment\Helper\Order;
 use Adyen\Payment\Test\Unit\AbstractAdyenTestCase;
+use Adyen\Webhook\PaymentStates;
 use Magento\Sales\Model\Order as MagentoOrder;
 use Adyen\Payment\Model\Notification;
 
@@ -102,6 +103,41 @@ class AbstractDisputeWebhookHandlerTest extends AbstractAdyenTestCase
         );
 
         $result = $this->abstractDisputeWebhookHandler->handleWebhook($orderMock, $notificationMock, 'REFUND');
+
+        $this->assertEquals($orderMock, $result);
+    }
+    public function testHandleRefundNotificationWhenDisputeNotificationsAreNotIgnored()
+    {
+        $storeId = 1;
+        $ignoreDisputeNotifications = false;
+        $transitionState = PaymentStates::STATE_REFUNDED;
+        $eventCode = 'REFUND';
+        $pspReference = 'test_psp_reference';
+        $merchantReference = 'test_merchant_reference';
+
+        $orderMock = $this->createMock(MagentoOrder::class);
+        $notificationMock = $this->createMock(Notification::class);
+
+        $orderMock->method('getStoreId')->willReturn($storeId);
+        $orderMock->method('getPayment')->willReturnSelf();
+        $orderMock->method('getData')->withConsecutive(['adyen_psp_reference'], ['entity_id'])
+            ->willReturnOnConsecutiveCalls($pspReference, $merchantReference);
+
+        $notificationMock->method('getEventCode')->willReturn($eventCode);
+        $notificationMock->method('getId')->willReturn('1');
+        $notificationMock->method('getPspreference')->willReturn($pspReference);
+        $notificationMock->method('getMerchantReference')->willReturn($merchantReference);
+
+        $this->configHelperMock->method('getConfigData')->with('ignore_dispute_notification', 'adyen_abstract', $storeId)
+            ->willReturn($ignoreDisputeNotifications);
+
+        $this->orderHelperMock->expects($this->once())->method('refundOrder')->with($orderMock, $notificationMock)->willReturn($orderMock);
+
+        $this->adyenLoggerMock->expects($this->once())->method('addAdyenNotification')
+            ->with($this->stringContains('The order has been updated by the REFUND notification.'), $this->arrayHasKey('pspReference'))
+            ->willReturn(null);
+
+        $result = $this->abstractDisputeWebhookHandler->handleWebhook($orderMock, $notificationMock, $transitionState);
 
         $this->assertEquals($orderMock, $result);
     }
