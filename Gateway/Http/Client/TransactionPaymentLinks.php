2c9a5ffa17a0ed07ab38c450d3eab046e1bc4c75
Sushmita Thakur <sushmitathakur1993@gmail.com>
2024-08-05 17:14:24 +0200

    Revert "[ECP-8938] Add applicationInfo in request body for transaction reques…" (#2698)
    This reverts commit ed7587140b9a0d2358969bbc978d208474ffff48.

--- Gateway/Http/Client/TransactionPaymentLinks.php
+++ Gateway/Http/Client/TransactionPaymentLinks.php
@@ -73,7 +73,6 @@ class TransactionPaymentLinks implements ClientInterface
 
         $requestOptions['idempotencyKey'] = $idempotencyKey;
         $requestOptions['headers'] = $this->adyenHelper->buildRequestHeaders();
-        $request['applicationInfo'] = $this->adyenHelper->buildApplicationInfo($client);
 
         $this->adyenHelper->logRequest($request, Client::API_CHECKOUT_VERSION, '/paymentLinks');
         try {
