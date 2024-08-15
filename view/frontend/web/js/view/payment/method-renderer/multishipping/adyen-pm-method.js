84cbfab9258019cf315f44d4989eb632b8dc7f44
RokPopov <rok.popovledinski@adyen.com>
2024-08-15 14:09:49 +0200

    make sure multishipping payment method components are loaded with event listeners and the logic they route to is executed

--- view/frontend/web/js/view/payment/method-renderer/multishipping/adyen-pm-method.js
+++ view/frontend/web/js/view/payment/method-renderer/multishipping/adyen-pm-method.js
@@ -32,15 +32,16 @@ define([
         paymentMethodReady: ko.observable(false),
 
         initialize: function() {
-            console.log('Initializing adyen-pm-method');
             let self = this;
             this._super();
+
+            this.isChecked = ko.observable(false);
 
             let paymentMethodsObserver = adyenPaymentService.getPaymentMethods();
             paymentMethodsObserver.subscribe(
                 function(paymentMethods) {
-                    console.log('Payment methods updated: ', paymentMethods);
                     self.paymentMethodReady(paymentMethods);
+                    self.renderCheckoutComponent();
                 }
             );
 
@@ -48,18 +49,83 @@ define([
         },
 
         selectPaymentMethod: function () {
+            let result = this._super();
+
+            // Trigger the payment method change
+            this.isChecked(true);
+
+            // Render the checkout component
+            this.renderCheckoutComponent();
+
             if (!!this.paymentComponent && this.paymentComponent.isValid) {
                 $('#stateData').val(JSON.stringify(this.paymentComponent.data));
+            } else {
+                console.warn('Payment component is not valid or not available');
             }
 
-            return this._super();
+            return result;
         },
 
         buildComponentConfiguration: function(paymentMethod, paymentMethodsExtraInfo) {
-            console.log('Building component configuration for: ', paymentMethod);
-            let config = configHelper.buildMultishippingComponentConfiguration(paymentMethod, paymentMethodsExtraInfo);
-            console.log('Component configuration: ', config);
-            return config;
+            return configHelper.buildMultishippingComponentConfiguration(paymentMethod, paymentMethodsExtraInfo);
+        },
+
+        renderCheckoutComponent: function() {
+            let methodCode = this.getMethodCode();
+            let paymentMethod = this.paymentMethod();
+
+            if (!paymentMethod || !this.isChecked()) {
+                console.error('Payment method is undefined for ', methodCode);
+                return;
+            }
+
+            let configuration = this.buildComponentConfiguration(paymentMethod, this.paymentMethodsExtraInfo());
+
+            if (this.paymentComponent) {
+                this.paymentComponent.update(configuration);
+            } else {
+                this.mountPaymentMethodComponent(paymentMethod, configuration, methodCode);
+            }
+        },
+
+        mountPaymentMethodComponent: function(paymentMethod, configuration, methodCode) {
+            let self = this;
+
+            const containerId = '#' + paymentMethod.type + 'Container';
+
+            if ($(containerId).length) {
+                if (this.paymentComponent && typeof this.paymentComponent.unmount === 'function') {
+                    this.paymentComponent.unmount();
+                }
+
+                const paymentMethodComponent = this.checkoutComponent.create(
+                    paymentMethod.type,
+                    configuration
+                );
+
+
+                paymentMethodComponent.mount(containerId);
+
+                this.paymentComponent = paymentMethodComponent;
+
+                if (typeof paymentMethodComponent.onChange === 'function') {
+                    paymentMethodComponent.onChange(function(state) {
+                        self.onPaymentMethodChange(state, methodCode);
+                    });
+                } else {
+                    console.warn('Unable to add onChange event listener to payment component', paymentMethodComponent);
+                }
+            } else {
+                console.warn('Container not found for', containerId);
+            }
+        },
+
+        onPaymentMethodChange: function(state, methodCode) {
+            if (methodCode !== this.getMethodCode()) {
+                return;
+            }
+            this.isPlaceOrderAllowed(state.isValid);
+            $('#stateData').val(state.data ? JSON.stringify(state.data) : '');
         }
     });
 });
