bfbdde74903e64734a6eca136b78c15ff92993e8
RokPopov <rok.popovledinski@adyen.com>
2024-09-30 10:51:22 +0200

    validate terms&conditions, make sure button is disabled if they are not checked

--- view/frontend/web/js/view/payment/method-renderer/adyen-paypal-method.js
+++ view/frontend/web/js/view/payment/method-renderer/adyen-paypal-method.js
@@ -11,12 +11,16 @@ define(
     [
         'Magento_Checkout/js/model/quote',
         'Adyen_Payment/js/view/payment/method-renderer/adyen-pm-method',
+        'Adyen_Payment/js/model/adyen-configuration',
         'Magento_Checkout/js/model/full-screen-loader',
+        'jquery'
     ],
     function(
         quote,
         adyenPaymentMethod,
-        fullScreenLoader
+        adyenConfiguration,
+        fullScreenLoader,
+        $
     ) {
         return adyenPaymentMethod.extend({
             placeOrderButtonVisible: false,
@@ -27,6 +31,25 @@ define(
                 let baseComponentConfiguration = this._super();
                 let paypalConfiguration = Object.assign(baseComponentConfiguration, paymentMethodsExtraInfo[paymentMethod.type].configuration);
                 paypalConfiguration.showPayButton = true;
+                let agreementsConfig = adyenConfiguration.getAgreementsConfig();
+
+                if (null !== agreementsConfig) {
+                    let self = this;
+
+                    paypalConfiguration.onInit = function (data, actions) {
+                        actions.disable()
+
+                        $(document).off('change', '.checkout-agreements input').on('change', '.checkout-agreements input', function () {
+                            self.updatePayPalButton(actions);
+                        });
+                    }
+
+                    paypalConfiguration.onClick = function (data, actions) {
+                        if(!self.validate()) {
+                            console.error('Agreements configuration failed');
+                        }
+                    }
+                }
 
                 return paypalConfiguration
             },
@@ -38,8 +61,18 @@ define(
             handleOnFailure: function(response, component) {
                 this.isPlaceOrderAllowed(true);
                 fullScreenLoader.stopLoader();
+                if (response && response.error) {
+                    console.error('Error details:', response.error);
+                }
                 component.handleReject(response);
             },
-        })
+            updatePayPalButton: function (actions) {
+                if (this.validate()) {
+                    actions.enable();
+                } else {
+                    actions.disable();
+                }
+            },
+         })
     }
 );
