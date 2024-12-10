0c8cff97cdb47c4ff29be07b894f395eabd1d9b4
sushmita <sushmita.thakur@adyen.com>
2024-12-10 22:49:40 +0100

    [ECP-9482-1] reverse the frontend type change for giftcard

--- view/frontend/web/js/view/payment/method-renderer/adyen-giftcard-method.js
+++ view/frontend/web/js/view/payment/method-renderer/adyen-giftcard-method.js
@@ -307,7 +307,7 @@ define(
                     additionalData.brand_code = stateData.data.paymentMethod.brand;
                     additionalData.stateData = JSON.stringify(stateData.data);
                 }
-                additionalData.frontendType = 'luma';
+                additionalData.frontendType = 'default';
                 let data = {
                     'method': this.item.method,
                     'additional_data': additionalData
