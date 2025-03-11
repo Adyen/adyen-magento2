d48ab2923b481d238881b501b55f8efa4ebb99f4
Can Demiralp <can.demiralp@adyen.com>
2025-03-11 14:23:18 +0100

    [ECP-9639] Refactor captures response handler

---
+++ Gateway/Response/ModificationsCapturesResponseHandler.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ *
+ * Adyen Payment module (https://www.adyen.com/)
+ *
+ * Copyright (c) 2025 Adyen N.V. (https://www.adyen.com/)
+ * See LICENSE.txt for license details.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+
+namespace Adyen\Payment\Gateway\Response;
+
+use Adyen\Payment\Gateway\Http\Client\TransactionCapture;
+use Adyen\Payment\Helper\Invoice;
+use Adyen\Payment\Logger\AdyenLogger;
+use Magento\Framework\Exception\AlreadyExistsException;
+use Magento\Payment\Gateway\Helper\SubjectReader;
+use Magento\Payment\Gateway\Response\HandlerInterface;
+use Magento\Sales\Model\Order\Payment;
+
+class ModificationsCapturesResponseHandler implements HandlerInterface
+{
+    /**
+     * @param AdyenLogger $adyenLogger
+     * @param Invoice $invoiceHelper
+     */
+    public function __construct(
+        private readonly AdyenLogger $adyenLogger,
+        private readonly Invoice $invoiceHelper
+    ) { }
+
+    /**
+     * @param array $handlingSubject
+     * @param array $responseCollection
+     * @throws AlreadyExistsException
+     */
+    public function handle(array $handlingSubject, array $responseCollection): void
+    {
+        $payment = SubjectReader::readPayment($handlingSubject);
+        /** @var Payment $payment */
+        $payment = $payment->getPayment();
+
+        foreach ($responseCollection as $response) {
+            if (count($responseCollection) > 1) {
+                $this->adyenLogger->info(sprintf(
+                    'Handling partial OR multiple capture response in details handler for order %s',
+                    $payment->getOrder()->getIncrementId()
+                ));
+            }
+
+            if (!empty($response['pspReference'])) {
+                // Set transId to the last capture request
+                $payment->setLastTransId($response['pspReference']);
+            }
+
+            $this->invoiceHelper->createAdyenInvoice(
+                $payment,
+                $response['pspReference'] ?? '',
+                $response[TransactionCapture::ORIGINAL_REFERENCE],
+                $response[TransactionCapture::CAPTURE_AMOUNT]
+            );
+        }
+
+        // Set the invoice status to pending and wait for CAPTURE webhook
+        $payment->setIsTransactionPending(true);
+        // Do not close parent authorisation since order can still be cancelled/refunded
+        $payment->setShouldCloseParentTransaction(false);
+    }
+}
