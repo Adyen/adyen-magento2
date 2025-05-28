11b2fa5a72efde6e6deec633637602787c03733c
Can Demiralp <can.demiralp@adyen.com>
2025-05-28 13:58:18 +0200

    [ECP-9698] Fix REST api tests and add new tests for donation campaigns (#2973)
    * [ECP-9698] Fix REST api tests
    
    * [ECP-9698] Add test case for donationCampaigns endpoint
    
    * [ECP-9698] Update workflow job names for clarity
    
    * [ECP-9698] Formatting
    
    * [ECP-9698] Formatting

--- Test/phpunit_rest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-ini_set('date.timezone', 'Europe/Amsterdam');
-
-define('TESTS_BASE_URL', 'http://' . getenv('MAGENTO_HOST'));
-define('TESTS_WEBSERVICE_USER', 'admin');
-define('TESTS_WEBSERVICE_APIKEY', '123123q');
-define('TESTS_MAGENTO_INSTALLATION', 'disabled');
-define('TESTS_CLEANUP', 'enabled');
