cb73996746408d181f2e16e3babe50697475d512
Can Demiralp <can.demiralp@adyen.com>
2025-06-06 09:23:46 +0200

    [ECP-9686] Adjust unit tests

--- Test/Unit/Model/CleanupAdditionalInformationTest.php
+++ Test/Unit/Model/CleanupAdditionalInformationTest.php
@@ -12,6 +12,7 @@
 namespace Adyen\Payment\Test\Helper\Unit\Model;
 
 use Adyen\Payment\Api\CleanupAdditionalInformationInterface;
+use Adyen\Payment\Logger\AdyenLogger;
 use Adyen\Payment\Model\CleanupAdditionalInformation;
 use Adyen\Payment\Test\Unit\AbstractAdyenTestCase;
 use Magento\Sales\Model\Order\Payment;
@@ -21,12 +22,16 @@ class CleanupAdditionalInformationTest extends AbstractAdyenTestCase
 {
     protected ?CleanupAdditionalInformation $cleanupAdditionalInformation;
     protected Payment|MockObject $orderPaymentMock;
+    protected AdyenLogger|MockObject $adyenLoggerMock;
 
     public function setUp(): void
     {
         $this->orderPaymentMock = $this->createMock(Payment::class);
+        $this->adyenLoggerMock = $this->createMock(AdyenLogger::class);
 
-        $this->cleanupAdditionalInformation = new CleanupAdditionalInformation();
+        $this->cleanupAdditionalInformation = new CleanupAdditionalInformation(
+            $this->adyenLoggerMock
+        );
     }
 
     public function tearDown(): void
@@ -36,7 +41,8 @@ class CleanupAdditionalInformationTest extends AbstractAdyenTestCase
 
     public function testExecute()
     {
-        $this->orderPaymentMock->expects($this->exactly(2))
+        $this->orderPaymentMock
+            ->expects($this->exactly(count(CleanupAdditionalInformationInterface::FIELDS_TO_BE_CLEANED_UP)))
             ->method('unsAdditionalInformation')
             ->willReturnMap([
                 [CleanupAdditionalInformationInterface::FIELD_ADDITIONAL_DATA, $this->orderPaymentMock],
