6fcab2965909a817f48ba4c8132cc9816af67215
Can Demiralp <can.demiralp@adyen.com>
2025-06-17 16:54:44 +0200

    Remove unnecessary array key

--- Gateway/Response/AbstractOrderStatusHistoryHandler.php
+++ Gateway/Response/AbstractOrderStatusHistoryHandler.php
@@ -46,11 +46,6 @@ class AbstractOrderStatusHistoryHandler implements HandlerInterface
             $payment = $readPayment->getPayment();
             $order = $payment->getOrder();
 
-            // Temporary workaround to clean-up `hasOnlyGiftCards` key. It needs to be handled separately.
-            if (isset($responseCollection['hasOnlyGiftCards'])) {
-                unset($responseCollection['hasOnlyGiftCards']);
-            }
-
             foreach ($responseCollection as $response) {
                 $comment = $this->orderStatusHistoryHelper->buildApiResponseComment(
                     $response,
