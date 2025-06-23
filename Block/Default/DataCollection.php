5dacfe12b310e97e1270a9698bee122a53d85667
Can Demiralp <can.demiralp@adyen.com>
2025-06-23 09:48:51 +0200

    [ECP-9651] Create new layout for data collection block

---
+++ Block/Default/DataCollection.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *
+ * Adyen Payment module (https://www.adyen.com/)
+ *
+ * Copyright (c) 2021 Adyen BV (https://www.adyen.com/)
+ * See LICENSE.txt for license details.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+
+namespace Adyen\Payment\Block\Default;
+
+use Magento\Framework\View\Element\Template;
+
+class DataCollection extends Template
+{
+
+}
