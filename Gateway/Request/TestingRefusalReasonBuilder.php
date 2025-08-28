7bce92cfc333d727002f9175d0c4aba4594f92ae
Dimitri BOUTEILLE <dimitri.bouteille@reflet-digital.com>
2025-08-28 11:53:57 +0200

    ✨ Update header in TestingRefusalReasonBuilder

--- Gateway/Request/TestingRefusalReasonBuilder.php
+++ Gateway/Request/TestingRefusalReasonBuilder.php
@@ -1,9 +1,13 @@
 <?php
 /**
- * Copyright © Reflet Digital, all rights reserved.
- * See LICENSE_REFLET.txt for license details.
+ * Adyen Payment Module
+ *
+ * Copyright (c) 2023 Adyen N.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ *
+ * Author: Adyen <magento@adyen.com>
  */
-declare(strict_types=1);
 
 namespace Adyen\Payment\Gateway\Request;
 
