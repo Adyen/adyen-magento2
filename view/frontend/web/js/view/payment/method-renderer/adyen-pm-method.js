e5368a4ec4011b1c5f583e359d50b778578bf4e9
Khushboo <khushboo.singhvi@adyen.com>
2025-11-19 09:18:42 +0100

    [ECP-9764] Place Order button enables with BCMC if billing address changes are not saved (#3147)
    * [ECP-9764] Place Order button enables with BCMC if billing address changes are not saved
    
    * [ECP-9764] Place Order button enables with BCMC if billing address changes are not saved

--- view/frontend/web/js/view/payment/method-renderer/adyen-pm-method.js
+++ view/frontend/web/js/view/payment/method-renderer/adyen-pm-method.js
@@ -488,7 +488,8 @@ define(
             },
 
             isButtonActive: function() {
-                return paymentComponentStates().getIsPlaceOrderAllowed(this.getMethodCode());
+                return this.isPlaceOrderActionAllowed() &&
+                    paymentComponentStates().getIsPlaceOrderAllowed(this.getMethodCode());
             },
 
             /**
