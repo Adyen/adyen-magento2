c6f7f180cf4ef8f3f20c9216e47c51b05f0fb965
Shubham Kumar <shubham.kumar@adyen.com>
2026-01-16 14:44:43 +0100

    [ECP-9487-V9] Handle exceptions in AdyenLogger getInvoiceContext (#3221)
    * [ECP-9487] Add exception handling to getInvoiceContext in AdyenLogger
    
    * [ECP-9487] Catch Throwable and log warning

--- Logger/AdyenLogger.php
+++ Logger/AdyenLogger.php
@@ -120,18 +120,23 @@ class AdyenLogger extends Logger
 
     public function getInvoiceContext(MagentoOrder\Invoice $invoice): array
     {
-        $stateName = $invoice->getStateName();
+        try {
+            $stateName = $invoice->getStateName();
 
-        return [
-            'invoiceId' => $invoice->getEntityId(),
-            'invoiceIncrementId' => $invoice->getIncrementId(),
-            'invoiceState' => $invoice->getState(),
-            'invoiceStateName' => $stateName instanceof Phrase ? $stateName->getText() : $stateName,
-            'invoiceWasPayCalled' => $invoice->wasPayCalled(),
-            'invoiceCanCapture' => $invoice->canCapture(),
-            'invoiceCanCancel' => $invoice->canCancel(),
-            'invoiceCanVoid' => $invoice->canVoid(),
-            'invoiceCanRefund' => $invoice->canRefund()
-        ];
+            return [
+                'invoiceId' => $invoice->getEntityId(),
+                'invoiceIncrementId' => $invoice->getIncrementId(),
+                'invoiceState' => $invoice->getState(),
+                'invoiceStateName' => $stateName instanceof Phrase ? $stateName->getText() : $stateName,
+                'invoiceWasPayCalled' => $invoice->wasPayCalled(),
+                'invoiceCanCapture' => $invoice->canCapture(),
+                'invoiceCanCancel' => $invoice->canCancel(),
+                'invoiceCanVoid' => $invoice->canVoid(),
+                'invoiceCanRefund' => $invoice->canRefund()
+            ];
+        } catch (\Throwable $e) {
+            $this->addAdyenWarning('Failed to retrieve invoice context: ' . $e->getMessage());
+            return [];
+        }
     }
 }
