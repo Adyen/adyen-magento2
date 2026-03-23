277353f7ad8ad798c4dcfadbe00ce6d539e3a061
Can Demiralp <can.demiralp@adyen.com>
2026-03-23 10:47:58 +0100

    [ECP-9891] Return structured /payments/details response in case of refusals and errors (#3277)
    * [ECP-9891] Stop throwing exception and return structured response
    
    * [ECP-9891] Add message to canceled and refused cases
    
    * [ECP-9891] Refactor frontend files
    
    * [ECP-9891] Formatting
    
    * [ECP-9891] Update GraphQl schema
    
    * [ECP-9891] Handle error message
    
    * [ECP-9891] Update unit tests
    
    * [ECP-9891] Upgrade Magento version
    
    * Revert "[ECP-9891] Upgrade Magento version"
    
    This reverts commit 4eb22e2378805447597e85072279e3d9fb1b5a9a.

--- Helper/PaymentResponseHandler.php
+++ Helper/PaymentResponseHandler.php
@@ -85,6 +85,12 @@ class PaymentResponseHandler
                     "canDonate" => $donationTokenExists
                 ];
             case self::REFUSED:
+            case self::CANCELLED:
+                return [
+                    "isFinal" => true,
+                    "resultCode" => $resultCode,
+                    "message" => __("The payment is %1.", strtoupper($resultCode))
+                ];
             case self::ERROR:
             case self::POS_SUCCESS:
                 return [
