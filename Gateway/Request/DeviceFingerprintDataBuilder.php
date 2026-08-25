05eed7cc58796e8ee72fb9ec718942fa5e57308f
Can Demiralp <can.demiralp@adyen.com>
2026-08-25 17:09:21 +0200

    [ECP-9987-v10] Add deviceFingerprint field into /payments request

---
+++ Gateway/Request/DeviceFingerprintDataBuilder.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ *
+ * Adyen Payment module (https://www.adyen.com/)
+ *
+ * Copyright (c) 2026 Adyen N.V. (https://www.adyen.com/)
+ * See LICENSE.txt for license details.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+
+namespace Adyen\Payment\Gateway\Request;
+
+use Adyen\Payment\Observer\AdyenCcDataAssignObserver;
+use Magento\Payment\Gateway\Data\PaymentDataObject;
+use Magento\Payment\Gateway\Helper\SubjectReader;
+use Magento\Payment\Gateway\Request\BuilderInterface;
+
+class DeviceFingerprintDataBuilder  implements BuilderInterface
+{
+    /**
+     * @param array $buildSubject
+     * @return array[]
+     */
+    public function build(array $buildSubject): array
+    {
+        /** @var PaymentDataObject $paymentDataObject */
+        $paymentDataObject = SubjectReader::readPayment($buildSubject);
+        $payment = $paymentDataObject->getPayment();
+
+        $deviceFingerprint = $payment->getAdditionalInformation(AdyenCcDataAssignObserver::DEVICE_FINGERPRINT);
+        if (!empty($deviceFingerprint)) {
+            $requestBody['deviceFingerprint'] = $deviceFingerprint;
+        }
+
+        return [
+            'body' => $requestBody ?? []
+        ];
+    }
+}
--- Observer/AdyenCcDataAssignObserver.php
+++ Observer/AdyenCcDataAssignObserver.php
@@ -33,6 +33,7 @@ class AdyenCcDataAssignObserver extends AbstractDataAssignObserver
     const STORE_PAYMENT_METHOD = 'storePaymentMethod';
     const RETURN_URL = 'returnUrl';
     const RECURRING_PROCESSING_MODEL = 'recurringProcessingModel';
+    const DEVICE_FINGERPRINT = 'deviceFingerprint';
 
     /**
      * Approved root level keys from additional data array
@@ -114,7 +115,12 @@ class AdyenCcDataAssignObserver extends AbstractDataAssignObserver
         // Remove each CC-specific field from the previous payment only if the incoming
         // data contains a replacement for that specific field. This prevents the placeOrder
         // mutation from clearing data that was set by setPaymentMethodOnCart.
-        $ccSpecificKeys = [self::CC_TYPE, self::NUMBER_OF_INSTALLMENTS, self::COMBO_CARD_TYPE];
+        $ccSpecificKeys = [
+            self::CC_TYPE,
+            self::NUMBER_OF_INSTALLMENTS,
+            self::COMBO_CARD_TYPE,
+            self::DEVICE_FINGERPRINT
+        ];
         foreach ($ccSpecificKeys as $ccKey) {
             if (array_key_exists($ccKey, $additionalData)) {
                 $paymentInfo->unsAdditionalInformation($ccKey);
@@ -126,6 +132,20 @@ class AdyenCcDataAssignObserver extends AbstractDataAssignObserver
             $stateData = json_decode((string)$additionalData[self::STATE_DATA], true);
         } else {
             $stateData = $this->stateDataCollection->getStateDataArrayWithQuoteId($paymentInfo->getData('quote_id'));
+        }
+
+        // Store deviceFingerprint to be used later for Visa DCAP if the value exists
+        if (isset($stateData['riskData']['clientData'])) {
+            $decodedClientData = base64_decode($stateData['riskData']['clientData']);
+            if ($decodedClientData !== false) {
+                $clientData = json_decode($decodedClientData, true);
+                if (json_last_error() === JSON_ERROR_NONE) {
+                    $paymentInfo->setAdditionalInformation(
+                        self::DEVICE_FINGERPRINT,
+                        $clientData['deviceFingerprint']
+                    );
+                }
+            }
         }
 
         // Get validated state data array
